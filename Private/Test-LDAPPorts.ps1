018e2fe22267ad72db052c88b4a39d9d9003723d
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-07-31 11:13:01 +0200

    Update

--- Private/Test-LDAPPorts.ps1
+++ Private/Test-LDAPPorts.ps1
@@ -4,29 +4,21 @@
         [string] $ServerName,
         [int] $Port
     )
-
-    $Output = @{
-        Status  = $false
-        Comment = $null
-    }
     if ($ServerName -and $Port -ne 0) {
         try {
             $LDAP = "LDAP://" + $ServerName + ':' + $Port
-            # $LDAP = $LDAPPrep + ":389"
             $Connection = [ADSI]($LDAP)
             $Connection.Close()
-            $Output['Status'] = $true
+            return $true
         } catch {
             if ($_.Exception.ToString() -match "The server is not operational") {
-                #Write-Warning "Can't open $ServerName`:$Port."
-                $Output['Comment'] = "Can't open $ServerName`:$Port."
+                Write-Warning "Can't open $ServerName`:$Port."
             } elseif ($_.Exception.ToString() -match "The user name or password is incorrect") {
-                #Write-Warning "Current user ($Env:USERNAME) doesn't seem to have access to to LDAP on port $Server`:$Port"
-                $Output['Comment'] = "Current user ($Env:USERNAME) doesn't seem to have access to to LDAP on port $Server`:$Port"
+                Write-Warning "Current user ($Env:USERNAME) doesn't seem to have access to to LDAP on port $Server`:$Port"
             } else {
-                # Write-Warning -Message $_
+                Write-Warning -Message $_
             }
         }
+        return $False
     }
-    $Output
 }
