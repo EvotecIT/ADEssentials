605b6d55ea9af23c7ffecd70fd04cac3501c16b4
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-01 09:54:58 +0200

    Update

--- Examples/Example-LDAP.ps1
+++ Examples/Example-LDAP.ps1
@@ -1,3 +1,3 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-Test-LDAP -ComputerName 'AD1','AD2' | Format-Table
+Test-LDAP -ComputerName 'AD1','AD2','ADPREVIEW2019','DC1','AD4','ADRODC' -WarningAction SilentlyContinue | Format-Table
