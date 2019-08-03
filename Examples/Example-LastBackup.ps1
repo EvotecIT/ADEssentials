9dd607263a2ef51ff88c37fd74744a554bfe5555
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-03 14:22:50 +0200

    Update

---
+++ Examples/Example-LastBackup.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+# Tests whole forest
+$LastBackup = Get-WinADLastBackup
+$LastBackup | Format-Table -AutoSize
+
+# Tests just one or more domains
+$LastBackup = Get-WinADLastBackup -Domain 'ad.evotec.pl', 'ad.evotec.xyz'
+$LastBackup | Format-Table -AutoSize
