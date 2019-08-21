17bef5a6c2d7a35e20ae8829f99ef7cf5957a346
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-21 23:31:12 +0200

    Update

--- Public/Get-WinADSiteConnections.ps1
+++ Public/Get-WinADSiteConnections.ps1
@@ -1,7 +1,8 @@
 ﻿function Get-WinADSiteConnections {
     [CmdletBinding()]
     param(
-
+        [alias('Joiner')][string] $Splitter,
+        [string] $Formatted
     )
 
     [Flags()]
@@ -19,45 +20,94 @@
     $NamingContext = (Get-ADRootDSE).configurationNamingContext
     $Connections = Get-ADObject –Searchbase $NamingContext -LDAPFilter "(objectCategory=ntDSConnection)" -Properties *
     $FormmatedConnections = foreach ($_ in $Connections) {
-        $Dictionary = [PSCustomObject] @{
+        if ($null -eq $_.Options) {
+            $Options = 'None'
+        } else {
+            $Options = ([ConnectionOption] $_.Options) -split ', '
+        }
+        if ($Formatted) {
+            $Dictionary = [PSCustomObject] @{
 
-            <# Regex extracts AD1 and AD2
+                <# Regex extracts AD1 and AD2
         CN=d1695d10-8d24-41db-bb0f-2963e2c7dfcd,CN=NTDS Settings,CN=AD1,CN=Servers,CN=KATOWICE-1,CN=Sites,CN=Configuration,DC=ad,DC=evotec,DC=xyz
         CN=NTDS Settings,CN=AD2,CN=Servers,CN=KATOWICE-1,CN=Sites,CN=Configuration,DC=ad,DC=evotec,DC=xyz
         #>
-            CN                = $_.CN
-            Description       = $_.Description
-            DisplayName       = $_.DisplayName
-            EnabledConnection = $_.enabledConnection
-            ServerFrom        = if ($_.fromServer -match '(?<=CN=NTDS Settings,CN=)(.*)(?=,CN=Servers,)') {
-                $Matches[0]
-            } else {
-                $_.fromServer
-            }
-            ServerTo          = if ($_.DistinguishedName -match '(?<=CN=NTDS Settings,CN=)(.*)(?=,CN=Servers,)') {
-                $Matches[0]
-            } else {
-                $_.fromServer
-            }
-            <# Regex extracts KATOWICE-1
+                'CN'                 = $_.CN
+                'Description'        = $_.Description
+                'Display Name'       = $_.DisplayName
+                'Enabled Connection' = $_.enabledConnection
+                'Server From'        = if ($_.fromServer -match '(?<=CN=NTDS Settings,CN=)(.*)(?=,CN=Servers,)') {
+                    $Matches[0]
+                } else {
+                    $_.fromServer
+                }
+                'Server To'          = if ($_.DistinguishedName -match '(?<=CN=NTDS Settings,CN=)(.*)(?=,CN=Servers,)') {
+                    $Matches[0]
+                } else {
+                    $_.fromServer
+                }
+                <# Regex extracts KATOWICE-1
         CN=d1695d10-8d24-41db-bb0f-2963e2c7dfcd,CN=NTDS Settings,CN=AD1,CN=Servers,CN=KATOWICE-1,CN=Sites,CN=Configuration,DC=ad,DC=evotec,DC=xyz
         CN=NTDS Settings,CN=AD2,CN=Servers,CN=KATOWICE-1,CN=Sites,CN=Configuration,DC=ad,DC=evotec,DC=xyz
         #>
-            SiteFrom          = if ($_.fromServer -match '(?<=,CN=Servers,CN=)(.*)(?=,CN=Sites,CN=Configuration)') {
-                $Matches[0]
-            } else {
-                $_.fromServer
+                'Site From'          = if ($_.fromServer -match '(?<=,CN=Servers,CN=)(.*)(?=,CN=Sites,CN=Configuration)') {
+                    $Matches[0]
+                } else {
+                    $_.fromServer
+                }
+                'Site To'            = if ($_.DistinguishedName -match '(?<=,CN=Servers,CN=)(.*)(?=,CN=Sites,CN=Configuration)') {
+                    $Matches[0]
+                } else {
+                    $_.fromServer
+                }
+                'Options'            = if ($Splitter -ne '') { $Options -Join $Splitter } else { $Options }
+                #'Options'            = $_.Options
+                'When Created'       = $_.WhenCreated
+                'When Changed'       = $_.WhenChanged
+                'Is Deleted'         = $_.IsDeleted
             }
-            SiteTo            = if ($_.DistinguishedName -match '(?<=,CN=Servers,CN=)(.*)(?=,CN=Sites,CN=Configuration)') {
-                $Matches[0]
-            } else {
-                $_.fromServer
+        } else {
+            $Dictionary = [PSCustomObject] @{
+
+                <# Regex extracts AD1 and AD2
+        CN=d1695d10-8d24-41db-bb0f-2963e2c7dfcd,CN=NTDS Settings,CN=AD1,CN=Servers,CN=KATOWICE-1,CN=Sites,CN=Configuration,DC=ad,DC=evotec,DC=xyz
+        CN=NTDS Settings,CN=AD2,CN=Servers,CN=KATOWICE-1,CN=Sites,CN=Configuration,DC=ad,DC=evotec,DC=xyz
+        #>
+                CN                = $_.CN
+                Description       = $_.Description
+                DisplayName       = $_.DisplayName
+                EnabledConnection = $_.enabledConnection
+                ServerFrom        = if ($_.fromServer -match '(?<=CN=NTDS Settings,CN=)(.*)(?=,CN=Servers,)') {
+                    $Matches[0]
+                } else {
+                    $_.fromServer
+                }
+                ServerTo          = if ($_.DistinguishedName -match '(?<=CN=NTDS Settings,CN=)(.*)(?=,CN=Servers,)') {
+                    $Matches[0]
+                } else {
+                    $_.fromServer
+                }
+                <# Regex extracts KATOWICE-1
+        CN=d1695d10-8d24-41db-bb0f-2963e2c7dfcd,CN=NTDS Settings,CN=AD1,CN=Servers,CN=KATOWICE-1,CN=Sites,CN=Configuration,DC=ad,DC=evotec,DC=xyz
+        CN=NTDS Settings,CN=AD2,CN=Servers,CN=KATOWICE-1,CN=Sites,CN=Configuration,DC=ad,DC=evotec,DC=xyz
+        #>
+                SiteFrom          = if ($_.fromServer -match '(?<=,CN=Servers,CN=)(.*)(?=,CN=Sites,CN=Configuration)') {
+                    $Matches[0]
+                } else {
+                    $_.fromServer
+                }
+                SiteTo            = if ($_.DistinguishedName -match '(?<=,CN=Servers,CN=)(.*)(?=,CN=Sites,CN=Configuration)') {
+                    $Matches[0]
+                } else {
+                    $_.fromServer
+                }
+                Options           = if ($Splitter -ne '') { $Options -Join $Splitter } else { $Options }
+                #Options           = $_.Options
+                WhenCreated       = $_.WhenCreated
+                WhenChanged       = $_.WhenChanged
+                IsDeleted         = $_.IsDeleted
             }
-            OptionsTranslated = [ConnectionOption] $_.Options
-            Options           = $_.Options
-            WhenCreated       = $_.WhenCreated
-            WhenChanged       = $_.WhenChanged
-            IsDeleted         = $_.IsDeleted
+
         }
         $Dictionary
     }
--- Public/Get-WinADSiteLinks.ps1
+++ Public/Get-WinADSiteLinks.ps1
@@ -1,22 +1,53 @@
 ﻿function Get-WinADSiteLinks {
     [CmdletBinding()]
     param(
-
+        [alias('Joiner')][string] $Splitter,
+        [string] $Formatted
     )
+    [Flags()]
+    enum SiteLinksOptions {
+        None = 0
+        UseNotify = 1
+        TwoWaySync = 2
+        DisableCompression = 4
+    }
+
     $NamingContext = (Get-ADRootDSE).configurationNamingContext
     $SiteLinks = Get-ADObject -LDAPFilter "(objectCategory=sitelink)" –Searchbase $NamingContext -Properties *
     foreach ($_ in $SiteLinks) {
-        [PSCustomObject] @{
-            Name                            = $_.CN
-            Cost                            = $_.Cost
-            ReplicationFrequencyInMinutes   = $_.ReplInterval
-            Options                         = $_.Options
-            #ReplInterval                    : 15
-            Created                         = $_.WhenCreated
-            Modified                        = $_.WhenChanged
-            #Deleted                         :
-            #InterSiteTransportProtocol      : IP
-            ProtectedFromAccidentalDeletion = $_.ProtectedFromAccidentalDeletion
+
+        if ($null -eq $_.Options) {
+            $Options = 'None'
+        } else {
+            $Options = ([SiteLinksOptions] $_.Options) -split ', '
+        }
+
+        if ($Formatted) {
+            [PSCustomObject] @{
+                Name                                 = $_.CN
+                Cost                                 = $_.Cost
+                'Replication Frequency In Minutes'   = $_.ReplInterval
+                Options                              = if ($Splitter -ne '') { $Options -Join $Splitter } else { $Options }
+                #ReplInterval                    : 15
+                Created                              = $_.WhenCreated
+                Modified                             = $_.WhenChanged
+                #Deleted                         :
+                #InterSiteTransportProtocol      : IP
+                'Protected From Accidental Deletion' = $_.ProtectedFromAccidentalDeletion
+            }
+        } else {
+            [PSCustomObject] @{
+                Name                            = $_.CN
+                Cost                            = $_.Cost
+                ReplicationFrequencyInMinutes   = $_.ReplInterval
+                Options                         = if ($Splitter -ne '') { $Options -Join $Splitter } else { $Options }
+                #ReplInterval                    : 15
+                Created                         = $_.WhenCreated
+                Modified                        = $_.WhenChanged
+                #Deleted                         :
+                #InterSiteTransportProtocol      : IP
+                ProtectedFromAccidentalDeletion = $_.ProtectedFromAccidentalDeletion
+            }
         }
     }
 }
