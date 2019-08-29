eb0ef3c8efaf20b5c8ff68b14e9fc14f947b59f3
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-29 21:51:25 +0200

    Update

---
+++ Public/Get-WinADUsersFP.ps1
@@ -0,0 +1,16 @@
+﻿function Get-WinADUsersForeignSecurityPrincipalList {
+    [alias('Get-WinADUserFP')]
+    param(
+        [string] $Domain
+    )
+    $ForeignSecurityPrincipalList = Get-ADObject -Filter { ObjectClass -eq 'ForeignSecurityPrincipal' } -Properties * -Server $Domain
+    foreach ($FSP in $ForeignSecurityPrincipalList) {
+        Try {
+            $Translated = (([System.Security.Principal.SecurityIdentifier]::new($FSP.objectSid)).Translate([System.Security.Principal.NTAccount])).Value
+        } Catch {
+            $Translated = $null
+        }
+        Add-Member -InputObject $FSP -Name 'TranslatedName' -Value $Translated -MemberType NoteProperty -Force
+    }
+    $ForeignSecurityPrincipalList
+}
