a864d4e7345f384eb078e5a2984761d7b80fd1ab
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-03 10:35:05 +0200

    Update

---
+++ Public/Get-WinADGPOMissingPermissions.ps1
@@ -0,0 +1,36 @@
+﻿Function Get-WinADGPOMissingPermissions {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER Domain
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    Based on https://secureinfra.blog/2018/12/31/most-common-mistakes-in-active-directory-and-domain-services-part-1/
+    #>
+
+    [cmdletBinding()]
+    param(
+        [string] $Domain = $Env:USERDNSDOMAIN
+    )
+    $GPOs = Get-GPO -All -Domain $Domain
+    $MissingPermissions = @(
+        foreach ($GPO in $GPOs) {
+            If ($GPO.User.Enabled) {
+                $GPOPermissionForAuthUsers = Get-GPPermission -Guid $GPO.Id -All | Select-Object -ExpandProperty Trustee | Where-Object { $_.Name -eq "Authenticated Users" }
+                $GPOPermissionForDomainComputers = Get-GPPermission -Guid $GPO.Id -All | Select-Object -ExpandProperty Trustee | Where-Object { $_.Name -eq "Domain Computers" }
+                If (-not $GPOPermissionForAuthUsers -and -not $GPOPermissionForDomainComputers) {
+                    $GPO
+                }
+            }
+        }
+    )
+    $MissingPermissions
+}
