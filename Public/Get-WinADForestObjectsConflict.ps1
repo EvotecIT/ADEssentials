6c282d5692a13da4f5bd5a1570b9d129eeee6361
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-11-18 15:45:21 +0100

    New function to find forest conflicts

---
+++ Public/Get-WinADForestObjectsConflict.ps1
@@ -0,0 +1,46 @@
+﻿Function Get-WinADForestObjectsConflict {
+    [CmdletBinding()]
+    Param()
+    # Based on https://gallery.technet.microsoft.com/scriptcenter/Get-ADForestConflictObjects-4667fa37
+    $Forest = Get-ADForest
+    foreach ($Domain in $Forest.Domains) {
+        $DC = Get-ADDomainController -DomainName $Domain -Discover
+        #Get conflict objects
+        Get-ADObject -LDAPFilter "(|(cn=*\0ACNF:*)(ou=*CNF:*))" -Properties WhenChanged -Server $DC |
+        ForEach-Object {
+            $LiveObject = $null
+            $ConflictObject = [PSCustomObject] @{
+                Name                = $_
+                ConflictDn          = $_.DistinguishedName
+                ConflictWhenChanged = $_.WhenChanged
+                LiveDn              = "N/A"
+                LiveWhenChanged     = "N/A"
+            }
+            #See if we are dealing with a 'cn' conflict object
+            if (Select-String -SimpleMatch "\0ACNF:" -InputObject $ConflictObject.ConflictDn) {
+                #Split the conflict object DN so we can remove the conflict notation
+                $SplitConfDN = $ConflictObject.ConflictDn -split "0ACNF:"
+                #Remove the conflict notation from the DN and try to get the live AD object
+                try {
+                    $LiveObject = Get-ADObject -Identity "$($SplitConfDN[0].TrimEnd("\"))$($SplitConfDN[1].Substring(36))" -Properties WhenChanged -Server $DC -erroraction Stop
+                } catch { }
+                if ($LiveObject) {
+                    $ConflictObject.LiveDN = $LiveObject.DistinguishedName
+                    $ConflictObject.LiveWhenChanged = $LiveObject.WhenChanged
+                }
+            } else {
+                #Split the conflict object DN so we can remove the conflict notation for OUs
+                $SplitConfDN = $ConflictObject.ConflictDn -split "CNF:"
+                #Remove the conflict notation from the DN and try to get the live AD object
+                try {
+                    $LiveObject = Get-ADObject -Identity "$($SplitConfDN[0])$($SplitConfDN[1].Substring(36))" -Properties WhenChanged -Server $DC -ErrorAction Stop
+                } catch { }
+                if ($LiveObject) {
+                    $ConflictObject.LiveDN = $LiveObject.DistinguishedName
+                    $ConflictObject.LiveWhenChanged = $LiveObject.WhenChanged
+                }
+            }
+            $ConflictObject
+        }
+    }
+}
