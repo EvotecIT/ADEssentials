5948ecc098d2fd659f4b1be8d1d3b033b62dbbd7
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-16 19:51:28 +0100

    Examples update

---
+++ Examples/Example-EnableInstantReplication.ps1
@@ -0,0 +1,13 @@
+﻿# Get currrent settings so that you can see what those are and change it back if needed
+Get-WinADSiteLinks
+Get-WinADSiteConnections -Verbose | Format-Table -Autosize
+# Set command - BE CAREFUL, I would run read only commands first
+Set-WinADReplication -ReplicationInterval 15 -Instant # you can use both or only one parameter.
+Set-WinADReplicationConnections -Verbose
+# Confirming the settings have applied correctly
+Get-WinADSiteLinks -Verbose | Format-Table -Autosize
+Get-WinADSiteConnections -Verbose | Format-Table -Autosize
+# Syncing changes so that those spread around quickly
+Sync-DomainController
+# Verify sync
+Get-WinADForestReplicationPartnerMetaData
---
+++ Examples/Example-VerifyADFSHealth.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+$T = Get-WinADDFSHealth -Verbose -Domains 'ad.evotec.xyz' #-DomainControllers 'adrodc'
+$T | Format-Table -a
