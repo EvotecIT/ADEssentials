2ddd8b48ee7b53eadc57fc8c667ee3ce3d0a2de0
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-02-16 14:45:16 +0100

    Updates

--- Examples/Example-SettingDiagnostics.ps1
+++ Examples/Example-SettingDiagnostics.ps1
@@ -1,4 +1,3 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
-Import-Module 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSSharedGoods\PSSharedGoods.psd1' -Force
 
 Set-WinADDiagnostics -Level Basic -Diagnostics 'LDAP Interface Events' -Verbose
