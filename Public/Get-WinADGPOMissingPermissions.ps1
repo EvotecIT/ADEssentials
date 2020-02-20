689433fa7a1ab7d29c8fa19521e82271ac6901c0
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-02-20 20:22:09 +0100

    Update

--- Public/Get-WinADGPOMissingPermissions.ps1
+++ Public/Get-WinADGPOMissingPermissions.ps1
@@ -23,7 +23,8 @@
         [alias('Domain', 'Domains')][string[]] $IncludeDomains,
         [switch] $SkipRODC,
         [System.Collections.IDictionary] $ExtendedForestInformation,
-        [validateset('AuthenticatedUsers', 'DomainComputers', 'Either')][string] $Mode = 'Either'
+        [validateset('AuthenticatedUsers', 'DomainComputers', 'Either')][string] $Mode = 'Either',
+        [switch] $Extended
     )
     if (-not $ExtendedForestInformation) {
         $ForestInformation = Get-WinADForestDetails -Forest $Forest -IncludeDomains $IncludeDomains -ExcludeDomains $ExcludeDomains -ExcludeDomainControllers $ExcludeDomainControllers -IncludeDomainControllers $IncludeDomainControllers -SkipRODC:$SkipRODC
@@ -31,16 +32,42 @@
         $ForestInformation = $ExtendedForestInformation
     }
     foreach ($Domain in $ForestInformation.Domains) {
+
         $QueryServer = $ForestInformation['QueryServers']["$Domain"].HostName[0]
-        $GPOs = Get-GPO -All -Domain $Domain -Server $QueryServer
+        if ($Extended) {
+            $GPOs = Get-GPO -All -Domain $Domain -Server $QueryServer | ForEach-Object {
+                [xml] $XMLContent = Get-GPOReport -ID $_.ID.Guid -ReportType XML -Server $ForestInformation.QueryServers[$Domain].HostName[0] -Domain $Domain
+                Add-Member -InputObject $_ -MemberType NoteProperty -Name 'LinksTo' -Value $XMLContent.GPO.LinksTo
+                if ($XMLContent.GPO.LinksTo) {
+                    Add-Member -InputObject $_ -MemberType NoteProperty -Name 'Linked' -Value $true
+                } else {
+                    Add-Member -InputObject $_ -MemberType NoteProperty -Name 'Linked' -Value $false
+                }
+                $_ | Select-Object -Property Id, DisplayName, DomainName, Owner, Linked, GpoStatus, CreationTime, ModificationTime,
+                @{
+                    label      = 'UserVersion'
+                    expression = { "AD Version: $($_.User.DSVersion), SysVol Version: $($_.User.SysvolVersion)" }
+                },
+                @{
+                    label      = 'ComputerVersion'
+                    expression = { "AD Version: $($_.Computer.DSVersion), SysVol Version: $($_.Computer.SysvolVersion)" }
+                }, WmiFilter, Description, User, Computer, LinksTo
+            }
+        } else {
+            $GPOs = Get-GPO -All -Domain $Domain -Server $QueryServer
+        }
+
+        $DomainInformation = Get-ADDomain -Server $QueryServer
+        $DomainComputersSID = $('{0}-515' -f $DomainInformation.DomainSID.Value)
+
         $MissingPermissions = @(
             foreach ($GPO in $GPOs) {
                 $Permissions = Get-GPPermission -Guid $GPO.Id -All -Server $QueryServer -DomainName $Domain | Select-Object -ExpandProperty Trustee
                 if ($Mode -eq 'Either' -or $Mode -eq 'AuthenticatedUsers') {
-                    $GPOPermissionForAuthUsers = $Permissions | Where-Object { $_.Name -eq "Authenticated Users" }
+                    $GPOPermissionForAuthUsers = $Permissions | Where-Object { $_.Sid.Value -eq 'S-1-5-11' }
                 }
                 if ($Mode -eq 'Either' -or $Mode -eq 'DomainComputers') {
-                    $GPOPermissionForDomainComputers = $Permissions | Where-Object { $_.Name -eq "Domain Computers" }
+                    $GPOPermissionForDomainComputers = $Permissions | Where-Object { $_.Sid.Value -eq $DomainComputersSID }
                 }
                 if ($Mode -eq 'Either') {
                     If (-not $GPOPermissionForAuthUsers -and -not $GPOPermissionForDomainComputers) {
