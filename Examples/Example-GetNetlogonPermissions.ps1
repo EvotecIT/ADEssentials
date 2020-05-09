2d6df196ed8a3d2e601727182fc5dc8ef7e5a63d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-09 21:35:24 +0200

    Update

--- Examples/Example-GetNetlogonPermissions.ps1
+++ Examples/Example-GetNetlogonPermissions.ps1
@@ -1,3 +1,10 @@
 ﻿Import-Module $PSScriptRoot\..\ADEssentials.psd1 -Force
 
-Get-WinADShare -ShareType NetLogon | Format-Table
+#Get-WinADShare -ShareType SYSVOL | Format-Table
+
+#Get-WinADShare -Path '\\ad.evotec.xyz\SYSVOL\ad.evotec.xyz\Policies\' -Owner | ft
+
+Get-WinADShare -Path '\\ad.evotec.xyz\SYSVOL\ad.evotec.xyz\Policies\{64AD41CA-BF07-4DB3-BFC0-20F9999ADAD6}' -Owner | Format-Table
+
+
+Get-GPOZaurr -OwnerOnly -GPOGuid '64AD41CA-BF07-4DB3-BFC0-20F9999ADAD6' | Format-Table
