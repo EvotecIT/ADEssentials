08f141c83e2e75f315f360233b6fc90c0458ac0c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-05-17 20:29:12 +0200

    Examples cleaning

--- Examples/Example-FixNetlogonPermissions.ps1
+++
@@ -1,5 +0,0 @@
-﻿Import-Module $PSScriptRoot\..\ADEssentials.psd1 -Force
-Clear-Host
-Remove-WinADSharePermission -Path '\\ad.evotec.xyz\SYSVOL\ad.evotec.xyz\scripts\' -Verbose #-LimitProcessing 1 #-WhatIf | Format-Table
-
-#Get-WinADSharePermission -Path '\\ad.evotec.xyz\SYSVOL\ad.evotec.xyz\scripts\' | Out-HtmlView -ScrollX -Filtering -DisablePaging
