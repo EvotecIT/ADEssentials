9118ac571e850e5c9beece050860f313185e3a56
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-06-24 20:41:16 +0200

    Update before release

--- Public/Get-WinADForestSchemaProperties.ps1
+++ Public/Get-WinADForestSchemaProperties.ps1
@@ -5,7 +5,26 @@
         [validateSet('Computers', 'Users')][string[]] $Schema = @('Computers', 'Users'),
         [System.Collections.IDictionary] $ExtendedForestInformation
     )
-
+    <#
+    Name                   : dLMemRejectPermsBL
+    CommonName             : ms-Exch-DL-Mem-Reject-Perms-BL
+    Oid                    : 1.2.840.113556.1.2.293
+    Syntax                 : DN
+    Description            :
+    IsSingleValued         : False
+    IsIndexed              : False
+    IsIndexedOverContainer : False
+    IsInAnr                : False
+    IsOnTombstonedObject   : False
+    IsTupleIndexed         : False
+    IsInGlobalCatalog      : True
+    RangeLower             :
+    RangeUpper             :
+    IsDefunct              : False
+    Link                   : dLMemRejectPerms
+    LinkId                 : 117
+    SchemaGuid             : a8df73c3-c5ea-11d1-bbcb-0080c76670c0
+    #>
     $ForestInformation = Get-WinADForestDetails -Forest $Forest -ExtendedForestInformation $ExtendedForestInformation
     if ($Forest) {
         $Type = [System.DirectoryServices.ActiveDirectory.DirectoryContextType]::Forest
@@ -15,15 +34,14 @@
         $CurrentSchema = [directoryservices.activedirectory.activedirectoryschema]::GetCurrentSchema()
     }
     if ($Schema -contains 'Computers') {
-        @(
-            $CurrentSchema.FindClass("computer").mandatoryproperties | Select-Object -Property name, commonname, description, syntax
-            $CurrentSchema.FindClass("computer").optionalproperties | Select-Object -Property name, commonname, description, syntax
-        )
+        $CurrentSchema.FindClass("computer").mandatoryproperties | Select-Object -Property name, commonname, description, syntax , SchemaGuid
+        $CurrentSchema.FindClass("computer").optionalproperties | Select-Object -Property name, commonname, description, syntax, SchemaGuid
+
     }
     if ($Schema -contains 'Users') {
-        @(
-            $CurrentSchema.FindClass("user").mandatoryproperties | Select-Object -Property name, commonname, description, syntax
-            $CurrentSchema.FindClass("user").optionalproperties | Select-Object -Property name, commonname, description, syntax
-        )
+        $CurrentSchema.FindClass("user").mandatoryproperties | Select-Object -Property name, commonname, description, syntax, SchemaGuid
+        $CurrentSchema.FindClass("user").optionalproperties | Select-Object -Property name, commonname, description, syntax, SchemaGuid
     }
 }
+
+#Get-WinADForestSchemaProperties | Where-Object { $_.Name -eq 'ms-Mcs-Admpwd' }
