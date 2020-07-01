00f1dd637e33907d3056f6ac6919f5692b8af3fa
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-01 21:44:29 +0200

    Version 2

--- Public/Get-WinADGroupMember.ps1
+++ Public/Get-WinADGroupMember.ps1
@@ -55,8 +55,25 @@
         [Parameter(DontShow)][System.Collections.Generic.List[object]] $CollectedGroups,
         [Parameter(DontShow)][System.Object] $Circular,
         [Parameter(DontShow)][string] $InitialGroupName,
-        [Parameter(DontShow)][switch] $Nested
+        [Parameter(DontShow)][switch] $Nested,
+        [switch] $Cache,
+        [switch] $ClearCache
     )
+    Begin {
+        if (-not $Script:WinADGroupMemberCache -or $ClearCache) {
+            $Script:WinADGroupMemberCache = @{}
+            if ($Cache) {
+                $ADObjects = @(
+                    Get-ADGroup -Filter * -Properties MemberOf, Members
+                    Get-ADComputer -Filter * -Properties Enabled, DisplayName
+                    Get-ADUser -Filter * -Properties Enabled, DisplayName
+                )
+                foreach ($Object in $ADObjects) {
+                    $Script:WinADGroupMemberCache[$Object.DistinguishedName] = $Object
+                }
+            }
+        }
+    }
     Process {
         foreach ($GroupName in $Group) {
             $Nesting++
@@ -64,10 +81,24 @@
                 $InitialGroupName = $GroupName
                 $CollectedGroups = [System.Collections.Generic.List[object]]::new()
             }
-            $ADGroupName = Get-ADGroup $GroupName -Properties MemberOf, Members
+            if ($GroupName -is [string]) {
+                $ADGroupName = Get-ADGroup -Identity $GroupName -Properties MemberOf, Members
+                $Script:WinADGroupMemberCache[$ADGroupName.DistinguishedName] = $ADGroupName
+            } elseif ($GroupName -is [Microsoft.ActiveDirectory.Management.ADPrincipal]) {
+                if ($Script:WinADGroupMemberCache[$GroupName.DistinguishedName]) {
+                    $ADGroupName = $Script:WinADGroupMemberCache[$GroupName.DistinguishedName]
+                } else {
+                    $ADGroupName = Get-ADGroup -Identity $GroupName -Properties MemberOf, Members
+                    $Script:WinADGroupMemberCache[$ADGroupName.DistinguishedName] = $ADGroupName
+                }
+            } else {
+                # shouldn't happen, but maybe...
+                $ADGroupName = Get-ADGroup -Identity $GroupName -Properties MemberOf, Members
+                $Script:WinADGroupMemberCache[$ADGroupName.DistinguishedName] = $ADGroupName
+            }
             if ($ADGroupName) {
                 if ($Circular) {
-                    $NestedMembers = Get-ADGroupMember -Identity $GroupName
+                    $NestedMembers = Get-ADGroupMember -Identity $ADGroupName
                     $NestedMembers = foreach ($Member in $NestedMembers) {
                         if ($CollectedGroups -notcontains $Member.DistinguishedName) {
                             $Member
@@ -75,7 +106,7 @@
                     }
                     $Circular = $null
                 } else {
-                    $NestedMembers = Get-ADGroupMember -Identity $GroupName
+                    $NestedMembers = Get-ADGroupMember -Identity $ADGroupName
                     if (-not $NestedMembers) {
                         if ($ADGroupName.Members) {
                             $NestedMembers = foreach ($Member in $ADGroupName.Members) {
@@ -86,6 +117,7 @@
                     }
                 }
                 foreach ($NestedMember in $NestedMembers) {
+                    <#
                     $CreatedObject = [ordered] @{
                         GroupName      = $InitialGroupName
                         Type           = $NestedMember.objectclass
@@ -98,14 +130,51 @@
                         DN             = $NestedMember.DistinguishedName
                         Circular       = $false
                     }
+                    #>
+
+                    $CreatedObject = [ordered] @{
+                        GroupName      = $InitialGroupName
+                        Type           = $null
+                        Name           = $null
+                        SamAccountName = $null
+                        DisplayName    = $null
+                        ParentGroup    = $ADGroupName.name
+                        Enabled        = $null
+                        Nesting        = $Nesting
+                        DN             = $null
+                        Circular       = $false
+                    }
                     if ($NestedMember.objectclass -eq "user") {
-                        $NestedADMember = Get-ADUser -Identity $NestedMember -Properties Enabled, DisplayName
+                        if ($Script:WinADGroupMemberCache[$NestedMember.DistinguishedName]) {
+                            $NestedADMember = $Script:WinADGroupMemberCache[$NestedMember.DistinguishedName]
+                        } else {
+                            $NestedADMember = Get-ADUser -Identity $NestedMember -Properties Enabled, DisplayName
+                            $Script:WinADGroupMemberCache[$NestedADMember.DistinguishedName] = $NestedADMember
+                        }
+                        #
+                        $CreatedObject['Type'] = $NestedADMember.objectclass
+                        $CreatedObject['Name'] = $NestedADMember.name
+                        $CreatedObject['SamAccountName'] = $NestedADMember.SamAccountName
+                        $CreatedObject['DisplayName'] = $NestedADMember.DisplayName
+                        $CreatedObject['DN'] = $NestedADMember.DistinguishedName
+                        ### old
                         $CreatedObject['Enabled'] = $NestedADMember.Enabled
                         $CreatedObject['Name'] = $NestedADMember.Name
                         $CreatedObject['DisplayName'] = $NestedADMember.DisplayName
                         [PSCustomObject] $CreatedObject
                     } elseif ($NestedMember.objectclass -eq "computer") {
-                        $NestedADMember = Get-ADComputer -Identity $NestedMember -Properties Enabled, DisplayName
+                        if ($Script:WinADGroupMemberCache[$NestedMember.DistinguishedName]) {
+                            $NestedADMember = $Script:WinADGroupMemberCache[$NestedMember.DistinguishedName]
+                        } else {
+                            $NestedADMember = Get-ADComputer -Identity $NestedMember -Properties Enabled, DisplayName
+                            $Script:WinADGroupMemberCache[$NestedADMember.DistinguishedName] = $NestedADMember
+                        }
+                        $CreatedObject['Type'] = $NestedADMember.objectclass
+                        $CreatedObject['Name'] = $NestedADMember.name
+                        $CreatedObject['SamAccountName'] = $NestedADMember.SamAccountName
+                        $CreatedObject['DisplayName'] = $NestedADMember.DisplayName
+                        $CreatedObject['DN'] = $NestedADMember.DistinguishedName
+                        ###
                         $CreatedObject['Enabled'] = $NestedADMember.Enabled
                         $CreatedObject['Name'] = $NestedADMember.Name
                         $CreatedObject['DisplayName'] = $NestedADMember.DisplayName
@@ -116,8 +185,15 @@
                             $Circular = $ADGroupName.DistinguishedName
                             $CreatedObject['Circular'] = $true
                         }
+                        $CreatedObject['Type'] = $NestedMember.objectclass
+                        $CreatedObject['Name'] = $NestedMember.name
+                        $CreatedObject['SamAccountName'] = $NestedMember.SamAccountName
+                        $CreatedObject['DisplayName'] = $NestedMember.DisplayName
+                        $CreatedObject['DN'] = $NestedMember.DistinguishedName
+
                         [PSCustomObject] $CreatedObject
                         $CollectedGroups.Add($ADGroupName.DistinguishedName)
+
                         Get-WinADGroupMember -GroupName $NestedMember -Nesting $Nesting -Circular $Circular -InitialGroupName $InitialGroupName -CollectedGroups $CollectedGroups -Nested
                     } else {
                         [PSCustomObject] $CreatedObject
@@ -128,4 +204,61 @@
     }
 }
 
-#Get-WinADGroupMember -Group 'Test Local Group', 'GDS-TestGroup5' | Format-Table *
+#Measure-Command {
+#Get-WinADGroupMember -Group 'Test Local Group', 'GDS-TestGroup5' -Cache | Format-Table
+#}
+
+<# 'Test Local Group', 'GDS-TestGroup5'
+Days              : 0
+Hours             : 0
+Minutes           : 0
+Seconds           : 2
+Milliseconds      : 120
+Ticks             : 21206866
+TotalDays         : 2,45449837962963E-05
+TotalHours        : 0,000589079611111111
+TotalMinutes      : 0,0353447766666667
+TotalSeconds      : 2,1206866
+TotalMilliseconds : 2120,6866
+
+Days              : 0
+Hours             : 0
+Minutes           : 0
+Seconds           : 1
+Milliseconds      : 442
+Ticks             : 14425890
+TotalDays         : 1,66966319444444E-05
+TotalHours        : 0,000400719166666667
+TotalMinutes      : 0,02404315
+TotalSeconds      : 1,442589
+TotalMilliseconds : 1442,589
+#>
+
+<# ITR01_AD Admins
+Days              : 0
+Hours             : 0
+Minutes           : 0
+Seconds           : 3
+Milliseconds      : 196
+Ticks             : 31967993
+TotalDays         : 3,69999918981481E-05
+TotalHours        : 0,000887999805555556
+TotalMinutes      : 0,0532799883333333
+TotalSeconds      : 3,1967993
+TotalMilliseconds : 3196,7993
+#>
+
+<# ITR01_AD Operators
+
+Days              : 0
+Hours             : 0
+Minutes           : 0
+Seconds           : 11
+Milliseconds      : 639
+Ticks             : 116397083
+TotalDays         : 0,000134718846064815
+TotalHours        : 0,00323325230555556
+TotalMinutes      : 0,193995138333333
+TotalSeconds      : 11,6397083
+TotalMilliseconds : 11639,7083
+#>
