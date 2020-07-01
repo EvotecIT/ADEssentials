1c16b5d8a00b8c69d4e964e61364708e4dbc75d3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-01 19:31:02 +0200

    Added Get/Remove-WinADDuplicateObject

---
+++ Public/Get-WinADDuplicateObject.ps1
@@ -0,0 +1,14 @@
+﻿function Get-WinADDuplicateObject {
+    [cmdletBinding()]
+    param(
+        [alias('ForestName')][string] $Forest,
+        [string[]] $ExcludeDomains,
+        [alias('Domain', 'Domains')][string[]] $IncludeDomains,
+        [System.Collections.IDictionary] $ExtendedForestInformation
+    )
+    $ForestInformation = Get-WinADForestDetails -Forest $Forest -IncludeDomains $IncludeDomains -ExcludeDomains $ExcludeDomains -ExtendedForestInformation $ExtendedForestInformation
+    foreach ($Domain in $ForestInformation.Domains) {
+        $QueryServer = $ForestInformation['QueryServers']["$Domain"].HostName[0]
+        Get-ADObject -LDAPFilter "(|(cn=*\0ACNF:*)(ou=*OACNF:*))" -SearchScope Subtree -Server $QueryServer
+    }
+}
