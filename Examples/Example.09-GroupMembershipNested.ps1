671ec81778bac3fd23abdf35b671c842f1b3fd71
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-02 22:33:28 +0200

    Update example

--- Examples/Example.09-GroupMembershipNested.ps1
+++ Examples/Example.09-GroupMembershipNested.ps1
@@ -7,3 +7,6 @@ Get-WinADGroupMember -Group 'Test Local Group', 'GDS-TestGroup5' -Cache | Format
 $Groups = Get-ADGroup -Filter *
 $AllGroups = $Groups | Get-WinADGroupMember -Cache
 $AllGroups | Out-HtmlView -ScrollX -DisablePaging -Filtering
+
+# Please notice we are using cache parameter, however that can slow things down if you're not going to ask for whole domain
+# It's often better to skip -Cache and still the script would keep cache on per query basis
