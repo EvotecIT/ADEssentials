8a7d623c3919cf73682de10e231ef9fff165aa95
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-02 22:29:14 +0200

    Updates

---
+++ Examples/Example.09-GroupMembershipNested.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+# Lets check only two groups
+Get-WinADGroupMember -Group 'Test Local Group', 'GDS-TestGroup5' -Cache | Format-Table *
+
+# How about all groups
+$Groups = Get-ADGroup -Filter *
+$AllGroups = $Groups | Get-WinADGroupMember
+$AllGroups | Out-HtmlView -ScrollX -DisablePaging -Filtering
