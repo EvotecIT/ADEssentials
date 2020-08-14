adf27528ff5227a739826a8bfcf4f1dddccdc8b7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-14 22:36:55 +0200

    Update

--- Examples/Example.11-RepairEmailAddress.ps1
+++ Examples/Example.11-RepairEmailAddress.ps1
@@ -8,4 +8,4 @@ $User = Get-ADUser 'testing1' -Properties ProxyAddresses, EmailAddress
 # You can optionally verify how it looks like or what is set
 #Get-WinADProxyAddresses -ADUser $User | Format-Table
 # Fix primary email address - it will be added to primary email field + proxy address as primary, existing addresses will be rearanged
-Repair-WinADEmailAddress -ADUser $User -ToEmail 'testing2@evotec.xyz' -Display -WhatIf | Format-Table
+Repair-WinADEmailAddress -ADUser $User -ToEmail 'testing2@evotec.be' -Display -WhatIf | Format-Table
