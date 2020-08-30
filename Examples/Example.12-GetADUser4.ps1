b0ec07c100347d1069f1aead22540b43f1ca2850
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-30 19:13:19 +0200

    Update examples

---
+++ Examples/Example.12-GetADUser4.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module $PSScriptRoot\..\ADEssentials.psd1 -Force
+
+# This is very slow, we need to improve
+@(
+    Get-WinADObject -Identity 'NT AUTHORITY\INTERACTIVE'
+    Get-WinADObject -Identity 'INTERACTIVE'
+    Get-WinADObject -Identity 'NT AUTHORITY\IUSR'
+    Get-WinADObject -Identity 'NT AUTHORITY\ENTERPRISE DOMAIN CONTROLLERS'
+    Get-WinADObject -Identity 'S-1-5-4'
+    Get-WinADObject -Identity 'S-1-5-11'
+    Get-WinADObject -Identity 'EVOTEC\Domain Admins'
+) | Format-Table *
+
+return
+
+@(
+    Convert-Identity -Identity 'NT AUTHORITY\INTERACTIVE'
+    Convert-Identity -Identity 'INTERACTIVE'
+    Convert-Identity -Identity 'EVOTEC\Domain Admins'
+    Convert-Identity -Identity 'EVOTECPL\Domain Admins'
+) | Format-Table
