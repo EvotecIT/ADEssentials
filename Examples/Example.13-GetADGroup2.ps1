4e38310d9ad5851cb29143c12ddb8e37747b42bb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-30 19:13:19 +0200

    Update examples

---
+++ Examples/Example.13-GetADGroup2.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+Get-WinADGroupMember -Group 'CN=Users,CN=Builtin,DC=ad,DC=evotec,DC=xyz' | Format-Table *
+Get-WinADGroupMember -Group 'Test-Group' | Format-Table *
