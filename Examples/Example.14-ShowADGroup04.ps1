29ac6b4e8ac3f96abb2fb5c032e68a5156483f3d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-31 16:22:31 +0200

    Update

---
+++ Examples/Example.14-ShowADGroup04.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+#Show-ADGroupMember -GroupName 'Test-Group', 'Domain Admins','Enterprise Admins', 'Administrators' -FilePath $PSScriptRoot\Reports\GroupMembership.html -Summary
+
+Show-ADGroupMember -GroupName 'Test-Group', 'Domain Admins','Enterprise Admins', 'Administrators' -FilePath $PSScriptRoot\Reports\GroupMembership.html -SummaryOnly
