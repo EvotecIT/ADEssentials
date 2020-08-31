05c1bdc820bf74ba3c446c599397a27fcd388e93
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-31 20:59:04 +0200

    Update

---
+++ Examples/Example.15-GetADObjectMember01.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+Get-WinADObjectMember -Identity 'przemyslaw.klys' -AddSelf | Format-Table *
+Get-WinADObjectMember -Identity 'adm.pklys' -AddSelf | Format-Table *
--- Public/Show-WinADObjectMember.ps1
+++ Public/Show-WinADObjectMember.ps1
@@ -7,37 +7,37 @@
         [ValidateSet('Default', 'Hierarchical', 'Both')][string] $RemoveAppliesTo = 'Both',
         [switch] $RemoveComputers,
         [switch] $RemoveUsers,
-        [switch] $RemoveOther,
-        [Parameter(ParameterSetName = 'Default')][switch] $Summary,
-        [Parameter(ParameterSetName = 'SummaryOnly')][switch] $SummaryOnly
+        [switch] $RemoveOther
+        #[Parameter(ParameterSetName = 'Default')][switch] $Summary,
+        #[Parameter(ParameterSetName = 'SummaryOnly')][switch] $SummaryOnly
     )
     if ($FilePath -eq '') {
         $FilePath = Get-FileName -Extension 'html' -Temporary
     }
-    $GroupsList = [System.Collections.Generic.List[object]]::new()
-    New-HTML -TitleText "Group Membership for $GroupName" {
+    #$GroupsList = [System.Collections.Generic.List[object]]::new()
+    New-HTML -TitleText "Visual Object MemberOf" {
         New-HTMLSectionStyle -BorderRadius 0px -HeaderBackGroundColor Grey -RemoveShadow
         New-HTMLTableOption -DataStore JavaScript
         New-HTMLTabStyle -BorderRadius 0px -TextTransform capitalize -BackgroundColorActive SlateGrey
         foreach ($ADObject in $Identity) {
             try {
                 $MyObject = Get-WinADObjectMember -Identity $ADObject -AddSelf
-                if ($Summary -or $SummaryOnly) {
-                    foreach ($Object in $MyObject) {
-                        $GroupsList.Add($Object)
-                    }
-                }
+                # if ($Summary -or $SummaryOnly) {
+                #     foreach ($Object in $MyObject) {
+                #         $GroupsList.Add($Object)
+                #    }
+                # }
             } catch {
-                Write-Warning "Show-GroupMember - Error processing group $Group. Skipping. Needs investigation why it failed. Error: $($_.Exception.Message)"
+                Write-Warning "Show-WinADObjectMember - Error processing group $Group. Skipping. Needs investigation why it failed. Error: $($_.Exception.Message)"
                 continue
             }
             if ($MyObject -and -not $SummaryOnly) {
-                $GroupName = $MyObject[0].GroupName
+                $ObjectName = $MyObject[0].ObjectName
                 $DataStoreID = -join ('table', (Get-RandomStringName -Size 10 -ToLower))
                 $DataTableID = -join ('table', (Get-RandomStringName -Size 10 -ToLower))
-                New-HTMLTab -TabName $GroupName {
+                New-HTMLTab -TabName $ObjectName {
                     New-HTMLTab -TabName 'Information' {
-                        New-HTMLSection -Title "Information for $GroupName" {
+                        New-HTMLSection -Title "Information for $ObjectName" {
                             New-HTMLTable -DataTable $MyObject -Filtering -DataStoreID $DataStoreID {
                                 New-TableHeader -Names Name, SamAccountName, DomainName, DisplayName -Title 'Member'
                                 # New-TableHeader -Names DirectMembers, DirectGroups, IndirectMembers, TotalMembers -Title 'Statistics'
@@ -49,7 +49,7 @@
                         }
                     }
                     New-HTMLTab -TabName 'Diagram Basic' {
-                        New-HTMLSection -Title "Diagram for $GroupName" {
+                        New-HTMLSection -Title "Diagram for $ObjectName" {
                             New-HTMLObjectDiagramDefault -Identity $MyObject -RemoveAppliesTo $RemoveAppliesTo -RemoveUsers:$RemoveUsers -RemoveComputers:$RemoveComputeres -RemoveOther:$RemoveOther -DataTableID $DataTableID -ColumnID 1
                         }
                         #New-HTMLSection -Title "Group membership table $GroupName" {
@@ -57,7 +57,7 @@
                         #}
                     }
                     New-HTMLTab -TabName 'Diagram Hierarchy' {
-                        New-HTMLSection -Title "Diagram for $GroupName" {
+                        New-HTMLSection -Title "Diagram for $ObjectName" {
                             New-HTMLObjectDiagramHierachical -Identity $MyObject -RemoveAppliesTo $RemoveAppliesTo -RemoveUsers:$RemoveUsers -RemoveComputers:$RemoveComputeres -RemoveOther:$RemoveOther
                         }
                         #New-HTMLSection -Title "Group membership table $GroupName" {
@@ -67,6 +67,7 @@
                 }
             }
         }
+        <#
         if ($Summary -or $SummaryOnly) {
             New-HTMLTab -Name 'Summary' {
                 New-HTMLTab -TabName 'Diagram Basic' {
@@ -81,5 +82,6 @@
                 }
             }
         }
+        #>
     } -Online -FilePath $FilePath -ShowHTML
 }
