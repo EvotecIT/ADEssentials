07e88a6a9756f064baf7cdc7370458a8462c788a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-31 13:56:19 +0200

    Update

---
+++ Examples/Example.14-ShowADGroup02.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+Show-ADGroupMember -GroupName 'Domain Admins', 'Enterprise Admins', 'Administrators', 'Account Operators', 'Backup Operators' -FilePath $PSScriptRoot\Reports\GroupMembership.html -RemoveUsers
+Show-ADGroupMember -GroupName 'Domain Admins' -FilePath $PSScriptRoot\Reports\GroupMembership.html #-RemoveUser
