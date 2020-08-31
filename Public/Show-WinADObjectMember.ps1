b2698c4d0ac0e9f307a4f4ad5f6043fbce35d8d4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-31 19:09:28 +0200

    Update

---
+++ Public/Show-WinADObjectMember.ps1
@@ -0,0 +1,85 @@
+﻿function Show-WinADObjectMember {
+    [alias('Show-ADObjectMember')]
+    [cmdletBinding(DefaultParameterSetName = 'Default')]
+    param(
+        [string[]] $Identity,
+        [string] $FilePath,
+        [ValidateSet('Default', 'Hierarchical', 'Both')][string] $RemoveAppliesTo = 'Both',
+        [switch] $RemoveComputers,
+        [switch] $RemoveUsers,
+        [switch] $RemoveOther,
+        [Parameter(ParameterSetName = 'Default')][switch] $Summary,
+        [Parameter(ParameterSetName = 'SummaryOnly')][switch] $SummaryOnly
+    )
+    if ($FilePath -eq '') {
+        $FilePath = Get-FileName -Extension 'html' -Temporary
+    }
+    $GroupsList = [System.Collections.Generic.List[object]]::new()
+    New-HTML -TitleText "Group Membership for $GroupName" {
+        New-HTMLSectionStyle -BorderRadius 0px -HeaderBackGroundColor Grey -RemoveShadow
+        New-HTMLTableOption -DataStore JavaScript
+        New-HTMLTabStyle -BorderRadius 0px -TextTransform capitalize -BackgroundColorActive SlateGrey
+        foreach ($ADObject in $Identity) {
+            try {
+                $MyObject = Get-WinADObjectMember -Identity $ADObject -AddSelf
+                if ($Summary -or $SummaryOnly) {
+                    foreach ($Object in $MyObject) {
+                        $GroupsList.Add($Object)
+                    }
+                }
+            } catch {
+                Write-Warning "Show-GroupMember - Error processing group $Group. Skipping. Needs investigation why it failed. Error: $($_.Exception.Message)"
+                continue
+            }
+            if ($MyObject -and -not $SummaryOnly) {
+                $GroupName = $MyObject[0].GroupName
+                $DataStoreID = -join ('table', (Get-RandomStringName -Size 10 -ToLower))
+                $DataTableID = -join ('table', (Get-RandomStringName -Size 10 -ToLower))
+                New-HTMLTab -TabName $GroupName {
+                    New-HTMLTab -TabName 'Information' {
+                        New-HTMLSection -Title "Information for $GroupName" {
+                            New-HTMLTable -DataTable $MyObject -Filtering -DataStoreID $DataStoreID {
+                                New-TableHeader -Names Name, SamAccountName, DomainName, DisplayName -Title 'Member'
+                                # New-TableHeader -Names DirectMembers, DirectGroups, IndirectMembers, TotalMembers -Title 'Statistics'
+                                New-TableCondition -BackgroundColor CoralRed -ComparisonType bool -Value $false -Name Enabled -Operator eq
+                                New-TableCondition -BackgroundColor LightBlue -ComparisonType string -Value '' -Name ParentGroup -Operator eq -Row
+                                New-TableCondition -BackgroundColor CoralRed -ComparisonType bool -Value $true -Name CrossForest -Operator eq
+                                New-TableCondition -BackgroundColor CoralRed -ComparisonType bool -Value $true -Name Circular -Operator eq
+                            }
+                        }
+                    }
+                    New-HTMLTab -TabName 'Diagram Basic' {
+                        New-HTMLSection -Title "Diagram for $GroupName" {
+                            New-HTMLObjectDiagramDefault -Identity $MyObject -RemoveAppliesTo $RemoveAppliesTo -RemoveUsers:$RemoveUsers -RemoveComputers:$RemoveComputeres -RemoveOther:$RemoveOther -DataTableID $DataTableID -ColumnID 1
+                        }
+                        #New-HTMLSection -Title "Group membership table $GroupName" {
+                        #    New-HTMLTable -DataTable $ADGroup -Filtering -DataStoreID $DataStoreID -DataTableID $DataTableID
+                        #}
+                    }
+                    New-HTMLTab -TabName 'Diagram Hierarchy' {
+                        New-HTMLSection -Title "Diagram for $GroupName" {
+                            New-HTMLObjectDiagramHierachical -Identity $MyObject -RemoveAppliesTo $RemoveAppliesTo -RemoveUsers:$RemoveUsers -RemoveComputers:$RemoveComputeres -RemoveOther:$RemoveOther
+                        }
+                        #New-HTMLSection -Title "Group membership table $GroupName" {
+                        #    New-HTMLTable -DataTable $ADGroup -Filtering -DataStoreID $DataStoreID
+                        #}
+                    }
+                }
+            }
+        }
+        if ($Summary -or $SummaryOnly) {
+            New-HTMLTab -Name 'Summary' {
+                New-HTMLTab -TabName 'Diagram Basic' {
+                    New-HTMLSection -Title "Diagram for Summary" {
+                        New-HTMLGroupDiagramSummary -ADGroup $GroupsList -RemoveAppliesTo $RemoveAppliesTo -RemoveUsers:$RemoveUsers -RemoveComputers:$RemoveComputeres -RemoveOther:$RemoveOther -DataTableID $DataTableID -ColumnID 1
+                    }
+                }
+                New-HTMLTab -TabName 'Diagram Hierarchy' {
+                    New-HTMLSection -Title "Diagram for Summary" {
+                        New-HTMLGroupDiagramSummaryHierarchical -ADGroup $GroupsList -RemoveAppliesTo $RemoveAppliesTo -RemoveUsers:$RemoveUsers -RemoveComputers:$RemoveComputeres -RemoveOther:$RemoveOther
+                    }
+                }
+            }
+        }
+    } -Online -FilePath $FilePath -ShowHTML
+}
