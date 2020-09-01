7b3f7f8257654aa9e443ff61e0575a6c79500708
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-01 21:36:21 +0200

    Update

---
+++ Examples/Example.15-GetGroupMemberOf01.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+Get-WinADGroupMemberOf -Identity 'przemyslaw.klys' -AddSelf | Format-Table *
+Get-WinADGroupMemberOf -Identity 'adm.pklys' -AddSelf | Format-Table *
--- Examples/Example.16-ShowADObject.ps1
+++
@@ -1,3 +0,0 @@
-﻿Import-Module .\ADEssentials.psd1 -Force
-
-Show-WinADObjectMember -Identity 'przemyslaw.klys', 'adm.pklys'
