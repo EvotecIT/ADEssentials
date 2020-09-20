357295f5c939c0bd8d0934e7c2548a96415a5c1d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-20 11:10:07 +0200

    Update

---
+++ Examples/Example.12-GetADComputer.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module $PSScriptRoot\..\ADEssentials.psd1 -Force
+
+$Computers = @(
+    'ADPreview2019.ad.evotec.pl'
+    'ADRODC.ad.evotec.pl'
+    'EVOWIN' # this will resolve as it's computer in same domain
+    'ADTEST' # this will not resolve as it's DC in another forest
+    'ADTEST.test.evotec.pl'
+    'adtest@test.evotec.pl'
+)
+
+$Output = Get-WinADObject -Identity $Computers -Verbose
+$Output | Format-Table *
+
+#$Ident = 'ADPreview2019.ad.evotec.pl'
+#$Ident -split '\.',2
--- Examples/Example.18-GetADACL.ps1
+++ Examples/Example.18-GetADACL.ps1
@@ -1,5 +1,8 @@
 ﻿Import-Module $PSScriptRoot\..\ADEssentials.psd1 -Force
 
-Get-ADACL -ADObject 'CN=Policies,CN=System,DC=ad,DC=evotec,DC=xyz' -IncludeActiveDirectoryRights 'GenericAll', 'CreateChild', 'WriteOwner', 'WriteDACL' -IncludeObjectTypeName All -ADRightsAsArray -ResolveTypes | Format-Table
+#Get-ADACL -ADObject 'CN=Policies,CN=System,DC=ad,DC=evotec,DC=xyz' -IncludeActiveDirectoryRights 'GenericAll', 'CreateChild', 'WriteOwner', 'WriteDACL' -IncludeObjectTypeName All -ADRightsAsArray -ResolveTypes | Format-Table
+#Get-ADACLOwner -ADObject 'CN=Policies,CN=System,DC=ad,DC=evotec,DC=xyz' -Resolve | Format-Table
 
-Get-ADACLOwner -ADObject 'CN=Policies,CN=System,DC=ad,DC=evotec,DC=xyz' -Resolve | Format-Table
+Get-ADObject -SearchBase 'CN=System,DC=ad,DC=evotec,DC=xyz' -Filter { ObjectClass -eq 'container' } | ForEach-Object {
+
+}
