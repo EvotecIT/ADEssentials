c981f39e90fc0feb71b8951ce9dba51dcf54ad30
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-21 11:25:48 +0100

    Update example

--- Examples/Example.21-RepairDC.ps1
+++ Examples/Example.21-RepairDC.ps1
@@ -2,4 +2,4 @@ Import-Module .\ADEssentials.psd1 -Force
 
 Get-WinADForestControllerInformation -Verbose | Format-Table *
 
-#Repair-WinADForestControllerInformation -Verbose -LimitProcessing 1 -Type Manager, Owner #-WhatIf
+Repair-WinADForestControllerInformation -Verbose -LimitProcessing 10 -Type Manager, Owner -WhatIf
