f01618817fe6a9fa1acd630ca64a5807152b897b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-05 20:05:02 +0100

    Update examples

---
+++ Examples/Example.23.ConfigurationOwners.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+#Get-WinADForestObjectsPermissions -ObjectType 'interSiteTransport', 'siteLink', 'wellKnownSecurityPrincipals' | Format-Table
+
+Get-WinADForestObjectsPermissions -ContainerType 'sites' -Owner | Format-Table
