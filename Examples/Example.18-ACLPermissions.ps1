657e40ba6d78ee9db6e51611ca796b328c957b0b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-17 18:54:43 +0100

    Update ACL examples

---
+++ Examples/Example.18-ACLPermissions.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module $PSScriptRoot\..\ADEssentials.psd1 -Force
+
+# With split per sheet
+$Permissions = Get-WinADACLForest -Verbose -Separate
+foreach ($Domain in $Permissions.Keys) {
+    $FilePath = "$Env:USERPROFILE\Desktop\PermissionsOutputPerSheet_$Domain.xlsx"
+    foreach ($Perm in $Permissions[$Domain].Keys) {
+        $Permissions[$Domain][$Perm] | ConvertTo-Excel -FilePath $FilePath -ExcelWorkSheetName $Perm -AutoFilter -AutoFit -FreezeTopRowFirstColumn
+    }
+}
+$Permissions | Format-Table *
+
+# With permissions in one sheet
+$FilePath = "$Env:USERPROFILE\Desktop\PermissionsOutput.xlsx"
+$PermissionsArray = Get-WinADACLForest -Verbose
+$PermissionsArray | ConvertTo-Excel -FilePath $FilePath -ExcelWorkSheetName 'Permissions' -AutoFilter -AutoFit -FreezeTopRowFirstColumn
+$PermissionsArray | Format-Table *
