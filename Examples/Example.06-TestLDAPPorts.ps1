3c19448e0adc4d50f517a56062f1262782378e78
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-17 22:14:24 +0100

    Update example

--- Examples/Example.06-TestLDAPPorts.ps1
+++ Examples/Example.06-TestLDAPPorts.ps1
@@ -1,4 +1,5 @@
 ﻿Import-Module $PSScriptRoot\..\ADEssentials.psd1 -Force
 
 #Test-LDAP -ComputerName 'AD1', 'AD2', 'DC1', 'AD3', 'ADRODC', 'ad1.ad.evotec.xyz', 'ad.evotec.xyz', '192.168.240.189' -Verify | Format-Table * #-WarningAction SilentlyContinue | Format-Table *
-Test-LDAP -ComputerName 'AD1', 'DC1' -Verify | Format-List * #-WarningAction SilentlyContinue | Format-Table *
+#Test-LDAP -ComputerName 'AD1', 'DC1' -Verify | Format-List * #-WarningAction SilentlyContinue | Format-Table *
+Test-LDAP -ComputerName 'AD1', '192.168.240.189', DC1, 'ADRODC','Mmm' -Verify | Format-Table * #-WarningAction SilentlyContinue | Format-Table *
