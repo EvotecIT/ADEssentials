e75afdafe5da2dbd3e1aff32a13dcc22442cd7b5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-17 20:27:43 +0100

    Update

--- Private/Test-LDAPPorts.ps1
+++ Private/Test-LDAPPorts.ps1
@@ -12,9 +12,9 @@
             return $true
         } catch {
             if ($_.Exception.ToString() -match "The server is not operational") {
-                Write-Warning "Can't open $ServerName`:$Port."
+                Write-Warning "Test-LDAPPorts - Can't open $ServerName`:$Port."
             } elseif ($_.Exception.ToString() -match "The user name or password is incorrect") {
-                Write-Warning "Current user ($Env:USERNAME) doesn't seem to have access to to LDAP on port $Server`:$Port"
+                Write-Warning "Test-LDAPPorts - Current user ($Env:USERNAME) doesn't seem to have access to to LDAP on port $Server`:$Port"
             } else {
                 Write-Warning -Message $_
             }
