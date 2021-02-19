40a9537bfdaf0fc7fc04d41645e346088cd1536f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-19 20:47:58 +0100

    Update LDAP example

--- Examples/Example.06-TestLDAPPorts.ps1
+++ Examples/Example.06-TestLDAPPorts.ps1
@@ -2,4 +2,6 @@
 
 #Test-LDAP -ComputerName 'AD1', 'AD2', 'DC1', 'AD3', 'ADRODC', 'ad1.ad.evotec.xyz', 'ad.evotec.xyz', '192.168.240.189' -Verify | Format-Table * #-WarningAction SilentlyContinue | Format-Table *
 #Test-LDAP -ComputerName 'AD1', 'DC1' -Verify | Format-List * #-WarningAction SilentlyContinue | Format-Table *
-Test-LDAP -ComputerName 'AD1', '192.168.240.189', DC1, 'ADRODC','Mmm' -Verify | Format-Table * #-WarningAction SilentlyContinue | Format-Table *
+#Test-LDAP -ComputerName 'AD1', '192.168.240.189', DC1, 'ADRODC','Mmm' -Verify | Format-Table * #-WarningAction SilentlyContinue | Format-Table *
+#Test-LDAP -ComputerName 'AD1' -Verify | Format-Table *
+Test-LDAP -VerifyCertificate -SkipRODC | Format-Table *
