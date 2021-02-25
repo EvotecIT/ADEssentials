28e473627293ba3650e5cbce3f7f91df6fdf2e0c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-25 13:03:09 +0100

    Get-WinADSubnet added

---
+++ Private/Get-ADSubnet.ps1
@@ -0,0 +1,36 @@
+﻿function Get-ADSubnet {
+    [cmdletBinding()]
+    param(
+        [Array] $Subnets,
+        [switch] $AsHashTable
+    )
+    foreach ($Subnet in $Subnets) {
+        if ($Subnet.SiteObject) {
+            $SiteObject = ConvertFrom-DistinguishedName -DistinguishedName $Subnet.SiteObject
+        } else {
+            $SiteObject = ''
+        }
+        $MaskBits = ([int](($Subnet.Name -split "/")[1]))
+        if ($AsHashTable) {
+            [ordered] @{
+                Name              = $Subnet.Name
+                SiteName          = $SiteObject
+                SiteStatus        = if ($SiteObject) { $true } else { $false }
+                SubnetOverLap     = $null
+                SubnetOverLapList = $null
+                Subnet            = ([IPAddress](($Subnet.Name -split "/")[0]))
+                MaskBits          = ([int](($Subnet.Name -split "/")[1]))
+                SubnetMask        = ([IPAddress]"$([system.convert]::ToInt64(("1"*$MaskBits).PadRight(32,"0"),2))")
+            }
+        } else {
+            [PSCustomObject] @{
+                Name       = $Subnet.Name
+                SiteName   = $SiteObject
+                SiteStatus = if ($SiteObject) { $true } else { $false }
+                Subnet     = ([IPAddress](($Subnet.Name -split "/")[0]))
+                MaskBits   = ([int](($Subnet.Name -split "/")[1]))
+                SubnetMask = ([IPAddress]"$([system.convert]::ToInt64(("1"*$MaskBits).PadRight(32,"0"),2))")
+            }
+        }
+    }
+}
---
+++ Private/Test-ADSubnet.ps1
@@ -0,0 +1,22 @@
+﻿function Test-ADSubnet {
+    [cmdletBinding()]
+    param(
+        [Array] $Subnets
+    )
+    foreach ($Subnet in $Subnets) {
+        $SmallSubnets = $Subnets | Where-Object { $_.MaskBits -gt $Subnet.MaskBits }
+        foreach ($SmallSubnet in $SmallSubnets ) {
+            if (($SmallSubnet.Subnet.Address -band $Subnet.SubnetMask.Address) -eq $Subnet.Subnet.Address) {
+                [PSCustomObject]@{
+                    Name                   = $Subnet.Name
+                    SiteName               = $Subnet.SiteName
+                    SiteStatus             = $Subnet.SiteStatus
+                    SubnetRange            = $Subnet.Subnet
+                    OverlappingSubnet      = $SmallSubnet.Name
+                    OverlappingSubnetRange = $SmallSubnet.Subnet
+                    SiteCollission         = $Subnet.Name -ne $SmallSubnet.Name
+                }
+            }
+        }
+    }
+}
