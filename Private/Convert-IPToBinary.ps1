234b145c39d4fa754303f322e916ec143c8f20dc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-25 18:27:40 +0100

    Added converters

---
+++ Private/Convert-IPToBinary.ps1
@@ -0,0 +1,19 @@
+﻿function Convert-IPToBinary {
+    [cmdletBinding()]
+    param(
+        [string] $IP
+    )
+    $IPv4Regex = '(?:(?:0?0?\d|0?[1-9]\d|1\d\d|2[0-5][0-5]|2[0-4]\d)\.){3}(?:0?0?\d|0?[1-9]\d|1\d\d|2[0-5][0-5]|2[0-4]\d)'
+    $IP = $IP.Trim()
+    if ($IP -match "\A${IPv4Regex}\z") {
+        try {
+            return ($IP.Split('.') | ForEach-Object { [System.Convert]::ToString([byte] $_, 2).PadLeft(8, '0') }) -join ''
+        } catch {
+            Write-Warning -Message "Error converting '$IP' to a binary string: $_"
+            return $Null
+        }
+    } else {
+        Write-Warning -Message "Invalid IP detected: '$IP'."
+        return $Null
+    }
+}
