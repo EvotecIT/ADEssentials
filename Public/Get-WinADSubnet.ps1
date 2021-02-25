11cee61da3cb1420a9cacafbdfccf241ecdf8142
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-25 13:03:09 +0100

    Get-WinADSubnet added

---
+++ Public/Get-WinADSubnet.ps1
@@ -0,0 +1,50 @@
+﻿function Get-WinADSubnet {
+    [cmdletBinding()]
+    param(
+        [string] $Forest,
+        [System.Collections.IDictionary] $ExtendedForestInformation,
+        [switch] $VerifyOverlap
+    )
+    $ForestInformation = Get-WinADForestDetails -Forest $Forest -ExtendedForestInformation $ExtendedForestInformation
+    $QueryServer = $ForestInformation.QueryServers[$($ForestInformation.Forest.Name)]['HostName'][0]
+    $ForestDN = ConvertTo-DistinguishedName -ToDomain -CanonicalName $ForestInformation.Forest.Name
+
+    $ADObjectSplat = @{
+        Server      = $QueryServer
+        LDAPFilter  = '(objectClass=subnet)'
+        SearchBase  = "CN=Subnets,CN=Sites,CN=Configuration,$($($ForestDN))"
+        SearchScope = 'OneLevel'
+        Properties  = 'Name', 'distinguishedName', 'CanonicalName', 'WhenCreated', 'whenchanged', 'ProtectedFromAccidentalDeletion', 'siteObject', 'location', 'objectClass', 'Description'
+    }
+    try {
+        $Subnets = Get-ADObject @ADObjectSplat -ErrorAction Stop
+    } catch {
+        Write-Warning "Get-WinADSites - LDAP Filter: $($ADObjectSplat.LDAPFilter), SearchBase: $($ADObjectSplat.SearchBase)), Error: $($_.Exception.Message)"
+    }
+
+    $Cache = @{}
+    if ($VerifyOverlap) {
+        $Subnets = Get-ADSubnet -Subnets $Subnets -AsHashTable
+        $OverlappingSubnets = Test-ADSubnet -Subnets $Subnets
+        foreach ($Subnet in $OverlappingSubnets) {
+            if (-not $Cache[$Subnet.Name]) {
+                $Cache[$Subnet.Name] = [System.Collections.Generic.List[string]]::new()
+            }
+            $Cache[$Subnet.Name].Add($Subnet.OverlappingSubnet)
+        }
+        foreach ($Subnet in $Subnets) {
+            $Subnet['SubnetOverlap'] = $false
+            if ($Cache[$Subnet.Name]) {
+                $Subnet['SubnetOverlap'] = $true
+                $Subnet['SubnetOverLapList'] = $Cache[$Subnet.Name]
+            } else {
+
+            }
+            [PSCustomObject] $Subnet
+        }
+
+
+    } else {
+        Get-ADSubnet -Subnets $Subnets
+    }
+}
