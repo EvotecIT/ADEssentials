61c3cd1b4deacc86626d0703c0aaecb75290a659
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-22 22:50:07 +0100

    Improved Get-WinADForestSubnet

--- Private/Get-ADSubnet.ps1
+++ Private/Get-ADSubnet.ps1
@@ -10,37 +10,88 @@
         } else {
             $SiteObject = ''
         }
-        $AddressRange = Get-IPAddressRange -NetworkAddress $Subnet.Name
-        $MaskBits = ([int](($Subnet.Name -split "/")[1]))
-        if ($AsHashTable) {
-            [ordered] @{
-                Name        = $Subnet.Name
-                SiteName    = $SiteObject
-                SiteStatus  = if ($SiteObject) { $true } else { $false }
-                OverLap     = $null
-                OverLapList = $null
-                Subnet      = ([IPAddress](($Subnet.Name -split "/")[0]))
-                MaskBits    = ([int](($Subnet.Name -split "/")[1]))
-                SubnetMask  = ([IPAddress]"$([system.convert]::ToInt64(("1"*$MaskBits).PadRight(32,"0"),2))")
-                TotalHosts  = $AddressRange.TotalHosts
-                UsableHosts = $AddressRange.UsableHosts
-                HostMin     = $AddressRange.HostMin
-                HostMax     = $AddressRange.HostMax
-                Broadcast   = $AddressRange.Broadcast
+        $Addr = $Subnet.Name.Split('/')
+        $Address = [PSCustomObject] @{
+            IP            = $Addr[0]
+            NetworkLength = $Addr[1]
+        }
+        try {
+            $IPAddress = ([IPAddress] $Address.IP)
+        } catch {
+            Write-Warning "Get-ADSubnet - Conversion to IP failed. Error: $($_.Exception.Message)"
+        }
+        if ($IPAddress.AddressFamily -eq 'InterNetwork') {
+            # IPv4
+            $AddressRange = Get-IPAddressRangeInformation -CIDRObject $Address
+            $MaskBits = ([int](($Subnet.Name -split "/")[1]))
+            if ($AsHashTable) {
+                [ordered] @{
+                    Name        = $Subnet.Name
+                    Type        = 'IPv4'
+                    SiteName    = $SiteObject
+                    SiteStatus  = if ($SiteObject) { $true } else { $false }
+                    OverLap     = $null
+                    OverLapList = $null
+                    Subnet      = ([IPAddress](($Subnet.Name -split "/")[0]))
+                    MaskBits    = ([int](($Subnet.Name -split "/")[1]))
+                    SubnetMask  = ([IPAddress]"$([system.convert]::ToInt64(("1"*$MaskBits).PadRight(32,"0"),2))")
+                    TotalHosts  = $AddressRange.TotalHosts
+                    UsableHosts = $AddressRange.UsableHosts
+                    HostMin     = $AddressRange.HostMin
+                    HostMax     = $AddressRange.HostMax
+                    Broadcast   = $AddressRange.Broadcast
+                }
+            } else {
+                [PSCustomObject] @{
+                    Name        = $Subnet.Name
+                    Type        = 'IPv4'
+                    SiteName    = $SiteObject
+                    SiteStatus  = if ($SiteObject) { $true } else { $false }
+                    Subnet      = ([IPAddress](($Subnet.Name -split "/")[0]))
+                    MaskBits    = ([int](($Subnet.Name -split "/")[1]))
+                    SubnetMask  = ([IPAddress]"$([system.convert]::ToInt64(("1"*$MaskBits).PadRight(32,"0"),2))")
+                    TotalHosts  = $AddressRange.TotalHosts
+                    UsableHosts = $AddressRange.UsableHosts
+                    HostMin     = $AddressRange.HostMin
+                    HostMax     = $AddressRange.HostMax
+                    Broadcast   = $AddressRange.Broadcast
+                }
             }
         } else {
-            [PSCustomObject] @{
-                Name        = $Subnet.Name
-                SiteName    = $SiteObject
-                SiteStatus  = if ($SiteObject) { $true } else { $false }
-                Subnet      = ([IPAddress](($Subnet.Name -split "/")[0]))
-                MaskBits    = ([int](($Subnet.Name -split "/")[1]))
-                SubnetMask  = ([IPAddress]"$([system.convert]::ToInt64(("1"*$MaskBits).PadRight(32,"0"),2))")
-                TotalHosts  = $AddressRange.TotalHosts
-                UsableHosts = $AddressRange.UsableHosts
-                HostMin     = $AddressRange.HostMin
-                HostMax     = $AddressRange.HostMax
-                Broadcast   = $AddressRange.Broadcast
+            # IPv6
+            $AddressRange = $null
+            if ($AsHashTable) {
+                [ordered] @{
+                    Name        = $Subnet.Name
+                    Type        = 'IPv6'
+                    SiteName    = $SiteObject
+                    SiteStatus  = if ($SiteObject) { $true } else { $false }
+                    OverLap     = $null
+                    OverLapList = $null
+                    Subnet      = ([IPAddress](($Subnet.Name -split "/")[0]))
+                    MaskBits    = ([int](($Subnet.Name -split "/")[1]))
+                    SubnetMask  = ([IPAddress]"$([system.convert]::ToInt64(("1"*$MaskBits).PadRight(32,"0"),2))")
+                    TotalHosts  = $AddressRange.TotalHosts
+                    UsableHosts = $AddressRange.UsableHosts
+                    HostMin     = $AddressRange.HostMin
+                    HostMax     = $AddressRange.HostMax
+                    Broadcast   = $AddressRange.Broadcast
+                }
+            } else {
+                [PSCustomObject] @{
+                    Name        = $Subnet.Name
+                    Type        = 'IPv6'
+                    SiteName    = $SiteObject
+                    SiteStatus  = if ($SiteObject) { $true } else { $false }
+                    Subnet      = ([IPAddress](($Subnet.Name -split "/")[0]))
+                    MaskBits    = ([int](($Subnet.Name -split "/")[1]))
+                    SubnetMask  = ([IPAddress]"$([system.convert]::ToInt64(("1"*$MaskBits).PadRight(32,"0"),2))")
+                    TotalHosts  = $AddressRange.TotalHosts
+                    UsableHosts = $AddressRange.UsableHosts
+                    HostMin     = $AddressRange.HostMin
+                    HostMax     = $AddressRange.HostMax
+                    Broadcast   = $AddressRange.Broadcast
+                }
             }
         }
     }
