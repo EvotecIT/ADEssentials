6c826c9111c9235b141aeee2b898daf8b4bf4f03
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-05-30 18:02:42 +0200

    Improvements

--- Private/Script.ADEssentialsConfiguration.ps1
+++ Private/Script.ADEssentialsConfiguration.ps1
@@ -2,6 +2,7 @@
     Users            = $Script:ShowWinADUser
     Computers        = $Script:ShowWinADComputer
     Laps             = $Script:ConfigurationLAPS
+    LapsACL          = $Script:ConfigurationLAPSACL
     LapsAndBitLocker = $Script:ConfigurationLAPSAndBitlocker
     BitLocker        = $Script:ConfigurationBitLocker
     ServiceAccounts  = $Script:ConfigurationServiceAccounts
