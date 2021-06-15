bedd1f9940448c56a7df16fe90c8c50c70257be3
SEPEEC <petter.carlsson@sweco.se>
2021-06-15 23:57:25 +0200

    Added the ability to copy ACL from one OU to another with one commnad

---
+++ Public/Copy-ADOUSecurity.ps1
@@ -0,0 +1,81 @@
+function Copy-ADOUSecurity {
+    <#
+    .SYNOPSIS
+    Copy AD security from one OU to another.
+
+    .DESCRIPTION
+    Copies the security for one OU to another with the ability to use a different target group with source group as reference.
+
+    .PARAMETER SourceOU
+    The reference OU.
+
+    .PARAMETER TargetOU
+    Target OU to apply security.
+
+    .PARAMETER SourceGroup
+    The reference group.
+
+    .PARAMETER TargetGroup
+    Target group to apply security
+
+    .PARAMETER Execute
+    Switch to execute - leaving this out will result in a dry run (whatif).
+
+    .EXAMPLE
+    Copy-ADOUSecurity -SourceOU "OU=Finance,DC=contoso,DC=com" -TargetOU "OU=Sales,DC=contoso,DC=com" -SourceGroup "FinanceAdmins" -TargetGroup "SalesAdmins"
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)][string]$SourceOU,
+        [Parameter(Mandatory = $true)][string]$TargetOU,
+        [Parameter(Mandatory = $true)][string]$SourceGroup,
+        [Parameter(Mandatory = $true)][string]$TargetGroup,
+        [Parameter(Mandatory = $false)][switch]$Execute
+    )
+    begin { }
+    process {
+
+        [string]$sDomain = (Get-ADDomain).NetBIOSName
+        [string]$sServer = (Get-ADDomainController -Writable -Discover).HostName
+
+        $sSourceOU = $SourceOU.Trim()
+        $sDestOU = $TargetOU.Trim()
+        $sSourceAccount = $SourceGroup.Trim()
+        $sDestAccount = $TargetGroup.Trim()
+
+        [ADSI]$oSourceOU = "LDAP://{0}/{1}" -f $sServer, $sSourceOU
+        [ADSI]$oTargetOU = "LDAP://{0}/{1}" -f $sServer, $sDestOU
+
+        $oDestAccountNT = New-Object -TypeName System.Security.Principal.NTAccount -ArgumentList $sDomain, $sDestAccount
+
+        $oSourceOU.ObjectSecurity.Access | Where-Object { $_.IdentityReference -like "$sDomain\$sSourceAccount" } | ForEach-Object {
+            $ActiveDirectoryRights = $_.ActiveDirectoryRights
+            $AccessControlType = $_.AccessControlType
+            $InheritanceType = $_.InheritanceType
+            $InheritedObjectType = $_.InheritedObjectType
+            $ObjectType = $_.ObjectType
+
+            $oAce = New-Object System.DirectoryServices.ActiveDirectoryAccessRule ($oDestAccountNT, $ActiveDirectoryRights, $AccessControlType, $ObjectType, $InheritanceType, $InheritedObjectType)
+            $oTargetOU.ObjectSecurity.AddAccessRule($oAce)
+        }
+
+        $oSourceOU.ObjectSecurity.Access | Where-Object { $_.IdentityReference -like "$sDomain\$sSourceAccount" }
+        $oTargetOU.ObjectSecurity.Access | Where-Object { $_.IdentityReference -like "$sDomain\$sDestAccount" }
+
+        if ($Execute) {
+            try {
+                $oTargetOU.CommitChanges()
+                Write-Output -InputObject "[Info] : Permissions commited"
+            } catch {
+                $ErrorMessage = $_.Exception.Message
+                Write-Warning -Message $ErrorMessage
+            }
+        } else {
+            Write-Output -InputObject "[Info] : Use the switch -Execute to apply..."
+        }
+    }
+    end { }
+}
