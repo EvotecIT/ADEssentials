a1f5555a0e7dc688cd3c53e506184c0147941d5d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-10-08 09:16:24 +0200

    ACL examples moved

---
+++ Examples/ACL/Example-ACL-GetADACL02.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+$DN = 'OU=Users,OU=Accounts,OU=Production,DC=ad,DC=evotec,DC=xyz'
+
+Get-ADACL -ADObject $DN | Format-Table *
+Get-ADACL -ADObject 'OU=Test \+ Test2,DC=ad,DC=evotec,DC=xyz' -Resolve | Format-Table *
--- Examples/ACL/Example-ACL-Owner01.ps1
+++ Examples/ACL/Example-ACL-Owner01.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module $PSScriptRoot\..\ADEssentials.psd1 -Force
+﻿Import-Module .\ADEssentials.psd1 -Force
 
 $T = Get-ADACLOwner -ADObject 'CN={051BCDDF-CC11-427B-BDF0-684C0A6E3DDB},CN=Policies,CN=System,DC=ad,DC=evotec,DC=xyz', 'CN={051BCDDF-CC11-427B-BDF0-684C0A6E3DDB},CN=Policies,CN=System,DC=ad,DC=evotec,DC=xyz'
 $T | Format-Table
