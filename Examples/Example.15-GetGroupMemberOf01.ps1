9799eefc9aaf0ef9dd23c9168932d7ed74fe2476
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-11-26 08:45:19 +0100

    Update examples

--- Examples/Example.15-GetGroupMemberOf01.ps1
+++ Examples/Example.15-GetGroupMemberOf01.ps1
@@ -1,6 +1,9 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-Get-WinADGroupMemberOf -Identity 'przemyslaw.klys' -AddSelf | Format-Table *
-Get-WinADGroupMemberOf -Identity 'adm.pklys' -AddSelf | Format-Table *
-
-(Get-WinADGroupMemberOf -Identity 'przemyslaw.klys' -AddSelf)[1]
+#Get-WinADGroupMemberOf -Identity 'przemyslaw.klys' -AddSelf -Verbose | Format-Table *
+#Show-WinADGroupMemberOf -Identity 'przemyslaw.klys' -Verbose -Summary
+Show-WinADGroupMemberOf -Identity 'przemyslaw.klys' -Verbose -Summary
+#Show-WinADGroupMember -Identity 'Domain Admins' -Verbose -Summary -Online
+#return
+#Get-WinADGroupMemberOf -Identity 'adm.pklys' -AddSelf | Format-Table *
+#(Get-WinADGroupMemberOf -Identity 'przemyslaw.klys' -AddSelf)[1]
