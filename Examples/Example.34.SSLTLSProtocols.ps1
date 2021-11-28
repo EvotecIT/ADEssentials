4936f66cdf9ba06f973305002430df48c0eddbb4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-11-28 21:10:59 +0100

    example update

---
+++ Examples/Example.34.SSLTLSProtocols.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+Import-Module "C:\Support\GitHub\PSSharedGoods\PSSharedGoods.psd1" -Force
+
+
+#Get-WinADProtocol -ComputerName 'AD1' -Verbose
+Get-WinADProtocol -Verbose | Format-Table *
+#Get-WinADProtocol -ComputerName 'EVOWIN', 'EVOPOWER', 'AD1' -Verbose | Format-Table *
+
+return
+Get-PSRegistry -RegistryPath "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols"
+Get-PSRegistry -RegistryPath "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client"
+Get-PSRegistry -RegistryPath "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client"
+Set-PSRegistry -RegistryPath "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Type REG_DWORD -Key 'DisabledByDefault' -Value 0
