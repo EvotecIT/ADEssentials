b46b3e60bbb21598cc04aff55b19dfeb55c4d9ea
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-06-11 09:30:47 +0200

    Small update to example

--- Examples/Example.22.Priviliged.ps1
+++ Examples/Example.22.Priviliged.ps1
@@ -1,3 +1,11 @@
 Import-Module .\ADEssentials.psd1 -Force
 
-Get-WinADPrivilegedObjects -Verbose | Format-Table *
+$Objects = Get-WinADPrivilegedObjects -Verbose
+$Objects | Format-Table *
+
+$Objects | Out-HtmlView {
+    New-HTMLTableCondition -Name "IsOrphaned" -Value $False -BackgroundColor TeaGreen
+    New-HTMLTableCondition -Name "IsOrphaned" -Value $True -BackgroundColor Red
+    New-HTMLTableCondition -Name "IsCriticalSystemObject" -Value $True -BackgroundColor TeaGreen
+    New-HTMLTableCondition -Name "IsCriticalSystemObject" -Value $False -BackgroundColor Red
+}
