96852cc1139518c826a6eb1949b5c2573b404bc7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-26 18:39:15 +0200

    Update examples

--- Examples/Example.39-GetDNSRecords.ps1
+++ Examples/Example.39-GetDNSRecords.ps1
@@ -1,3 +1,8 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-Get-WinDNSRecords -Verbose | Format-Table
+Get-WinDNSRecords -Verbose -IncludeDetails | Format-Table
+Get-WinDNSRecords -Prettify -IncludeDetails | Format-Table
+
+Get-WinDNSIPAddresses | Format-Table *
+Get-WinDNSIPAddresses -Prettify | Format-Table *
+Get-WinDNSIPAddresses -Prettify -IncludeDetails -IncludeDNSRecords | Format-Table *
--- Examples/Example.40-GetCriticalGroups.ps1
+++ Examples/Example.40-GetCriticalGroups.ps1
@@ -1,3 +1,3 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-Show-WinADGroupCritical -ReportPath "$PSScriptRoot\GroupMembership-CriticalGroups_$(Get-Date -f yyyy-MM-dd_HHmmss).html"
+Show-WinADGroupCritical -ReportPath "$PSScriptRoot\GroupMembership-CriticalGroups_$(Get-Date -f yyyy-MM-dd_HHmmss).html" -Additional -Online
