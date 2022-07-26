09a7885cdd528e8e1d54c8fd92d1e3ccc85fa16a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-26 13:04:56 +0200

    Add new command

---
+++ Examples/Example.40-GetCriticalGroups.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+Show-WinADGroupCritical -ReportPath "$PSScriptRoot\GroupMembership-CriticalGroups_$(Get-Date -f yyyy-MM-dd_HHmmss).html"
---
+++ Public/Show-WinADGroupCritical.ps1
@@ -0,0 +1,85 @@
+﻿function Show-WinADGroupCritical {
+    [alias('Show-WinADCriticalGroups')]
+    [cmdletBinding()]
+    param(
+        [validateSet(
+            "Domain Admins",
+            "Cert Publishers",
+            "Schema Admins",
+            "Enterprise Admins",
+            "DnsAdmins1",
+            "DnsAdmins2",
+            "DnsUpdateProxy",
+            "Group Policy Creator Owners",
+            'Protected Users',
+            'Key Admins',
+            'Enterprise Key Admins',
+            'Server Management',
+            'Organization Management',
+            'DHCP Users',
+            'DHCP Administrators',
+            'Administrators',
+            'Account Operators',
+            'Server Operators',
+            'Print Operators',
+            'Backup Operators',
+            'Replicators',
+            'Network Configuration Operations',
+            'Incoming Forest Trust Builders',
+            'Event Log Readers',
+            'Hyper-V Administrators',
+            'Remote Management Users'
+        )]
+        [string[]] $GroupName,
+        [parameter(Mandatory)][string] $ReportPath,
+        [ValidateSet('Default', 'Hierarchical', 'Both')][string] $HideAppliesTo = 'Both',
+        [switch] $HideComputers,
+        [switch] $HideUsers,
+        [switch] $HideOther,
+        [switch] $Online,
+        [switch] $HideHTML,
+        [switch] $DisableBuiltinConditions,
+        [switch] $AdditionalStatistics,
+        [switch] $Summary
+    )
+
+    $ForestInformation = Get-WinADForestDetails -Extended
+    $ListGroups = foreach ($Domain in $ForestInformation.Domains) {
+        $DomainSidValue = $ForestInformation.DomainsExtended[$Domain].DomainSID
+        $PriviligedGroups = [ordered] @{
+            "Domain Admins"                    = "$DomainSidValue-512"
+            "Cert Publishers"                  = "$DomainSidValue-517"
+            "Schema Admins"                    = "$DomainSidValue-518"
+            "Enterprise Admins"                = "$DomainSidValue-519"
+            "DnsAdmins1"                       = "$DomainSidValue-1101"
+            "DnsAdmins2"                       = "$DomainSidValue-1105"
+            "DnsUpdateProxy"                   = "$DomainSidValue-1106"
+            "Group Policy Creator Owners"      = "$DomainSidValue-520"
+            'Protected Users'                  = "$DomainSidValue-525"
+            'Key Admins'                       = "$DomainSidValue-526"
+            'Enterprise Key Admins'            = "$DomainSidValue-527"
+            'Server Management'                = "$DomainSidValue-1125"
+            'Organization Management'          = "$DomainSidValue-1117"
+            'DHCP Users'                       = "$DomainSidValue-2111"
+            'DHCP Administrators'              = "$DomainSidValue-2112"
+            'Administrators'                   = "S-1-5-32-544"
+            'Account Operators'                = "S-1-5-32-548"
+            'Server Operators'                 = "S-1-5-32-549"
+            'Print Operators'                  = "S-1-5-32-550"
+            'Backup Operators'                 = "S-1-5-32-551"
+            'Replicators'                      = "S-1-5-32-552"
+            'Network Configuration Operations' = "S-1-5-32-556"
+            'Incoming Forest Trust Builders'   = "S-1-5-32-557"
+            'Event Log Readers'                = "S-1-5-32-573"
+            'Hyper-V Administrators'           = "S-1-5-32-578"
+            'Remote Management Users'          = "S-1-5-32-580"
+        }
+        foreach ($Group in $PriviligedGroups.Values) {
+            if ($GroupName -and $Group -notin $GroupName) {
+                continue
+            }
+            (Get-ADGroup -Filter "SID -eq '$Group'" -Server $ForestInformation['QueryServers'][$Domain].HostName[0] -ErrorAction SilentlyContinue).DistinguishedName
+        }
+    }
+    Show-WinADGroupMember -Identity $ListGroups -HideHTML:$HideHTML.IsPresent -FilePath $ReportPath -DisableBuiltinConditions:$DisableBuiltinConditions.IsPresent -Online:$Online.IsPresent -HideUsers:$HideUsers.IsPresent -HideComputers:$HideComputers.IsPresent -AdditionalStatistics:$AdditionalStatistics.IsPresent -Summary:$Summary.IsPresent
+}
