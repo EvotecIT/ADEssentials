1351799b9813de515d25ee1076ec78c72adecd76
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-01 20:27:47 +0100

    Update example

--- Examples/Example.14-ShowADGroup01.ps1
+++ Examples/Example.14-ShowADGroup01.ps1
@@ -1,6 +1,6 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-Show-WinADGroupMember -GroupName 'Domain Admins' -FilePath $PSScriptRoot\Reports\GroupMembership1.html -Online -Verbose
-Show-WinADGroupMember -GroupName 'Test-Group', 'Domain Admins' -FilePath $PSScriptRoot\Reports\GroupMembership2.html -Online -Verbose
+Show-WinADGroupMember -GroupName 'Domain Admins' -FilePath $PSScriptRoot\Reports\GroupMembership1.html -Online -Verbose -SkipDiagram
+Show-WinADGroupMember -GroupName 'Test-Group', 'Domain Admins' -FilePath $PSScriptRoot\Reports\GroupMembership2.html -Online -Verbose -SkipDiagram
 Show-WinADGroupMember -GroupName 'GDS-TestGroup4' -FilePath $PSScriptRoot\Reports\GroupMembership3.html -Summary -Online -Verbose
 Show-WinADGroupMember -GroupName 'Group1' -Verbose -Online
