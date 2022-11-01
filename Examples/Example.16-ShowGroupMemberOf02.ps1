b69f36a528390ae44ef04123891196465381cdfd
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-01 20:27:29 +0100

    Update example

--- Examples/Example.16-ShowGroupMemberOf02.ps1
+++ Examples/Example.16-ShowGroupMemberOf02.ps1
@@ -1,3 +1,3 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-Show-WinADGroupMemberOf -Identity 'przemyslaw.klys', 'adm.pklys' -Summary
+Show-WinADGroupMemberOf -Identity 'przemyslaw.klys', 'adm.pklys' -Summary -SkipDiagram
