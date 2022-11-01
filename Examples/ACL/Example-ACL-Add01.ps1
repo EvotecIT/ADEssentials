49c3697ad113eb3efb50035ec59020d581b6533f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-01 20:26:51 +0100

    Examples for ACL

---
+++ Examples/ACL/Example-ACL-Add01.ps1
@@ -0,0 +1,7 @@
+﻿Clear-Host
+Import-Module .\ADEssentials.psd1 -Force
+
+$FindOU = 'OU=Users,OU=Accounts,OU=Production,DC=ad,DC=evotec,DC=xyz'
+
+Add-ADACL -Verbose -ADObject $FindOU -Principal 'mmmm@ad.evotec.pl' -AccessRule GenericAll -AccessControlType Allow
+Add-ADACL -Verbose -ADObject $FindOU -Principal 'przemyslaw.klys' -AccessRule GenericAll -AccessControlType Allow
---
+++ Examples/ACL/Example-ACL-GetADACL03.ps1
@@ -0,0 +1,13 @@
+﻿Clear-Host
+Import-Module $PSScriptRoot\..\..\ADEssentials.psd1 -Force
+
+$Splat = @{
+    Resolve           = $True
+    #Bundle            = $true
+    ADObject          = 'OU=Accounts01,OU=Tier2,DC=ad,DC=evotec,DC=xyz'
+    AccessControlType = 'Allow'
+    #Principal         = 'mmmm@ad.evotec.pl'
+    Principal         = 'przemyslaw.klys'
+}
+$ACL1 = Get-ADACL @Splat
+$ACL1 | Format-Table
