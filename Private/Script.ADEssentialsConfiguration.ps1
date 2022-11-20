f2c911293c07d399dacd0a4302d78b5fb90515e0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-20 20:17:00 +0100

    Added new report

--- Private/Script.ADEssentialsConfiguration.ps1
+++ Private/Script.ADEssentialsConfiguration.ps1
@@ -1,6 +1,7 @@
 ﻿$Script:ADEssentialsConfiguration = [ordered] @{
     Users            = $Script:ShowWinADUser
     Computers        = $Script:ShowWinADComputer
+    Groups           = $Script:ShowWinADGroup
     Laps             = $Script:ConfigurationLAPS
     LapsACL          = $Script:ConfigurationLAPSACL
     LapsAndBitLocker = $Script:ConfigurationLAPSAndBitlocker
