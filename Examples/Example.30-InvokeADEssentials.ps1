e7b22bcbc59e6a8d8371e212d467be5ab4c65760
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-12-14 11:51:59 +0100

    Update example

--- Examples/Example.30-InvokeADEssentials.ps1
+++ Examples/Example.30-InvokeADEssentials.ps1
@@ -3,4 +3,9 @@
 #Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html
 #Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Users
 #Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type ServiceAccounts
-Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type ForestACLOwners
+#Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type ForestACLOwners
+#Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Computers, Users, ServiceAccounts, ForestACLOwners -SplitReports -HideHTML
+#Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Users
+#
+#Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Computers, Users, Groups
+Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Groups -Online
