2f563d2612f49068175642ee6b075ae63919be40
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-08 09:59:49 +0100

    Add some examples for ACL

--- Examples/ACL/Example-ACL-GetADACL03.ps1
+++ Examples/ACL/Example-ACL-GetADACL03.ps1
@@ -3,11 +3,17 @@ Import-Module $PSScriptRoot\..\..\ADEssentials.psd1 -Force
 
 $Splat = @{
     Resolve           = $True
-    #Bundle            = $true
-    ADObject          = 'OU=Accounts01,OU=Tier2,DC=ad,DC=evotec,DC=xyz'
+    Bundle            = $true
+    ADObject          = 'OU=Accounts02,OU=Tier2,DC=ad,DC=evotec,DC=xyz'
     AccessControlType = 'Allow'
-    #Principal         = 'mmmm@ad.evotec.pl'
-    Principal         = 'przemyslaw.klys'
+    Principal         = 'mmmm@ad.evotec.pl'
+    #Principal         = 'przemyslaw.klys'
 }
-$ACL1 = Get-ADACL @Splat
-$ACL1 | Format-Table
+# $ACL1 = Get-ADACL @Splat
+# $ACL1 | Format-Table
+
+$Principal = 'mmmm@ad.evotec.pl'
+$ACL = Get-ADACL -ADObject 'OU=Accounts02,OU=Tier2,DC=ad,DC=evotec,DC=xyz' -Bundle #-Verbose:$VerbosePreference
+if ($ACL) {
+    Remove-ADACL -ACL $ACL -Principal $Principal -AccessControlType Allow -Verbose -WhatIf
+}
