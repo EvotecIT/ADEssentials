e837934139cb00ca00ffb5953319ccb90e6bb9a1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-08 10:02:44 +0100

    Export ADACL

---
+++ Examples/ACL/Example-Export-ACL.ps1
@@ -0,0 +1,4 @@
+﻿Clear-Host
+Import-Module $PSScriptRoot\..\..\ADEssentials.psd1 -Force
+
+Export-ADACLObject -ADObject 'DC=ad,DC=evotec,DC=xyz' -OneLiner -ExcludePrincipal 'BUILTIN\Pre-Windows 2000 Compatible Access' -Bundle | Format-Table
