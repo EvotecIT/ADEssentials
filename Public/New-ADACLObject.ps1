a917897503677ea6fe99f8baa5a5586a3a22a929
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-09 20:50:05 +0100

    Add ability to reset cache add autocompleter

--- Public/New-ADACLObject.ps1
+++ Public/New-ADACLObject.ps1
@@ -7,10 +7,11 @@
         [parameter(Mandatory)][alias('ObjectTypeName')][string] $ObjectType,
         [parameter(Mandatory)][alias('InheritedObjectTypeName')][string] $InheritedObjectType,
         [parameter(Mandatory)][alias('ActiveDirectorySecurityInheritance')][nullable[System.DirectoryServices.ActiveDirectorySecurityInheritance]] $InheritanceType,
-        [switch] $OneLiner
+        [switch] $OneLiner,
+        [switch] $Force
     )
 
-    $ConvertedIdentity = Convert-Identity -Identity $Principal -Verbose:$false
+    $ConvertedIdentity = Convert-Identity -Identity $Principal -Verbose:$false -Force:$Force.IsPresent
     if ($ConvertedIdentity.Error) {
         Write-Warning -Message "New-ADACLObject - Converting identity $($Principal) failed with $($ConvertedIdentity.Error). Be warned."
     }
@@ -37,3 +38,16 @@
         }
     }
 }
+
+[scriptblock] $ADACLObjectAutoCompleter = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+
+    if (-not $Script:ADSchemaGuids) {
+        Import-Module ActiveDirectory -Verbose:$false
+        $Script:ADSchemaGuids = Convert-ADSchemaToGuid
+    }
+    $Script:ADSchemaGuids.Keys | Sort-Object | Where-Object { $_ -like "*$wordToComplete*" }
+}
+
+Register-ArgumentCompleter -CommandName New-ADACLObject -ParameterName ObjectType -ScriptBlock $ADACLObjectAutoCompleter
+Register-ArgumentCompleter -CommandName New-ADACLObject -ParameterName InheritedObjectType -ScriptBlock $ADACLObjectAutoCompleter
