023a845f005f8b3b05af27d31e8ee1045f51453a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-16 19:59:57 +0200

    Move examples a bit

---
+++ Examples/ACL/Example-ACL-ACLPermissions01.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+# Saves all permissions directly into XLSX file
+# It saves each domain to separate file (if there are multiple domains)
+Get-WinADACLForest -Verbose -OutputFile "$PSSCriptRoot\PermissionsOutputPerSheet.xlsx"
