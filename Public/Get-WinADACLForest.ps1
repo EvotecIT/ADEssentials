834a3818eb85d942c365286e00723fc46138ab5e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-16 20:03:08 +0200

    Add support for output to excel directly

--- Public/Get-WinADACLForest.ps1
+++ Public/Get-WinADACLForest.ps1
@@ -30,6 +30,14 @@
     .PARAMETER Separate
     Returns OrderedDictionary with each top level container being in separate key
 
+    .PARAMETER OutputFile
+    Saves output to Excel file. Requires PSWriteExcel module.
+    This was added to speed up processing and reduce memory usage.
+    When using this option, you can use PassThru option, to get objects as well.
+
+    .PARAMETER PassThru
+    Returns objects as well as saves to Excel file. Requires PSWriteExcel module.
+
     .EXAMPLE
     # With split per sheet
     $FilePath = "$Env:USERPROFILE\Desktop\PermissionsOutputPerSheet.xlsx"
@@ -76,8 +84,19 @@
         [switch] $Separate,
         [switch] $IncludeInherited,
         [validateSet('WellKnownAdministrative', 'Administrative', 'NotAdministrative', 'Unknown')][string[]] $IncludeOwnerType,
-        [validateSet('WellKnownAdministrative', 'Administrative', 'NotAdministrative', 'Unknown')][string[]] $ExcludeOwnerType
+        [validateSet('WellKnownAdministrative', 'Administrative', 'NotAdministrative', 'Unknown')][string[]] $ExcludeOwnerType,
+        [string] $OutputFile,
+        [switch] $PassThru
     )
+    if ($OutputFile) {
+        $CommandExists = Get-Command -Name 'ConvertTo-Excel' -ErrorAction SilentlyContinue
+        if (-not $CommandExists) {
+            Write-Warning -Message "ConvertTo-Excel command is missing. Please install PSWriteExcel module when using OutputFile option."
+            Write-Warning -Message "Install-Module -Name PSWriteExcel -Force -Verbose"
+            return
+        }
+    }
+
     $ForestTime = Start-TimeLog
     $ForestInformation = Get-WinADForestDetails -Forest $Forest -IncludeDomains $IncludeDomains -ExcludeDomains $ExcludeDomains -ExtendedForestInformation $ExtendedForestInformation -Extended
     $Output = [ordered]@{}
@@ -97,7 +116,7 @@
                 continue
             }
         }
-        Write-Verbose "Get-WinADACLForest - [Start][Domain $Domain]"
+        Write-Verbose -Message "Get-WinADACLForest - [Start][Domain $Domain]"
         $DomainTime = Start-TimeLog
         $Output[$Domain] = [ordered] @{}
         $Server = $ForestInformation.QueryServers[$Domain].HostName[0]
@@ -118,7 +137,7 @@
             $ObjectName = "[$Domain][$($Structure.CanonicalName)][$($Structure.ObjectClass)][$($Structure.DistinguishedName)]"
             #$ObjectOutputName = "$($Structure.Name)_$($Structure.ObjectClass)".Replace(' ', '').ToLower()
             $ObjectOutputName = "$($Structure.Name)".Replace(' ', '').ToLower()
-            Write-Verbose "Get-WinADACLForest - [Start]$ObjectName"
+            Write-Verbose -Message "Get-WinADACLForest - [Start]$ObjectName"
             if ($Structure.ObjectClass -eq 'organizationalUnit') {
                 #$Containers = Get-ADOrganizationalUnit -Filter '*' -Server $Server -SearchBase $Structure.DistinguishedName -Properties canonicalName
                 $Ignore = @()
@@ -158,15 +177,15 @@
                 } | Sort-Object canonicalName
             } else {
                 $EndTime = Stop-TimeLog -Time $Time -Option OneLiner
-                Write-Verbose "Get-WinADACLForest - [Skip  ]$ObjectName[ObjectClass not requested]"
+                Write-Verbose -Message "Get-WinADACLForest - [Skip  ]$ObjectName[ObjectClass not requested]"
                 continue
             }
             if (-not $Containers) {
                 $EndTime = Stop-TimeLog -Time $Time -Option OneLiner
-                Write-Verbose "Get-WinADACLForest - [End  ]$ObjectName[$EndTime]"
+                Write-Verbose -Message "Get-WinADACLForest - [End  ]$ObjectName[$EndTime]"
                 continue
             }
-            Write-Verbose "Get-WinADACLForest - [Read ]$ObjectName[Objects to process: $($Containers.Count)]"
+            Write-Verbose -Message "Get-WinADACLForest - [Read ]$ObjectName[Objects to process: $($Containers.Count)]"
             if ($Owner) {
                 $getADACLOwnerSplat = @{
                     ADObject         = $Containers
@@ -184,19 +203,47 @@
                     $MYACL = Get-ADACL -ADObject $Containers -ResolveTypes -NotInherited
                 }
             }
-            if ($Separate) {
+
+            if ($OutputFile) {
+                $TimeExport = Start-TimeLog
+                $Extension = [io.path]::GetExtension($OutputFile)
+                $DirectoryPath = [io.path]::GetDirectoryName($OutputFile)
+                $FileName = [io.path]::GetFileNameWithoutExtension($OutputFile)
+                if ($ForestInformation.Domains.Count -gt 1) {
+                    $FinalPath = [io.path]::Combine($DirectoryPath, "$FileName-$Domain$Extension")
+                } else {
+                    $FinalPath = [io.path]::Combine($DirectoryPath, "$FileName$Extension")
+                }
+                Write-Verbose -Message "Get-WinADACLForest - [Save  ]$ObjectName[OutputFile: $FinalPath]"
+                if ($Structure.ObjectClass -eq 'domainDns') {
+                    $WorkSheetName = "$($Structure.CanonicalName)".Replace("/", "")
+                } else {
+                    $WorkSheetName = "$($Structure.Name)"
+                }
+                $MYACL | ConvertTo-Excel -FilePath $FinalPath -ExcelWorkSheetName $WorkSheetName -AutoFilter -AutoFit -FreezeTopRowFirstColumn
+                $EndTimeExport = Stop-TimeLog -Time $TimeExport -Option OneLiner
+                Write-Verbose -Message "Get-WinADACLForest - [End  ]$ObjectName[OutputFile: $FinalPath][$EndTimeExport]"
+                Write-Verbose -Message "Get-WinADACLForest - [Start]$ObjectName[Garbage Collection]"
+                [System.GC]::Collect()
+                Start-Sleep -Seconds 5
+                [System.GC]::Collect()
+                Write-Verbose -Message "Get-WinADACLForest - [End  ]$ObjectName[Garbage Collection][Done]"
+                if ($PassThru) {
+                    $MYACL
+                }
+            } elseif ($Separate) {
                 $Output[$Domain][$ObjectOutputName] = $MYACL
             } else {
                 $MYACL
             }
             $EndTime = Stop-TimeLog -Time $Time -Option OneLiner
-            Write-Verbose "Get-WinADACLForest - [End  ]$ObjectName[$EndTime]"
+            Write-Verbose -Message "Get-WinADACLForest - [End  ]$ObjectName[$EndTime]"
         }
         $DomainEndTime = Stop-TimeLog -Time $DomainTime -Option OneLiner
-        Write-Verbose "Get-WinADACLForest - [End  ][Domain $Domain][$DomainEndTime]"
+        Write-Verbose -Message "Get-WinADACLForest - [End  ][Domain $Domain][$DomainEndTime]"
     }
     $ForestEndTime = Stop-TimeLog -Time $ForestTime -Option OneLiner
-    Write-Verbose "Get-WinADACLForest - [End  ][Forest][$ForestEndTime]"
+    Write-Verbose -Message "Get-WinADACLForest - [End  ][Forest][$ForestEndTime]"
     if ($Separate) {
         $Output
     }
