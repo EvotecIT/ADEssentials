60fb51614153b8db2ad823c92878485ad662d7aa
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-02 10:58:28 +0200

    Update xample

--- Examples/Example.30-InvokeADEssentials.ps1
+++ Examples/Example.30-InvokeADEssentials.ps1
@@ -1,11 +1,14 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-#Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html
-#Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Users
-#Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type ServiceAccounts
-#Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type ForestACLOwners
-#Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Computers, Users, ServiceAccounts, ForestACLOwners -SplitReports -HideHTML
-#Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Users
-#
-#Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Computers, Users, Groups
-Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Groups -Online
+Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html
+Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Computers, Groups, Users, ServiceAccounts
+Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type ForestACLOwners
+Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Computers, Users, ServiceAccounts, ForestACLOwners -SplitReports -HideHTML
+Invoke-ADEssentials -Online -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Users
+Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Computers, Users, Groups, LAPS, LapsACL
+Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type AccountDelegation -Online -SplitReports
+Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Laps, LapsAndBitLocker, BitLocker -Online
+Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Laps, LapsAndBitLocker, BitLocker -Online
+Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type Laps -Online -SplitReports
+Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type LapsAndBitLocker, BitLocker -Online
+Invoke-ADEssentials -FilePath $PSScriptRoot\Reports\ADEssentials.html -Type LapsACL -Online
