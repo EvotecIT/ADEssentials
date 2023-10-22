96b98513610615067fe755ad95f4093f381d5256
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-22 09:45:17 +0200

    Update example

---
+++ Examples/ACL/Example-ACL-RestoreDefaultPermissions.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+$ObjectCheck = Get-ADObject -Id 'OU=_root,DC=ad,DC=evotec,DC=xyz' -Properties 'NtSecurityDescriptor', 'DistinguishedName'
+$Object = Restore-ADACLDefault -Object $ObjectCheck -Verbose -WhatIf
+$Object
+
+$Object = Restore-ADACLDefault -Object 'OU=ITR01,DC=ad,DC=evotec,DC=xyz' -RemoveInheritedAccessRules -Verbose -WhatIf
+$Object
+
+$Object = Restore-ADACLDefault -Object 'CN=WO_SVC_Delete,CN=Managed Service Accounts,DC=ad,DC=evotec,DC=xyz' -RemoveInheritedAccessRules -Verbose -WhatIf
+$Object
