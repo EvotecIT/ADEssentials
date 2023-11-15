871317f7af08235827eb3647a435dd7231a33dde
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-15 21:27:53 +0100

    Add Find-WinADObjectDifference function to Public directory

---
+++ Public/Find-WinADObjectDifference.ps1
@@ -0,0 +1,169 @@
+﻿function Find-WinADObjectDifference {
+    [CmdletBinding(DefaultParameterSetName = 'Standard')]
+    param(
+        [Parameter(ParameterSetName = 'Standard', Mandatory)]
+        [Array] $Identity,
+
+        [Parameter(ParameterSetName = 'Standard')]
+        [alias('ForestName')][string] $Forest,
+
+        [Parameter(ParameterSetName = 'Standard')]
+        [string[]] $ExcludeDomains,
+
+        [Parameter(ParameterSetName = 'Standard')]
+        [alias('Domain', 'Domains')][string[]] $IncludeDomains,
+
+        [Parameter(ParameterSetName = 'Standard')]
+        [switch] $GlobalCatalog
+    )
+
+    $ForestInformation = Get-WinADForestDetails -Extended
+
+    $Output = [ordered] @{
+        List        = [System.Collections.Generic.List[Object]]::new()
+        ListSummary = [System.Collections.Generic.List[Object]]::new()
+    }
+
+
+    $Properties = @(
+        'company'
+        'department'
+        'Description'
+        'info'
+        'l'
+        for ($i = 1; $i -le 15; $i++) {
+            "extensionAttribute$i"
+        }
+        'manager'
+        'memberOf'
+        'facsimileTelephoneNumber'
+        'givenName'
+        'homePhone'
+        'postalCode'
+        'pager'
+        'UserAccountControl', 'DisplayName', 'mailNickname', 'mail', 'ipPhone'
+
+    )
+    $Properties = $Properties | Sort-Object -Unique
+
+    if ($GlobalCatalog) {
+        [Array] $GCs = foreach ($DC in $ForestInformation.ForestDomainControllers) {
+            if ($DC.IsGlobalCatalog) {
+                $DC
+            }
+        }
+    } else {
+        $DomainFromIdentity = ConvertFrom-DistinguishedName -DistinguishedName $Identity[0] -ToDomainCN
+        [Array] $GCs = foreach ($DC in $ForestInformation.ForestDomainControllers) {
+            if ($DC.Domain -eq $DomainFromIdentity) {
+                $DC
+            }
+        }
+    }
+
+    $CountObject = 0
+    foreach ($I in $Identity) {
+        $PrimaryObject = $null
+        $ADObject = [ordered] @{
+            DistinguishedName = $I
+        }
+        $ADObjectMinimal = [ordered] @{
+            DistinguishedName     = $I
+            ServersDifferent      = [System.Collections.Generic.List[Object]]::new()
+            ServersDifferentCount = 0
+            ServersSame           = [System.Collections.Generic.List[Object]]::new()
+            ServersSameCount      = 0
+            PropertiesSame        = [System.Collections.Generic.List[Object]]::new()
+            PropertiesDifferent   = [System.Collections.Generic.List[Object]]::new()
+        }
+
+        $CountObject++
+        $DistinguishedName = $I
+
+        $Count = 0
+        foreach ($GC in $GCs) {
+            $Count++
+            Write-Verbose -Message "Find-WinADObjectDifference - Processing object [Object: $CountObject / $($Identity.Count)][DC: $Count / $($GCs.Count)] $($GC.HostName) for $I"
+            # Query the specific object on each GC
+            Try {
+                if ($GlobalCatalog) {
+                    $ObjectInfo = Get-ADObject -Identity $I -Server "$($GC.HostName):3268" -ErrorAction Stop -Properties $Properties
+                } else {
+                    $ObjectInfo = Get-ADObject -Identity $I -Server $GC.HostName -Properties * -ErrorAction Stop
+                }
+            } catch {
+                $ObjectInfo = $null
+                Write-Warning "Test-ADObject - Error: $($_.Exception.Message.Replace([System.Environment]::NewLine,''))"
+                $ErrorValue = $_.Exception.Message.Replace([System.Environment]::NewLine, '')
+            }
+            if ($ObjectInfo) {
+                if (-not $PrimaryObject) {
+                    $PrimaryObject = $ObjectInfo
+                }
+                foreach ($Property in $Properties) {
+                    $PropertyNameSame = "$Property-Same"
+                    $PropertyNameDiff = "$Property-Diff"
+                    if (-not $ADObject[$PropertyNameSame]) {
+                        $ADObject[$PropertyNameSame] = [System.Collections.Generic.List[Object]]::new()
+                    }
+                    if (-not $ADObject[$PropertyNameDiff]) {
+                        $ADObject[$PropertyNameDiff] = [System.Collections.Generic.List[Object]]::new()
+                    }
+                    if ($Property -eq 'MemberOf') {
+
+
+                    } elseif ($null -eq $($PrimaryObject.$Property) -and $null -eq ($ObjectInfo.$Property)) {
+                        $ADObject[$PropertyNameSame].Add($GC.HostName)
+                        if ($Property -notin $ADObjectMinimal.PropertiesSame) {
+                            $ADObjectMinimal.PropertiesSame.Add($Property)
+                        }
+                        if ($GC.HostName -notin $ADObjectMinimal.ServersSame) {
+                            $ADObjectMinimal.ServersSame.Add($GC.HostName)
+                        }
+                    } elseif ($null -eq $PrimaryObject.$Property) {
+                        $ADObject[$PropertyNameDiff].Add($GC.HostName)
+                        if ($Property -notin $ADObjectMinimal.PropertiesDifferent) {
+                            $ADObjectMinimal.PropertiesDifferent.Add($Property)
+                        }
+                        if ($GC.HostName -notin $ADObjectMinimal.ServersDifferent) {
+                            $ADObjectMinimal.ServersDifferent.Add($GC.HostName)
+                        }
+                    } elseif ($null -eq $ObjectInfo.$Property) {
+                        $ADObject[$PropertyNameDiff].Add($GC.HostName)
+                        if ($Property -notin $ADObjectMinimal.PropertiesDifferent) {
+                            $ADObjectMinimal.PropertiesDifferent.Add($Property)
+                        }
+                        if ($GC.HostName -notin $ADObjectMinimal.ServersDifferent) {
+                            $ADObjectMinimal.ServersDifferent.Add($GC.HostName)
+                        }
+                    } else {
+                        if ($ObjectInfo.$Property -ne $PrimaryObject.$Property) {
+                            $ADObject[$PropertyNameDiff].Add($GC.HostName)
+                            if ($Property -notin $ADObjectMinimal.PropertiesDifferent) {
+                                $ADObjectMinimal.PropertiesDifferent.Add($Property)
+                            }
+                            if ($GC.HostName -notin $ADObjectMinimal.ServersDifferent) {
+                                $ADObjectMinimal.ServersDifferent.Add($GC.HostName)
+                            }
+                        } else {
+                            $ADObject[$PropertyNameSame].Add($GC.HostName)
+                            if ($Property -notin $ADObjectMinimal.PropertiesSame) {
+                                $ADObjectMinimal.PropertiesSame.Add($Property)
+                            }
+                            if ($GC.HostName -notin $ADObjectMinimal.ServersSame) {
+                                $ADObjectMinimal.ServersSame.Add($GC.HostName)
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+        $ADObjectMinimal.ServersDifferentCount = $ADObjectMinimal.ServersDifferent.Count
+        $ADObjectMinimal.ServersSameCount = $ADObjectMinimal.ServersSame.Count
+        $Output.List.Add([PSCustomObject] $ADObject)
+        $Output.ListSummary.Add([PSCustomObject] $ADObjectMinimal)
+    }
+    $Output
+}
