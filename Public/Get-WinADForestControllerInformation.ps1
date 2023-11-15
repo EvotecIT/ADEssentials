b1347e078aa36a29e111b94c6b0c01e7cb2dd0ee
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-15 21:29:24 +0100

    Add options and roles to Get-WinADForestControllerInformation function

--- Public/Get-WinADForestControllerInformation.ps1
+++ Public/Get-WinADForestControllerInformation.ps1
@@ -49,6 +49,36 @@ function Get-WinADForestControllerInformation {
                 $PasswordLastChangedDays = $null
             }
 
+            $Options = Get-WinADDomainControllerOption -DomainController $DC.DNSHostName
+            if ($Options.Options -contains 'DISABLE_OUTBOUND_REPL') {
+                $DisabledOutboundReplication = $true
+            } else {
+                $DisabledOutboundReplication = $false
+            }
+            if ($Options.Options -contains 'DISABLE_INBOUND_REPL') {
+                $DisabledInboundReplication = $true
+            } else {
+                $DisabledInboundReplication = $false
+            }
+            if ($Options.Options -contains "IS_GC") {
+                $IsGlobalCatalog = $true
+            } else {
+                $IsGlobalCatalog = $false
+            }
+            if ($Options.Options -contains 'IS_RODC') {
+                $IsReadOnlyDomainController = $true
+            } else {
+                $IsReadOnlyDomainController = $false
+            }
+
+            $Roles = [ordered] @{}
+            $Roles['SchemaMaster'] = $ForestInformation.Forest.SchemaMaster
+            $Roles['DomainNamingMaster'] = $ForestInformation.Forest.DomainNamingMaster
+            $Roles['InfrastructureMaster'] = $ForestInformation.DomainsExtended[$Domain].InfrastructureMaster
+            $Roles['RIDMaster'] = $ForestInformation.DomainsExtended[$Domain].RIDMaster
+            $Roles['PDCEmulator'] = $ForestInformation.DomainsExtended[$Domain].PDCEmulator
+
+
             $DNS = Resolve-DnsName -DnsOnly -Name $DC.DNSHostName -ErrorAction SilentlyContinue -QuickTimeout -Verbose:$false
             if ($DNS) {
                 $ResolvedIP4 = ($DNS | Where-Object { $_.Section -eq 'Answer' -and $_.Type -eq 'A' }).IPAddress
@@ -60,38 +90,48 @@ function Get-WinADForestControllerInformation {
                 $DNSStatus = $false
             }
             [PSCustomObject] @{
-                DNSHostName                = $DC.DNSHostName
-                DomainName                 = $Domain
-                Enabled                    = $DC.Enabled
-                DNSStatus                  = $DNSStatus
-                IPAddressStatusV4          = if ($ResolvedIP4 -eq $DC.IPv4Address) { $true } else { $false }
-                IPAddressStatusV6          = if ($ResolvedIP6 -eq $DC.IPv6Address) { $true } else { $false }
-                IPAddressHasOneIpV4        = $ResolvedIP4 -isnot [Array]
-                IPAddressHasOneipV6        = $ResolvedIP6 -isnot [Array]
-                ManagerNotSet              = $Null -eq $ManagedBy
-                OwnerType                  = $Owner.OwnerType
-                PasswordLastChangedDays    = $PasswordLastChangedDays
-                LastLogonDays              = $LastLogonDays
-                Owner                      = $Owner.OwnerName
-                OwnerSid                   = $Owner.OwnerSid
-                ManagedBy                  = $DC.ManagedBy
-                DNSResolvedIPv4            = $ResolvedIP4
-                DNSResolvedIPv6            = $ResolvedIP6
-                IPv4Address                = $DC.IPv4Address
-                IPv6Address                = $DC.IPv6Address
-                LastLogonDate              = $DC.LastLogonDate
-                OperatingSystem            = $DC.OperatingSystem
-                OperatingSystemVersion     = $DC.OperatingSystemVersion
-                PasswordExpired            = $DC.PasswordExpired
-                PasswordLastSet            = $DC.PasswordLastSet
-                PasswordNeverExpires       = $DC.PasswordNeverExpires
-                PasswordNotRequired        = $DC.PasswordNotRequired
-                TrustedForDelegation       = $DC.TrustedForDelegation
-                TrustedToAuthForDelegation = $DC.TrustedToAuthForDelegation
-                UseDESKeyOnly              = $DC.UseDESKeyOnly
-                WhenCreated                = $DC.WhenCreated
-                WhenChanged                = $DC.WhenChanged
-                DistinguishedName          = $DC.DistinguishedName
+                DNSHostName                 = $DC.DNSHostName
+                DomainName                  = $Domain
+                Enabled                     = $DC.Enabled
+                DNSStatus                   = $DNSStatus
+                IsGC                        = $IsGlobalCatalog
+                IsRODC                      = $IsReadOnlyDomainController
+                IPAddressStatusV4           = if ($ResolvedIP4 -eq $DC.IPv4Address) { $true } else { $false }
+                IPAddressStatusV6           = if ($ResolvedIP6 -eq $DC.IPv6Address) { $true } else { $false }
+                IPAddressHasOneIpV4         = $ResolvedIP4 -isnot [Array]
+                IPAddressHasOneipV6         = $ResolvedIP6 -isnot [Array]
+                ManagerNotSet               = $Null -eq $ManagedBy
+                OwnerType                   = $Owner.OwnerType
+                PasswordLastChangedDays     = $PasswordLastChangedDays
+                LastLogonDays               = $LastLogonDays
+                Owner                       = $Owner.OwnerName
+                OwnerSid                    = $Owner.OwnerSid
+                ManagedBy                   = $DC.ManagedBy
+                DNSResolvedIPv4             = $ResolvedIP4
+                DNSResolvedIPv6             = $ResolvedIP6
+                IPv4Address                 = $DC.IPv4Address
+                IPv6Address                 = $DC.IPv6Address
+                LastLogonDate               = $DC.LastLogonDate
+                OperatingSystem             = $DC.OperatingSystem
+                OperatingSystemVersion      = $DC.OperatingSystemVersion
+                PasswordExpired             = $DC.PasswordExpired
+                PasswordLastSet             = $DC.PasswordLastSet
+                PasswordNeverExpires        = $DC.PasswordNeverExpires
+                PasswordNotRequired         = $DC.PasswordNotRequired
+                TrustedForDelegation        = $DC.TrustedForDelegation
+                TrustedToAuthForDelegation  = $DC.TrustedToAuthForDelegation
+                DisabledOutboundReplication = $DisabledOutboundReplication
+                DisabledInboundReplication  = $DisabledInboundReplication
+                Options                     = $Options.Options -join ', '
+                UseDESKeyOnly               = $DC.UseDESKeyOnly
+                SchemaMaster                = if ($Roles['SchemaMaster'] -eq $DC.DNSHostName) { $true } else { $false }
+                DomainNamingMaster          = if ($Roles['DomainNamingMaster'] -eq $DC.DNSHostName) { $true } else { $false }
+                InfrastructureMaster        = if ($Roles['InfrastructureMaster'] -eq $DC.DNSHostName) { $true } else { $false }
+                RIDMaster                   = if ($Roles['RIDMaster'] -eq $DC.DNSHostName) { $true } else { $false }
+                PDCEmulator                 = if ($Roles['PDCEmulator'] -eq $DC.DNSHostName) { $true } else { $false }
+                WhenCreated                 = $DC.WhenCreated
+                WhenChanged                 = $DC.WhenChanged
+                DistinguishedName           = $DC.DistinguishedName
             }
         }
     }
