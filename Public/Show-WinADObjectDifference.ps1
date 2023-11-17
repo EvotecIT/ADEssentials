f7a248a37fcfce318339b5b4c582ed1f00261683
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-17 10:22:53 +0100

    Refactored HTML table layout in Show-WinADObjectDifference.ps1

--- Public/Show-WinADObjectDifference.ps1
+++ Public/Show-WinADObjectDifference.ps1
@@ -11,13 +11,15 @@
     New-HTML {
         New-HTMLTableOption -DataStore JavaScript -BoolAsString -ArrayJoinString ", " -ArrayJoin
 
-        New-HTMLTable -DataTable $OutputValue.ListSummary -Filtering -DataStore JavaScript -ScrollX {
-            New-HTMLTableCondition -Name 'ServersDifferentCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor LimeGreen -FailBackgroundColor Salmon
-            New-HTMLTableCondition -Name 'ServersSameCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor Salmon -FailBackgroundColor LimeGreen
+        New-HTMLTab -Name 'Summary of Differences' {
+            New-HTMLTable -DataTable $OutputValue.ListSummary -Filtering -DataStore JavaScript -ScrollX {
+                New-HTMLTableCondition -Name 'ServersDifferentCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor LimeGreen -FailBackgroundColor Salmon
+                New-HTMLTableCondition -Name 'ServersSameCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor Salmon -FailBackgroundColor LimeGreen
+            }
         }
-
-        New-HTMLTable -DataTable $OutputValue.List -Filtering -DataStore JavaScript -ScrollX
-
+        New-HTMLTab -Name 'Detailed Differences' {
+            New-HTMLTable -DataTable $OutputValue.List -Filtering -DataStore JavaScript -ScrollX
+        }
     } -ShowHTML
     Write-Verbose -Message "Show-WinADObjectDifference - Generating HTML - Done"
 }
