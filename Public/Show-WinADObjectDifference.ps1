461421ace9c020ddb93a89b5b532732a825660fb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-19 09:28:32 +0100

    Refactor HTML generation in Show-WinADObjectDifference.ps1

--- Public/Show-WinADObjectDifference.ps1
+++ Public/Show-WinADObjectDifference.ps1
@@ -11,13 +11,21 @@
     New-HTML {
         New-HTMLTableOption -DataStore JavaScript -BoolAsString -ArrayJoinString ", " -ArrayJoin
 
-        New-HTMLTable -DataTable $OutputValue.ListSummary -Filtering -DataStore JavaScript -ScrollX {
-            New-HTMLTableCondition -Name 'ServersDifferentCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor LimeGreen -FailBackgroundColor Salmon
-            New-HTMLTableCondition -Name 'ServersSameCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor Salmon -FailBackgroundColor LimeGreen
+        New-HTMLTab -Name 'Summary' {
+            New-HTMLTable -DataTable $OutputValue.ListSummary -Filtering -DataStore JavaScript -ScrollX {
+                New-HTMLTableCondition -Name 'ServersDifferentCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor LimeGreen -FailBackgroundColor Salmon
+                New-HTMLTableCondition -Name 'ServersSameCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor Salmon -FailBackgroundColor LimeGreen
+            }
         }
-
-        New-HTMLTable -DataTable $OutputValue.List -Filtering -DataStore JavaScript -ScrollX
-
+        New-HTMLTab -Name 'Details per property' {
+            New-HTMLTable -DataTable $OutputValue.ListDetails -Filtering -DataStore JavaScript -ScrollX
+        }
+        New-HTMLTab -Name 'Details per server' {
+            New-HTMLTable -DataTable $OutputValue.ListDetailsReversed -Filtering -DataStore JavaScript -ScrollX
+        }
+        New-HTMLTab -Name 'Details summary' {
+            New-HTMLTable -DataTable $OutputValue.List -Filtering -DataStore JavaScript -ScrollX
+        }
     } -ShowHTML
     Write-Verbose -Message "Show-WinADObjectDifference - Generating HTML - Done"
 }
