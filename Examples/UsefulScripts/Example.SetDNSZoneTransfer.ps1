32bca203eb4d27d1e85eccbc8f30afde76ad3977
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-20 08:29:29 +0100

    UsefulScripts

---
+++ Examples/UsefulScripts/Example.SetDNSZoneTransfer.ps1
@@ -0,0 +1,33 @@
+﻿
+$ZoneNames = "ad.evotec.xyz"
+$DnsServer = "AD1"
+
+foreach ($ZoneName in $ZoneNames) {
+    $setDnsServerPrimaryZoneSplat = @{
+        Name              = $zoneName
+        ComputerName      = $DnsServer
+        SecureSecondaries = 'TransferToSecureServers'
+        SecondaryServers  = @(
+            "192.168.1.10", "192.168.1.11"
+        )
+        Notify            = 'NotifyServers'
+        NotifyServers     = @(
+            "192.168.1.10", "192.168.1.11"
+        )
+        PassThru          = $true
+    }
+    # Set the zone transfer settings
+    Set-DnsServerPrimaryZone @setDnsServerPrimaryZoneSplat
+
+    <# Set the zone transfer settings reverting to no zone transfer
+    $setDnsServerPrimaryZoneSplat = @{
+        Name              = $zoneName
+        ComputerName      = $DnsServer
+        SecureSecondaries = 'NoTransfer'
+        Notify            = 'NoNotify'
+        PassThru          = $true
+    }
+
+    Set-DnsServerPrimaryZone @setDnsServerPrimaryZoneSplat
+    #>
+}
