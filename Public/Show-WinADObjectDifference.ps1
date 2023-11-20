249e7edb539987ff9385789058d9629bc24fedac
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-20 08:26:49 +0100

    Refactor Show-WinADObjectDifference function to include Properties parameter

--- Public/Show-WinADObjectDifference.ps1
+++ Public/Show-WinADObjectDifference.ps1
@@ -1,11 +1,12 @@
 ﻿function Show-WinADObjectDifference {
     [CmdletBinding()]
     param(
-        [string[]] $Identity,
-        [switch] $GlobalCatalog
+        [Array] $Identity,
+        [switch] $GlobalCatalog,
+        [string[]] $Properties
     )
 
-    $OutputValue = Find-WinADObjectDifference -Identity $Identity -GlobalCatalog:$GlobalCatalog.IsPresent
+    $OutputValue = Find-WinADObjectDifference -Identity $Identity -GlobalCatalog:$GlobalCatalog.IsPresent -Properties $Properties
 
     Write-Verbose -Message "Show-WinADObjectDifference - Generating HTML"
     New-HTML {
@@ -13,12 +14,12 @@
 
         New-HTMLTab -Name 'Summary' {
             New-HTMLTable -DataTable $OutputValue.ListSummary -Filtering -DataStore JavaScript -ScrollX {
-                New-HTMLTableCondition -Name 'ServersDifferentCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor LimeGreen -FailBackgroundColor Salmon
-                New-HTMLTableCondition -Name 'ServersSameCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor Salmon -FailBackgroundColor LimeGreen
+                New-HTMLTableCondition -Name 'DifferentServersCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor LimeGreen -FailBackgroundColor Salmon -HighlightHeaders 'DifferentServersCount', 'DifferentServers', 'DifferentProperties'
+                New-HTMLTableCondition -Name 'SameServersCount' -Operator eq -ComparisonType number -Value 0 -BackgroundColor Salmon -FailBackgroundColor LimeGreen -HighlightHeaders 'SameServersCount', 'SameServers', 'SameProperties'
             }
         }
         New-HTMLTab -Name 'Details per property' {
-            New-HTMLTable -DataTable $OutputValue.ListDetails -Filtering -DataStore JavaScript -ScrollX
+            New-HTMLTable -DataTable $OutputValue.ListDetails -Filtering -DataStore JavaScript -ScrollX -AllProperties
         }
         New-HTMLTab -Name 'Details per server' {
             New-HTMLTable -DataTable $OutputValue.ListDetailsReversed -Filtering -DataStore JavaScript -ScrollX
