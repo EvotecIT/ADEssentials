2d5ff57dfcb99fb4b921bf1916e5c906a8b7b76c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-15 09:36:23 +0100

    Add 3 examples

---
+++ Examples/Example.56-DFS_Topology.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+Get-WinADDFSTopology -Type All | Format-Table
---
+++ Examples/Example.58-ResetPasswordOnExpiry.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+# 
+$OU = @(
+    'OU=Default,OU=Users.NoSync,OU=Accounts,OU=Production,DC=ad,DC=evotec,DC=xyz'
+    'OU=Administrative,OU=Users.NoSync,OU=Accounts,OU=Production,DC=ad,DC=evotec,DC=xyz'
+)
+
+Request-ChangePasswordAtLogon -OrganizationalUnit $OU -LimitProcessing 1 -PassThru -Verbose -WhatIf | Format-Table
+
+# Disable accounts on account expiration (not password expiration), without limits of OU
+Request-ChangePasswordAtLogon -LimitProcessing 1 -PassThru -Verbose -WhatIf | Format-Table
