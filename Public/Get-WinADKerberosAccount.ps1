7b62555c08d4c8d411aad9ed0b14db0eb24f9e0c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-16 22:53:28 +0100

    Optimize Kerberos account retrieval
    Enhanced the Get-WinADKerberosAccount script for better reporting and error handling. It now includes verbose logging with progress tracking across domains, accounts, and domain controllers, improving troubleshooting and monitoring capabilities. Removed the ExtendedForestInformation parameter and instead added the PreferWritable flag in forest information retrieval for increased stability. Implemented try-catch blocks around AD user queries to gracefully handle errors and continue processing. Added status checks for Kerberos accounts and servers, allowing for more detailed investigation of potential issues. This change facilitates administrators in managing Kerberos accounts across large forests by providing clearer insights and reducing the likelihood of script interruptions due to temporary AD connection issues.

--- Public/Get-WinADKerberosAccount.ps1
+++ Public/Get-WinADKerberosAccount.ps1
@@ -3,17 +3,21 @@
     param(
         [alias('ForestName')][string] $Forest,
         [string[]] $ExcludeDomains,
-        [alias('Domain', 'Domains')][string[]] $IncludeDomains,
-        [System.Collections.IDictionary] $ExtendedForestInformation
+        [alias('Domain', 'Domains')][string[]] $IncludeDomains
     )
     $Today = Get-Date
     $Accounts = [ordered] @{}
-    $ForestInformation = Get-WinADForestDetails -Forest $Forest -IncludeDomains $IncludeDomains -ExcludeDomains $ExcludeDomains -ExtendedForestInformation $ExtendedForestInformation
+    Write-Verbose -Message "Get-WinADKerberosAccount - Gathering information about forest"
+    $ForestInformation = Get-WinADForestDetails -Forest $Forest -IncludeDomains $IncludeDomains -ExcludeDomains $ExcludeDomains -PreferWritable
     foreach ($Domain in $ForestInformation.Domains) {
         $Accounts["$Domain"] = [ordered] @{}
     }
+    $DomainCount = 0
+    $DomainCountTotal = $ForestInformation.Domains.Count
     foreach ($Domain in $ForestInformation.Domains) {
-        Write-Verbose -Message "Get-WinADKerberosAccount - Processing domain $Domain"
+        $DomainCount++
+        $ProcessingText = "[Domain: $DomainCount/$DomainCountTotal]"
+        Write-Verbose -Message "Get-WinADKerberosAccount - $ProcessingText Processing domain $Domain"
         $QueryServer = $ForestInformation['QueryServers']["$Domain"].HostName[0]
 
         $Properties = @(
@@ -24,9 +28,17 @@
             'SID', 'SIDHistory'
         )
 
-        $KerberosPasswords = Get-ADUser -Filter "Name -like 'krbtgt*'" -Server $QueryServer -Properties $Properties #| Select-Object -Property $Properties
+        $CountK = 0
+        try {
+            [Array] $KerberosPasswords = Get-ADUser -Filter "Name -like 'krbtgt*'" -Server $QueryServer -Properties $Properties -ErrorAction Stop
+        } catch {
+            Write-Warning -Message "Get-WinADKerberosAccount - $ProcessingText Processing domain $Domain - unable to get Kerberos accounts. Error: $($_.Exception.Message)"
+            continue
+        }
         foreach ($Account in $KerberosPasswords) {
-            Write-Verbose -Message "Get-WinADKerberosAccount - Processing domain $Domain \ Kerberos account $($Account.SamAccountName) \ DC"
+            $CountK++
+            $ProcessingText = "[Domain: $DomainCount/$DomainCountTotal / Account: $CountK/$($KerberosPasswords.Count)]"
+            Write-Verbose -Message "Get-WinADKerberosAccount - $ProcessingText Processing domain $Domain \ Kerberos account ($CountK/$($KerberosPasswords.Count)) $($Account.SamAccountName) \ DC"
 
             if ($Account.SamAccountName -like "*_*" -and -not $Account.'msDS-KrbTgtLinkBl') {
                 Write-Warning -Message "Get-WinADKerberosAccount - Processing domain $Domain \ Kerberos account $($Account.SamAccountName) \ DC - Skipping"
@@ -34,47 +46,90 @@
             }
 
             $CachedServers = [ordered] @{}
+            $CountDC = 0
+            $CountDCTotal = $ForestInformation.DomainDomainControllers[$Domain].Count
             foreach ($DC in $ForestInformation.DomainDomainControllers[$Domain]) {
+                $CountDC++
                 $Server = $DC.HostName
-                Write-Verbose -Message "Get-WinADKerberosAccount - Processing domain $Domain \ Kerberos account $($Account.SamAccountName) \ DC Server $Server"
-                $ServerData = Get-ADUser -Identity $Account.DistinguishedName -Server $Server -Properties 'msDS-KrbTgtLinkBl', 'PasswordLastSet', 'WhenCreated', 'WhenChanged'
-
-                $WhenChangedDaysAgo = ($Today) - $ServerData.WhenChanged
-                $PasswordLastSetAgo = ($Today) - $ServerData.PasswordLastSet
-
-                $CachedServers[$Server] = [PSCustomObject] @{
-                    'Server'              = $Server
-                    'Name'                = $ServerData.Name
-                    'GlobalCatalog'       = $ServerData.'msDS-KrbTgtLinkBl'
-                    'PasswordLastSet'     = $ServerData.'PasswordLastSet'
-                    'PasswordLastSetDays' = $PasswordLastSetAgo.Days
-                    'WhenChangedDays'     = $WhenChangedDaysAgo.Days
-                    'WhenChanged'         = $ServerData.'WhenChanged'
-                    'WhenCreated'         = $ServerData.'WhenCreated'
-                }
-            }
-
-            Write-Verbose -Message "Get-WinADKerberosAccount - Processing domain $Domain \ Kerberos account $($Account.SamAccountName) \ GC"
-            $GlobalCatalogs = [ordered] @{}
-            foreach ($DC in $ForestInformation.ForestDomainControllers) {
-                if ($DC.IsGlobalCatalog ) {
-                    $Server = $DC.HostName
-                    #$DomainPerServer = $DC.Domain
-                    Write-Verbose -Message "Get-WinADKerberosAccount - Processing domain $Domain \ Kerberos account $($Account.SamAccountName) \ GC Server $Server"
-                    $ServerData = Get-ADUser -Identity $Account.DistinguishedName -Server "$($Server):3268" -Properties 'msDS-KrbTgtLinkBl', 'PasswordLastSet', 'WhenCreated', 'WhenChanged'
-
+                $ProcessingText = "[Domain: $DomainCount/$DomainCountTotal / Account: $CountK/$($KerberosPasswords.Count), DC: $CountDC/$CountDCTotal]"
+                Write-Verbose -Message "Get-WinADKerberosAccount - $ProcessingText Processing domain $Domain \ Kerberos account $($Account.SamAccountName) \ DC Server $Server"
+                try {
+                    $ServerData = Get-ADUser -Identity $Account.DistinguishedName -Server $Server -Properties 'msDS-KrbTgtLinkBl', 'PasswordLastSet', 'WhenCreated', 'WhenChanged' -ErrorAction Stop
                     $WhenChangedDaysAgo = ($Today) - $ServerData.WhenChanged
                     $PasswordLastSetAgo = ($Today) - $ServerData.PasswordLastSet
 
-                    $GlobalCatalogs[$Server] = [PSCustomObject] @{
+                    $CachedServers[$Server] = [PSCustomObject] @{
                         'Server'              = $Server
                         'Name'                = $ServerData.Name
-                        'GlobalCatalog'       = $ServerData.'msDS-KrbTgtLinkBl'
                         'PasswordLastSet'     = $ServerData.'PasswordLastSet'
                         'PasswordLastSetDays' = $PasswordLastSetAgo.Days
                         'WhenChangedDays'     = $WhenChangedDaysAgo.Days
                         'WhenChanged'         = $ServerData.'WhenChanged'
                         'WhenCreated'         = $ServerData.'WhenCreated'
+                        'msDS-KrbTgtLinkBl'   = $ServerData.'msDS-KrbTgtLinkBl'
+                        'Status'              = 'OK'
+                    }
+                } catch {
+                    Write-Warning -Message "Get-WinADKerberosAccount - Processing domain $Domain $ProcessingText \ Kerberos account $($Account.SamAccountName) \ DC Server $Server - Error: $($_.Exception.Message)"
+                    $CachedServers[$Server] = [PSCustomObject] @{
+                        'Server'              = $Server
+                        'Name'                = $Server
+                        'PasswordLastSet'     = $null
+                        'PasswordLastSetDays' = $null
+                        'WhenChangedDays'     = $null
+                        'WhenChanged'         = $null
+                        'WhenCreated'         = $null
+                        'msDS-KrbTgtLinkBl'   = $ServerData.'msDS-KrbTgtLinkBl'
+                        'Status'              = $_.Exception.Message
+                    }
+                }
+            }
+
+            Write-Verbose -Message "Get-WinADKerberosAccount - Gathering information about forest for Global Catalogs"
+            $ForestInformationGC = Get-WinADForestDetails -Forest $Forest
+            $ProcessingText = "[Domain: $DomainCount/$DomainCountTotal / Account: $CountK/$($KerberosPasswords.Count)]"
+            Write-Verbose -Message "Get-WinADKerberosAccount - $ProcessingText Processing domain $Domain \ Kerberos account $($Account.SamAccountName) \ GC"
+            $GlobalCatalogs = [ordered] @{}
+            $GlobalCatalogCount = 0
+            $GlobalCatalogCountTotal = $ForestInformationGC.ForestDomainControllers.Count
+            foreach ($DC in $ForestInformationGC.ForestDomainControllers) {
+                $GlobalCatalogCount++
+
+                $Server = $DC.HostName
+                $ProcessingText = "[Domain: $DomainCount/$DomainCountTotal / Account: $CountK/$($KerberosPasswords.Count), GC: $GlobalCatalogCount/$GlobalCatalogCountTotal]"
+                Write-Verbose -Message "Get-WinADKerberosAccount - $ProcessingText Processing domain $Domain \ Kerberos account $($Account.SamAccountName) \ GC Server $Server"
+
+                if ($DC.IsGlobalCatalog ) {
+                    try {
+                        $ServerData = Get-ADUser -Identity $Account.DistinguishedName -Server "$($Server):3268" -Properties 'msDS-KrbTgtLinkBl', 'PasswordLastSet', 'WhenCreated', 'WhenChanged' -ErrorAction Stop
+
+                        $WhenChangedDaysAgo = ($Today) - $ServerData.WhenChanged
+                        $PasswordLastSetAgo = ($Today) - $ServerData.PasswordLastSet
+
+                        $GlobalCatalogs[$Server] = [PSCustomObject] @{
+                            'Server'              = $Server
+                            'Name'                = $ServerData.Name
+                            'PasswordLastSet'     = $ServerData.'PasswordLastSet'
+                            'PasswordLastSetDays' = $PasswordLastSetAgo.Days
+                            'WhenChangedDays'     = $WhenChangedDaysAgo.Days
+                            'WhenChanged'         = $ServerData.'WhenChanged'
+                            'WhenCreated'         = $ServerData.'WhenCreated'
+                            'msDS-KrbTgtLinkBl'   = $ServerData.'msDS-KrbTgtLinkBl'
+                            'Status'              = $_.Exception.Message
+                        }
+                    } catch {
+                        Write-Warning -Message "Get-WinADKerberosAccount - Processing domain $Domain $ProcessingText \ Kerberos account $($Account.SamAccountName) \ GC Server $Server - Error: $($_.Exception.Message)"
+                        $GlobalCatalogs[$Server] = [PSCustomObject] @{
+                            'Server'              = $Server
+                            'Name'                = $Server
+                            'PasswordLastSet'     = $null
+                            'PasswordLastSetDays' = $null
+                            'WhenChangedDays'     = $null
+                            'WhenChanged'         = $null
+                            'WhenCreated'         = $null
+                            'msDS-KrbTgtLinkBl'   = $null
+                            'Status'              = $_.Exception.Message
+                        }
                     }
                 }
             }
@@ -87,8 +142,8 @@
                     'SamAccountName'                    = $Account.SamAccountName
                     'Enabled'                           = $Account.Enabled
                     'PasswordLastSet'                   = $Account.PasswordLastSet
-                    'PasswordLastSetDays'               = $PasswordLastSetAgo
-                    'WhenChangedDays'                   = $WhenChangedDaysAgo
+                    'PasswordLastSetDays'               = $PasswordLastSetAgo.Days
+                    'WhenChangedDays'                   = $WhenChangedDaysAgo.Days
                     'WhenChanged'                       = $Account.WhenChanged
                     'WhenCreated'                       = $Account.WhenCreated
                     'AllowReversiblePasswordEncryption' = $Account.AllowReversiblePasswordEncryption
