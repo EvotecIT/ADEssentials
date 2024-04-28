ca5a0131577b41323e1d4fd02c27d872408d86ff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-28 20:13:58 +0200

    feat: Add advanced option to Compare-WinADGlobalCatalogObjects function

--- Public/Compare-WinADGlobalCatalogObjects.ps1
+++ Public/Compare-WinADGlobalCatalogObjects.ps1
@@ -1,7 +1,7 @@
 ﻿function Compare-WinADGlobalCatalogObjects {
     [CmdletBinding()]
     param(
-
+        [switch] $Advanced
     )
 
     $SummaryDomains = [ordered] @{}
@@ -11,5 +11,21 @@
         $QueryServer = $ForestInformation['QueryServers'][$Domain].HostName[0]
         $SummaryDomains[$Domain] = Compare-InternalMissingObject -ForestInformation $ForestInformation -Server $QueryServer -SourceDomain $Domain -TargetDomain $ForestInformation.Domains
     }
-    $SummaryDomains
+
+    if ($Advanced) {
+        $SummaryDomains
+    } else {
+        foreach ($Domain in $SummaryDomains.Keys) {
+            foreach ($Server in $SummaryDomains[$Domain].Keys) {
+                if ($Server -notin 'Summary') {
+                    if ($null -ne $SummaryDomains[$Domain][$Server].Missing.Count -gt 0) {
+                        $SummaryDomains[$Domain][$Server].Missing
+                    }
+                    if ($Null -ne $SummaryDomains[$Domain][$Server].WrongGuid.Count -gt 0) {
+                        $SummaryDomains[$Domain][$Server].WrongGuid
+                    }
+                }
+            }
+        }
+    }
 }
