4f07a707792907ae511a9e027787e857813c6153
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-29 09:33:41 +0200

    Update example

--- Examples/Example.65.CompareGCObjects.ps1
+++ Examples/Example.65.CompareGCObjects.ps1
@@ -1,6 +1,6 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-$Find = Compare-WinADGlobalCatalogObjects -Verbose
+$Find = Compare-WinADGlobalCatalogObjects -Verbose -IncludeDomains 'ad.evotec.pl' -Advanced
 $Find
 
-Invoke-ADEssentials -Type GlobalCatalogComparison -Verbose
+Invoke-ADEssentials -Type GlobalCatalogComparison -Verbose -IncludeDomains 'ad.evotec.xyz'
