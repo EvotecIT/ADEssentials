5affb0ca7ff79a51ea11c15cc10b18800d4bca09
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-01 09:23:19 +0200

    Add new feature

---
+++ Public/Invoke-PingCastle.ps1
@@ -0,0 +1,70 @@
+﻿function Invoke-PingCastle {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $FolderPath,
+        [Parameter(Mandatory)][string] $ReportPath
+    )
+    $PingCastleExecutable = [io.path]::Combine($FolderPath, 'PingCastle.exe')
+    if ($FolderPath -and (Test-Path -LiteralPath $FolderPath) -and (Test-Path -LiteralPath $PingCastleExecutable)) {
+
+    } else {
+        Write-Warning -Message "Invoke-PingCastle - FolderPath [$FolderPath] doesn't exist. Please provide path with PingCastle.exe"
+        return
+    }
+
+    if ($ReportPath -and (Test-Path -LiteralPath $ReportPath)) {
+
+    } else {
+        Write-Warning -Message "Invoke-PingCastle - ReportPath [$ReportPath] doesn't exist. Please provide path with PingCastle report"
+        return
+    }
+
+    $TemporaryReportFolder = [io.path]::Combine($Env:TEMP, 'PingCastle')
+    if (-not (Test-Path -LiteralPath $TemporaryReportFolder)) {
+        $null = New-Item -Path $TemporaryReportFolder -ItemType Directory -Force
+    }
+    if (Test-Path -LiteralPath $TemporaryReportFolder) {
+        $Items = Get-ChildItem -LiteralPath $TemporaryReportFolder -Recurse
+        foreach ($Item in $Items) {
+            Remove-Item -LiteralPath $Item.FullName -Force
+        }
+    }
+
+    try {
+        Set-Location -LiteralPath $TemporaryReportFolder -ErrorAction Stop
+    } catch {
+        Write-Warning -Message "Invoke-PingCastle - Error while switch to $TemporaryReportFolder. Error: $($_.Exception.Message)"
+        return
+    }
+
+    try {
+        & $PingCastleExecutable --healthcheck --server * --reachable
+    } catch {
+        Write-Warning -Message "Invoke-PingCastle - Error while executing $PingCastleExecutable. Error: $($_.Exception.Message)"
+        return
+    }
+
+    $AllFiles = Get-ChildItem -LiteralPath $TemporaryReportFolder
+    foreach ($File in $AllFiles) {
+        $DomainName = $File.BaseName.Replace("ad_hc_", '')
+        $Name = "PingCastle-Domain-$($DomainName)_$(Get-Date -f yyyy-MM-dd_HHmmss -Date $File.CreationTime)$($File.Extension)"
+        $DestinationPath = [io.path]::Combine($ReportPath, $Name)
+        try {
+            Move-Item -LiteralPath $File.FullName -Destination $DestinationPath -Force -ErrorAction Stop
+            [PSCustomObject] @{
+                DomainName = $DomainName
+                FilePath   = $DestinationPath
+                Error      = $null
+            }
+        } catch {
+            Write-Warning -Message "Invoke-PingCastle - Error while moving file $File to $DestinationPath. Error: $($_.Exception.Message)"
+            [PSCustomObject] @{
+                DomainName = $DomainName
+                FilePath   = $DestinationPath
+                Error      = $_.Exception.Message
+            }
+        }
+    }
+
+}
+
