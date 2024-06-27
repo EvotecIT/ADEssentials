88bf5483bc86f44346986a8900cf1fbe08e4726c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-27 18:14:02 +0200

    uncomment example

--- Examples/Example.12-GetADComputer.ps1
+++ Examples/Example.12-GetADComputer.ps1
@@ -1,20 +1,20 @@
 ﻿Import-Module $PSScriptRoot\..\ADEssentials.psd1 -Force
 
-# $Computers = @(
-#     'EVOWIN'
-#     'ADPreview2019.ad.evotec.pl'
-#     'ADRODC.ad.evotec.pl'
-#     'EVOWIN' # this will resolve as it's computer in same domain
-#     'ADTEST' # this will not resolve as it's DC in another forest
-#     'ADTEST.test.evotec.pl'
-#     'adtest@test.evotec.pl'
-#     'test.test'
-#     'test7.ad.evotec.pl'
-#     'test8.test.evotec.pl'
-# )
+$Computers = @(
+    'EVOWIN'
+    'ADPreview2019.ad.evotec.pl'
+    'ADRODC.ad.evotec.pl'
+    'EVOWIN' # this will resolve as it's computer in same domain
+    'ADTEST' # this will not resolve as it's DC in another forest
+    'ADTEST.test.evotec.pl'
+    'adtest@test.evotec.pl'
+    'test.test'
+    'test7.ad.evotec.pl'
+    'test8.test.evotec.pl'
+)
 
-# $Output = Get-WinADObject -Identity $Computers -Verbose #-IncludeAllTypes
-# $Output | Format-Table *
+$Output = Get-WinADObject -Identity $Computers -Verbose #-IncludeAllTypes
+$Output | Format-Table *
 
 $Computers = Get-WinADComputers
 $Computers.Count
