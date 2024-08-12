288feb13bb4780f39feb4f8364ff5aea24590509
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-08-12 23:09:15 +0200

    Update example

--- Examples/Example.06-TestLDAPPorts.ps1
+++ Examples/Example.06-TestLDAPPorts.ps1
@@ -1,8 +1,11 @@
 ﻿Clear-Host
 Import-Module .\ADEssentials.psd1 -Force
 
+
+Test-LDAP -IncludeDomainControllers 'ad1.ad.evotec.xyz' -Identity "krbtgt" -Verbose -RetryCount 3 | Format-list *
+
 Test-LDAP -ComputerName 'ad.evotec.xyz' -VerifyCertificate -Verbose | Format-Table
 
-Test-LDAP -VerifyCertificate -Verbose | Format-Table
+Test-LDAP -VerifyCertificate -Verbose  | Format-Table
 
 Test-LDAP -VerifyCertificate -Verbose -IncludeDomains 'ad.evotec.pl' | Format-Table
