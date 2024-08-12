291d8292865739e09393b8f52cbc1aa1543d19a0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-08-12 23:09:45 +0200

    Add example

---
+++ Examples/Example.71.SiteCoverageSettings.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+Get-WinADSiteCoverage | Format-Table *
+
+Get-WinADDomainControllerNetLogonSettings -DomainController 'AD1'
+
+Set-WinADDomainControllerNetLogonSettings -DomainController 'AD1' -SiteCoverage 'Default-First-Site-Name' -GCSiteCoverage 'Default-First-Site-Name', 'AD1' -WhatIf
+Get-WinADDomainControllerNetLogonSettings -DomainController 'AD1'
+
+Set-WinADDomainControllerNetLogonSettings -DomainController 'AD1' -SiteCoverage $null -GCSiteCoverage $null -WhatIf
