cf6c8e598348e8a0292cc5ecaf57bdd6f349fa47
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-10-29 12:48:38 +0100

    Add example

---
+++ Examples/Example.75-GetPermissionSchema.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+#$SchemaInformation = Get-WinADForestSchemaDetails #-Verbose
+#$SchemaInformation
+
+#$SchemaInformation['SchemaSummary']['File-Link-Tracking'] | Format-Table *
+#$SchemaInformation['SchemaSummary'].Values | Format-Table *
+
+Invoke-ADEssentials -Type Schema #-Verbose
