0338f32330dedd0543394fd549f7c20af2f44a42
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-06 18:46:16 +0100

    add basic report

---
+++ Private/Configuration.WinADBrokenProtectedFromDeletion.ps1
@@ -0,0 +1,21 @@
+﻿$Script:ShowWinADBrokenProtectedFromDeletion = [ordered] @{
+    Name       = ''
+    Enabled    = $true
+    Execute    = {
+        Get-WinADBrokenProtectedFromDeletion -Type All
+    }
+    Processing = {
+
+    }
+    Summary    = {
+
+    }
+    Variables  = @{
+
+    }
+    Solution   = {
+        New-HTMLTable -DataTable $Script:Reporting['BrokenProtectedFromDeletion']['Data'] -Filtering {
+            New-HTMLTableCondition -Name 'HasBrokenPermissions' -Value $true -Operator 'eq' -ComparisonType string -BackgroundColor Salmon  -FailBackgroundColor MintGreen
+        } -ScrollX
+    }
+}
