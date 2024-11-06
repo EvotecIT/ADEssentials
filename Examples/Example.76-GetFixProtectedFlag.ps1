e10d1a208c1aecc97d55ac451a3b47c80564f1f0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-06 20:04:59 +0100

    Add script to invoke ADEssentials for handling broken protected flags

---
+++ Examples/Example.76-GetFixProtectedFlag.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module .\ADEssentials.psd1 -Force
+
+#$Objects = Get-WinADBrokenProtectedFromDeletion -Verbose -Type All -LimitProcessing 3 -ReturnBrokenOnly
+#$Objects | Format-Table -AutoSize *
+
+#Repair-WinADBrokenProtectedFromDeletion -Type All -LimitProcessing 5 -Verbose -WhatIf
+
+Invoke-ADEssentials -Type BrokenProtectedFromDeletion #-Verbose
