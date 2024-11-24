ce89a49248dcb98b0af3628a783b10c24468502d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-24 13:22:46 +0100

    Refactor Example.17-ShowADTrusts.ps1 to store Show-WinADTrust output in a variable for improved data handling

--- Examples/Example.17-ShowADTrusts.ps1
+++ Examples/Example.17-ShowADTrusts.ps1
@@ -1,6 +1,6 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-Show-WinADTrust -Online -FilePath $PSScriptRoot\Reports\Trusts.html -Verbose
+$Trusts = Show-WinADTrust -Online -FilePath $PSScriptRoot\Reports\Trusts.html -Verbose -PassThru
 
 $Trusts.AdditionalInformation | Format-Table
 $Trusts.AdditionalInformation.msDSTrustForestTrustInfo | Format-Table
@@ -8,5 +8,3 @@ $Trusts.AdditionalInformation.SuffixesInclude | Format-Table
 $Trusts.AdditionalInformation.SuffixesExclude | Format-Table
 $Trusts.AdditionalInformation.TrustObject | Format-Table
 $Trusts.AdditionalInformation.GroupExists | Format-Table
-
-Show-WinADTrust -Online -FilePath $PSScriptRoot\Reports\Trusts.html
