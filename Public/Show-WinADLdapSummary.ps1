3e76de1d557cabfb27badabb10413ae589238822
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-24 17:36:52 +0100

    Add Show-WinADLdapSummary function for detailed LDAP server reporting and visualization

---
+++ Public/Show-WinADLdapSummary.ps1
@@ -0,0 +1,99 @@
+﻿function Show-WinADLdapSummary {
+    [CmdletBinding()]
+    param(
+        [alias('ForestName')][string] $Forest,
+        [string[]] $ExcludeDomains,
+        [string[]] $ExcludeDomainControllers,
+        [alias('Domain', 'Domains')][string[]] $IncludeDomains,
+        [alias('DomainControllers')][string[]] $IncludeDomainControllers,
+        [switch] $SkipRODC,
+        $Identity,
+        [int] $RetryCount = 3,
+        [string] $FilePath,
+        [switch] $Online,
+        [switch] $HideHTML,
+        [switch] $PassThru
+    )
+
+    $Script:Reporting = [ordered] @{}
+    $Script:Reporting['Version'] = Get-GitHubVersion -Cmdlet 'Invoke-ADEssentials' -RepositoryOwner 'evotecit' -RepositoryName 'ADEssentials'
+
+    if ($FilePath -eq '') {
+        $FilePath = Get-FileName -Extension 'html' -Temporary
+    }
+
+    $getWinADLDAPSummarySplat = @{
+        IncludeDomains           = $IncludeDomains
+        ExcludeDomains           = $ExcludeDomains
+        IncludeDomainControllers = $IncludeDomainControllers
+        ExcludeDomainControllers = $ExcludeDomainControllers
+        SkipRODC                 = $SkipRODC
+        Identity                 = $Identity
+        RetryCount               = $RetryCount
+        Forest                   = $Forest
+        Extended                 = $true
+    }
+
+    $Output = Get-WinADLDAPSummary @getWinADLDAPSummarySplat
+
+    New-HTML {
+        New-HTMLSectionStyle -BorderRadius 0px -HeaderBackGroundColor Grey -RemoveShadow
+        New-HTMLTableOption -DataStore JavaScript -ArrayJoin -ArrayJoinString "," -BoolAsString
+        New-HTMLTabStyle -BorderRadius 0px -TextTransform capitalize -BackgroundColorActive SlateGrey
+
+        New-HTMLHeader {
+            New-HTMLSection -Invisible {
+                New-HTMLSection {
+                    New-HTMLText -Text "Report generated on $(Get-Date)" -Color Blue
+                } -JustifyContent flex-start -Invisible
+                New-HTMLSection {
+                    New-HTMLText -Text "ADEssentials - $($Script:Reporting['Version'])" -Color Blue
+                } -JustifyContent flex-end -Invisible
+            }
+        }
+
+        New-HTMLSection -HeaderText "LDAP Summary" {
+            New-HTMLPanel -Invisible {
+                New-HTMLText -Text "Summary for $($Output.Count) servers" -Color Blue -FontSize 10pt -FontWeight bold
+
+                New-HTMLList -FontSize 10pt {
+                    New-HTMLListItem -Text "Servers with no issues: ", $($Output.GoodServers.Count) -Color None, LightGreen -FontWeight normal, bold
+                    New-HTMLListItem -Text "Servers with issues: ", $($Output.FailedServersCount) -Color None, Salmon -FontWeight normal, bold
+                }
+
+                New-HTMLText -Text "Servers certificate summary" -Color Blue -FontSize 10pt -FontWeight bold
+
+                New-HTMLList -FontSize 10pt {
+                    New-HTMLListItem -Text "Servers with certificate expiring More Than 30 Days: ", $($Output.ServersExpiringMoreThan30Days.Count) -FontWeight normal, bold
+                    New-HTMLListItem -Text "Servers with certificate expiring In 30 Days: ", $($Output.ServersExpiringIn30Days.Count) -FontWeight normal, bold
+                    New-HTMLListItem -Text "Servers with certificate expiring In 15 Days: ", $($Output.ServersExpiringIn15Days.Count) -FontWeight normal, bold
+                    New-HTMLListItem -Text "Servers with certificate expiring In 7 Days: ", $($Output.ServersExpiringIn7Days.Count) -FontWeight normal, bold
+                    New-HTMLListItem -Text "Servers with certificate expiring In 3 Days Or Less: ", $($Output.ServersExpiringIn3DaysOrLess.Count) -FontWeight normal, bold
+                    New-HTMLListItem -Text "Servers with certificate expired: ", $($Output.ServersExpired.Count) -FontWeight normal, bold
+                }
+            }
+            New-HTMLPanel {
+                New-HTMLPanel {
+                    New-HTMLChart {
+                        New-ChartPie -Name 'Servers with no issues' -Value $($Output.GoodServers.Count) -Color LightGreen
+                        New-ChartPie -Name 'Servers with issues' -Value $($Output.FailedServersCount) -Color Salmon
+                    } -Title 'Servers status' -TitleColor Dandelion
+                }
+            }
+        }
+
+        New-HTMLSection -HeaderText "LDAP Servers" {
+            New-HTMLTable -DataTable $Output.List -Filtering {
+                New-HTMLTableCondition -Name 'StatusDate' -ComparisonType string -Operator eq -Value 'OK' -BackgroundColor LightGreen -FailBackgroundColor Salmon
+                New-HTMLTableCondition -Name 'X509DnsNameStatus' -ComparisonType string -Operator eq -Value 'OK' -BackgroundColor LightGreen -FailBackgroundColor Salmon -HighlightHeaders 'X509DnsNameStatus', 'X509DnsNameList'
+                New-HTMLTableCondition -Name 'StatusPorts' -ComparisonType string -Operator eq -Value 'OK' -BackgroundColor LightGreen -FailBackgroundColor Salmon
+                New-HTMLTableCondition -Name 'StatusIdentity' -ComparisonType string -Operator eq -Value 'OK' -BackgroundColor LightGreen -FailBackgroundColor Salmon
+
+            } -DataTableID 'DT-LDAPSummary' -ScrollX -WarningAction SilentlyContinue
+        }
+    } -FilePath $FilePath -ShowHTML:(-not $HideHTML)
+
+    if ($PassThru) {
+        $Output
+    }
+}
