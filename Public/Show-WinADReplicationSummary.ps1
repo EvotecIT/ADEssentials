f1f6af56eb140c78bca9064fac9b2570e17b00ad
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-24 17:37:14 +0100

    Add Show-WinADReplicationSummary function for detailed Active Directory replication reporting

---
+++ Public/Show-WinADReplicationSummary.ps1
@@ -0,0 +1,61 @@
+﻿function Show-WinADReplicationSummary {
+    [CmdletBinding()]
+    param(
+        [string] $FilePath,
+        [switch] $Online,
+        [switch] $HideHTML,
+        [switch] $PassThru
+    )
+    $Script:Reporting = [ordered] @{}
+    $Script:Reporting['Version'] = Get-GitHubVersion -Cmdlet 'Invoke-ADEssentials' -RepositoryOwner 'evotecit' -RepositoryName 'ADEssentials'
+
+    if ($FilePath -eq '') {
+        $FilePath = Get-FileName -Extension 'html' -Temporary
+    }
+
+    $ReplicationSummary = Get-WinADForestReplicationSummary -IncludeStatisticsVariable Statistics
+
+    New-HTML {
+        New-HTMLSectionStyle -BorderRadius 0px -HeaderBackGroundColor Grey -RemoveShadow
+        New-HTMLTableOption -DataStore JavaScript -ArrayJoin -ArrayJoinString "," -BoolAsString
+        New-HTMLTabStyle -BorderRadius 0px -TextTransform capitalize -BackgroundColorActive SlateGrey
+
+        New-HTMLHeader {
+            New-HTMLSection -Invisible {
+                New-HTMLSection {
+                    New-HTMLText -Text "Report generated on $(Get-Date)" -Color Blue
+                } -JustifyContent flex-start -Invisible
+                New-HTMLSection {
+                    New-HTMLText -Text "ADEssentials - $($Script:Reporting['Version'])" -Color Blue
+                } -JustifyContent flex-end -Invisible
+            }
+        }
+
+        New-HTMLSection -HeaderText "Summary" {
+            New-HTMLList {
+                New-HTMLListItem -Text "Servers with good replication: ", $($Statistics.Good) -Color Black, SpringGreen -FontWeight normal, bold
+                New-HTMLListItem -Text "Servers with replication failures: ", $($Statistics.Failures) -Color Black, Red -FontWeight normal, bold
+                New-HTMLListItem -Text "Servers with replication delta over 24 hours: ", $($Statistics.DeltaOver24Hours) -Color Black, Red -FontWeight normal, bold
+                New-HTMLListItem -Text "Servers with replication delta over 12 hours: ", $($Statistics.DeltaOver12Hours) -Color Black, Red -FontWeight normal, bold
+                New-HTMLListItem -Text "Servers with replication delta over 6 hours: ", $($Statistics.DeltaOver6Hours) -Color Black, Red -FontWeight normal, bold
+                New-HTMLListItem -Text "Servers with replication delta over 3 hours: ", $($Statistics.DeltaOver3Hours) -Color Black, Red -FontWeight normal, bold
+                New-HTMLListItem -Text "Servers with replication delta over 1 hour: ", $($Statistics.DeltaOver1Hours) -Color Black, Red -FontWeight normal, bold
+                New-HTMLListItem -Text "Unique replication errors: ", $($Statistics.UniqueErrors.Count) -Color Black, Red -FontWeight normal, bold
+            }
+        }
+
+        New-HTMLSection -HeaderText "Replication Summary" {
+            New-HTMLTable -DataTable $ReplicationSummary -DataTableID 'DT-ReplicationSummary' -ScrollX {
+                New-HTMLTableCondition -Inline -Name "Fail" -HighlightHeaders 'Fails', 'Total', 'PercentageError' -ComparisonType number -Operator gt 0 -BackgroundColor Salmon -FailBackgroundColor SpringGreen
+            } -Filtering -PagingLength 50 -PagingOptions @(5, 10, 15, 25, 50, 100)
+        }
+    } -FilePath $FilePath -ShowHTML:(-not $HideHTML)
+
+
+    if ($PassThru) {
+        [ordered] @{
+            ReplicationSummary = $ReplicationSummary
+            Statistics         = $Statistics
+        }
+    }
+}
