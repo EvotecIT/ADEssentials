6fefe63aa77f73aca008a649a367894a301dc168
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-02-09 13:48:17 +0100

    Fix Get-WinADSIDHistory function to use correct Get-WinADTrust command
    - Updated command from Get-WinADTrusts to Get-WinADTrust for accuracy

--- Public/Get-WinADSIDHistory.ps1
+++ Public/Get-WinADSIDHistory.ps1
@@ -78,7 +78,7 @@
     }
 
     # Lets find out all trusts in the forest
-    $Output['Trusts'] = Get-WinADTrusts -Forest $Forest -Recursive
+    $Output['Trusts'] = Get-WinADTrust -Forest $Forest -Recursive
 
     # Lets find out all SIDs that are in the forest and in trusts
     $DomainSIDs = [ordered]@{}
