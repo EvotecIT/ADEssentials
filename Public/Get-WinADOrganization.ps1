118d9223c7dc4d951d28abfee96db1e953819969
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-04-11 19:09:22 +0200

    feat(Get-WinADOrganization): ✨ add Get-WinADOrganization function
    - Introduced `Get-WinADOrganization` function to retrieve and structure Active Directory organization details.
    - Collects forest information, domains, and organizational units.
    - Enhances the ability to visualize and manage Active Directory structures.

---
+++ Public/Get-WinADOrganization.ps1
@@ -0,0 +1,50 @@
+﻿function Get-WinADOrganization {
+    [cmdletBinding()]
+    param(
+
+    )
+
+    $ForestInformation = Get-WinADForestDetails
+    $Organization = [ordered] @{
+        Forest              = [System.Collections.Generic.List[PSCustomObject]]::new()
+        Domains             = [System.Collections.Generic.List[PSCustomObject]]::new()
+        OrganizationalUnits = [ordered] @{}
+    }
+
+    foreach ($Domain in $ForestInformation.Domains) {
+        $CurrentDomainDN = ConvertTo-DistinguishedName -CanonicalName $Domain -ToDomain
+        $Organization.Domains.Add(
+            [PSCustomObject] @{
+                Domain                   = $Domain
+                Type                     = 'Domain'
+                DistinguishedName        = $CurrentDomainDN
+                Name                     = $Domain
+                OrganizationalUnits      = @()
+                OrganizationalUnitsCount = 0
+            }
+        )
+        $Organization.OrganizationalUnits[$Domain] = @(
+            $DomainOUs = Get-ADOrganizationalUnit -Filter "*" -Server $ForestInformation['QueryServers'][$Domain].HostName[0] -Properties DistinguishedName, CanonicalName
+            foreach ($OU in $DomainOUs) {
+                $SubOus = ConvertFrom-DistinguishedName -DistinguishedName $OU.DistinguishedName -ToMultipleOrganizationalUnit
+                [Array] $OutputSubOu = @(
+                    if ($SubOus) {
+                        $SubOus
+                    }
+                    $CurrentDomainDN
+                )
+                $OUData = [PSCustomObject]@{
+                    Domain                   = $Domain
+                    Type                     = 'OrganizationalUnit'
+                    DistinguishedName        = $OU.DistinguishedName
+                    Name                     = $OU.Name
+                    OrganizationalUnits      = $OutputSubOu
+                    OrganizationalUnitsCount = $OutputSubOu.Count
+                }
+
+                $OUData
+            }
+        )
+    }
+    $Organization
+}
