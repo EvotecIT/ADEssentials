9f9d7b2e33f0cc8e7293296039404d465b9075bd
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-04-13 17:16:02 +0200

    feat(Get-WinADOrganization): ✨ enhance organization data structure
    - Added detailed object counts for users, computers, groups, contacts, and others.
    - Improved the handling of organizational units in the `Show-WinADOrganization` function.
    - Updated HTML report generation to include tabs for Subnets and Sites.

--- Public/Get-WinADOrganization.ps1
+++ Public/Get-WinADOrganization.ps1
@@ -3,26 +3,88 @@
     param(
 
     )
-
     $ForestInformation = Get-WinADForestDetails
     $Organization = [ordered] @{
-        Forest              = [System.Collections.Generic.List[PSCustomObject]]::new()
-        Domains             = [System.Collections.Generic.List[PSCustomObject]]::new()
-        OrganizationalUnits = [ordered] @{}
+        Forest                = [System.Collections.Generic.List[PSCustomObject]]::new()
+        Domains               = [System.Collections.Generic.List[PSCustomObject]]::new()
+        OrganizationalUnits   = [ordered] @{}
+        Objects               = [ordered] @{}
+        ObjectsCount          = [ordered] @{}
+        ObjectsUsersCount     = [ordered] @{}
+        ObjectsComputersCount = [ordered] @{}
+        ObjectsGroupsCount    = [ordered] @{}
+        ObjectsContactsCount  = [ordered] @{}
+        ObjectsOtherCount     = [ordered] @{}
     }
 
     foreach ($Domain in $ForestInformation.Domains) {
         $CurrentDomainDN = ConvertTo-DistinguishedName -CanonicalName $Domain -ToDomain
-        $Organization.Domains.Add(
-            [PSCustomObject] @{
-                Domain                   = $Domain
-                Type                     = 'Domain'
-                DistinguishedName        = $CurrentDomainDN
-                Name                     = $Domain
-                OrganizationalUnits      = @()
-                OrganizationalUnitsCount = 0
-            }
+
+        $DomainInformation = [ordered] @{
+            Domain                   = $Domain
+            Type                     = 'Domain'
+            DistinguishedName        = $CurrentDomainDN
+            Name                     = $Domain
+            OrganizationalUnits      = @()
+            OrganizationalUnitsCount = 0
+            ObjectsCount             = 0
+            ObjectsUsersCount        = 0
+            ObjectsComputersCount    = 0
+            ObjectsGroupsCount       = 0
+            ObjectsContactsCount     = 0
+            ObjectsOtherCount        = 0
+        }
+
+        $ValidObjectClasses = @(
+            'user', 'computer', 'group', 'contact',
+            'msDS-GroupManagedServiceAccount', 'msDS-ManagedServiceAccount',
+            'printer', 'volume', 'foreignSecurityPrincipal',
+            'inetOrgPerson', 'sharedFolder'
         )
+
+        $Filter = ($ValidObjectClasses | ForEach-Object { "(ObjectClass -eq '$_')" }) -join ' -or '
+
+        $getADObjectSplat = @{
+            Filter     = $Filter
+            Server     = $ForestInformation['QueryServers'][$Domain].HostName[0]
+            Properties = 'DistinguishedName', 'CanonicalName', 'ObjectClass', 'Name'
+        }
+
+        $Objects = Get-ADObject @getADObjectSplat
+
+        foreach ($Object in $Objects) {
+            $DN = ConvertFrom-DistinguishedName -DistinguishedName $Object.DistinguishedName -ToOrganizationalUnit
+            if (-not $Organization['Objects'][$DN]) {
+                $Organization['Objects'][$DN] = [System.Collections.Generic.List[PSCustomObject]]::new()
+                $Organization['ObjectsCount'][$DN] = 0
+                $Organization['ObjectsUsersCount'][$DN] = 0
+                $Organization['ObjectsComputersCount'][$DN] = 0
+                $Organization['ObjectsGroupsCount'][$DN] = 0
+                $Organization['ObjectsContactsCount'][$DN] = 0
+                $Organization['ObjectsOtherCount'][$DN] = 0
+            }
+            $Organization['Objects'][$DN].Add($Object)
+            if ($Object.ObjectClass -eq 'user') {
+                $Organization['ObjectsUsersCount'][$DN]++
+                $DomainInformation['ObjectsUsersCount']++
+            } elseif ($Object.ObjectClass -eq 'computer') {
+                $Organization['ObjectsComputersCount'][$DN]++
+                $DomainInformation['ObjectsComputersCount']++
+            } elseif ($Object.ObjectClass -eq 'group') {
+                $Organization['ObjectsGroupsCount'][$DN]++
+                $DomainInformation['ObjectsGroupsCount']++
+            } elseif ($Object.ObjectClass -eq 'contact') {
+                $Organization['ObjectsContactsCount'][$DN]++
+                $DomainInformation['ObjectsContactsCount']++
+            } else {
+                $Organization['ObjectsOtherCount'][$DN]++
+            }
+            $Organization['ObjectsCount'][$DN]++
+            $DomainInformation['ObjectsCount']++
+        }
+
+        $Organization.Domains.Add([PSCustomObject]$DomainInformation)
+
         $Organization.OrganizationalUnits[$Domain] = @(
             $DomainOUs = Get-ADOrganizationalUnit -Filter "*" -Server $ForestInformation['QueryServers'][$Domain].HostName[0] -Properties DistinguishedName, CanonicalName
             foreach ($OU in $DomainOUs) {
@@ -40,6 +102,12 @@
                     Name                     = $OU.Name
                     OrganizationalUnits      = $OutputSubOu
                     OrganizationalUnitsCount = $OutputSubOu.Count
+                    ObjectsCount             = $Organization['Objects'][$OU.DistinguishedName].Count
+                    ObjectsUsersCount        = if ($null -eq $Organization['ObjectsUsersCount'][$OU.DistinguishedName]) { 0 } else { $Organization['ObjectsUsersCount'][$OU.DistinguishedName] }
+                    ObjectsComputersCount    = if ($null -eq $Organization['ObjectsComputersCount'][$OU.DistinguishedName]) { 0 } else { $Organization['ObjectsComputersCount'][$OU.DistinguishedName] }
+                    ObjectsGroupsCount       = if ($null -eq $Organization['ObjectsGroupsCount'][$OU.DistinguishedName]) { 0 } else { $Organization['ObjectsGroupsCount'][$OU.DistinguishedName] }
+                    ObjectsContactsCount     = if ($null -eq $Organization['ObjectsContactsCount'][$OU.DistinguishedName]) { 0 } else { $Organization['ObjectsContactsCount'][$OU.DistinguishedName] }
+                    ObjectsOtherCount        = if ($null -eq $Organization['ObjectsOtherCount'][$OU.DistinguishedName]) { 0 } else { $Organization['ObjectsOtherCount'][$OU.DistinguishedName] }
                 }
 
                 $OUData
