e51bc4c49901facb6094dc93bd8eed9f05b672c7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-04-14 15:17:54 +0200

    feat(Show-WinADOrganization): ✨ add parameters for diagram options
    * Introduced `AddDiagramStandard`, `AddDiagramHierarchical`, and `Online` parameters to enhance report generation.
    * Updated the cmdlet to conditionally include standard and hierarchical diagrams based on user input.
    * Improved overall functionality for visualizing Active Directory organizational structures.

--- Public/Show-WinADOrganization.ps1
+++ Public/Show-WinADOrganization.ps1
@@ -12,6 +12,22 @@
     .PARAMETER FilePath
     The path to save the HTML report. If not specified, a temporary file is used.
 
+    .PARAMETER HideHTML
+    Hides the HTML report after generation. This is useful for generating reports without displaying them in a browser.
+
+    .PARAMETER AddDiagramStandard
+    Adds a standard diagram of the organizational units and their relationships to the report.
+    It's important to understand that large environments may take a long time to generate the diagram if they even work.
+    Test before using it in production.
+
+    .PARAMETER AddDiagramHierarchical
+    Adds a hierarchical diagram of the organizational units and their relationships to the report.
+    It's important to understand that large environments may take a long time to generate the diagram if they even work.
+    Test before using it in production.
+
+    .PARAMETER Online
+    Switch to indicate if the report should be generated with online resources.
+
     .EXAMPLE
     Show-WinADOrganization -FilePath "C:\Reports\AD Organization Report.html"
 
@@ -22,7 +38,10 @@
     param(
         [ScriptBlock] $Conditions,
         [string] $FilePath,
-        [switch] $HideHTML
+        [switch] $HideHTML,
+        [switch] $AddDiagramStandard,
+        [switch] $AddDiagramHierarchical,
+        [switch] $Online
     )
 
     $Script:Reporting = [ordered] @{}
@@ -53,89 +72,93 @@
             }
         }
 
-        New-HTMLTabPanel {
-            New-HTMLTab -TabName 'Standard' {
-                New-HTMLSection -HeaderText 'Organization Diagram' {
-                    $Duplicates = [ordered] @{}
-                    New-HTMLDiagram -Height 'calc(50vh)' {
-                        New-DiagramEvent -ID 'DT-OrganizationalUnits' -ColumnID 2
-                        New-DiagramNode -Label 'Active Directory Forest' -Id 'Forest' -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -ImageType squareImage
-                        foreach ($Domain in $Organization.Domains) {
-                            New-DiagramNode -Label $Domain.Name -Id $Domain.DistinguishedName -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -ImageType squareImage
-                            New-DiagramEdge -From 'Forest' -To $Domain.DistinguishedName -Color Blue -ArrowsToEnabled -Dashes
-                        }
-                        foreach ($Domain in $Organization.OrganizationalUnits.Keys) {
-                            foreach ($OU in $Organization.OrganizationalUnits[$Domain]) {
-                                New-DiagramNode -Id $OU.DistinguishedName -Label $OU.Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png' -ImageType squareImage
-                                if ($OU.OrganizationalUnits.Count -gt 0) {
-                                    $TopOU = $OU.DistinguishedName
-                                    foreach ($Sub in $OU.OrganizationalUnits) {
-                                        #$Name = ConvertFrom-DistinguishedName -DistinguishedName $Sub -ToLastName
-                                        #New-DiagramNode -Id $Sub -Label $Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png'
-                                        if (-not $Duplicates[$TopOU]) {
-                                            New-DiagramEdge -From $TopOU -To $Sub -Color Blue -ArrowsToEnabled -Dashes
-                                            $Duplicates[$TopOU] = $true
-                                        }
-                                        $TopOU = $Sub
-                                    }
+        if ($AddDiagramStandard -or $AddDiagramHierarchical) {
+            New-HTMLTabPanel {
+                if ($AddDiagramStandard) {
+                    New-HTMLTab -TabName 'Standard' {
+                        New-HTMLSection -HeaderText 'Organization Diagram' {
+                            $Duplicates = [ordered] @{}
+                            New-HTMLDiagram -Height 'calc(50vh)' {
+                                New-DiagramEvent -ID 'DT-OrganizationalUnits' -ColumnID 2
+                                New-DiagramNode -Label 'Active Directory Forest' -Id 'Forest' -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -ImageType squareImage
+                                foreach ($Domain in $Organization.Domains) {
+                                    New-DiagramNode -Label $Domain.Name -Id $Domain.DistinguishedName -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -ImageType squareImage
+                                    New-DiagramEdge -From 'Forest' -To $Domain.DistinguishedName -Color Blue -ArrowsToEnabled -Dashes
                                 }
-                            }
-                        }
-                    } -EnableFiltering -EnableFilteringButton
-                }
-            }
-            New-HTMLTab -TabName 'Hierarchical' {
-                New-HTMLSection -HeaderText 'Organization Diagram' {
-                    $Duplicates = [ordered] @{}
-                    New-HTMLDiagram -Height 'calc(50vh)' {
-                        #New-DiagramOptionsLayout -HierarchicalEnabled $true
-                        New-DiagramEvent -ID 'DT-OrganizationalUnits' -ColumnID 2
-                        #New-DiagramOptionsPhysics -RepulsionNodeDistance 200 -Solver repulsion
-                        #New-DiagramOptionsPhysics -Enabled $true -HierarchicalRepulsionAvoidOverlap 1.00
-                        New-DiagramOptionsLayout -ImprovedLayout $true -HierarchicalEnabled $true -HierarchicalDirection FromUpToDown -HierarchicalNodeSpacing 280 #-HierarchicalSortMethod directed -HierarchicalShakeTowards leaves
-                        New-DiagramOptionsPhysics -Enabled $false
-
-                        New-DiagramNode -Label 'Active Directory Forest' -Id 'Forest' -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -Leve 0 -ImageType squareImage
-                        foreach ($Domain in $Organization.Domains) {
-                            New-DiagramNode -Label $Domain.Name -Id $Domain.DistinguishedName -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -Level 1 -ImageType squareImage
-                            New-DiagramEdge -From 'Forest' -To $Domain.DistinguishedName -Color Blue -ArrowsToEnabled -Dashes
-                        }
-                        foreach ($Domain in $Organization.OrganizationalUnits.Keys) {
-                            foreach ($OU in $Organization.OrganizationalUnits[$Domain]) {
-                                New-DiagramNode -Id $OU.DistinguishedName -Label $OU.Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png' -Level ($OU.OrganizationalUnitsCount + 2) -ImageType squareImage
-                                if ($OU.OrganizationalUnits.Count -gt 0) {
-                                    $TopOU = $OU.DistinguishedName
-                                    foreach ($Sub in $OU.OrganizationalUnits) {
-                                        #$Name = ConvertFrom-DistinguishedName -DistinguishedName $Sub -ToLastName
-                                        #New-DiagramNode -Id $Sub -Label $Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png'
-                                        if (-not $Duplicates[$TopOU]) {
-                                            $newDiagramEdgeSplat = @{
-                                                From            = $TopOU
-                                                To              = $Sub
-                                                Color           = 'Blue'
-                                                ArrowsToEnabled = $true
-                                                Dashes          = $true
-                                                ColorOpacity    = 0.7
+                                foreach ($Domain in $Organization.OrganizationalUnits.Keys) {
+                                    foreach ($OU in $Organization.OrganizationalUnits[$Domain]) {
+                                        New-DiagramNode -Id $OU.DistinguishedName -Label $OU.Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png' -ImageType squareImage
+                                        if ($OU.OrganizationalUnits.Count -gt 0) {
+                                            $TopOU = $OU.DistinguishedName
+                                            foreach ($Sub in $OU.OrganizationalUnits) {
+                                                #$Name = ConvertFrom-DistinguishedName -DistinguishedName $Sub -ToLastName
+                                                #New-DiagramNode -Id $Sub -Label $Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png'
+                                                if (-not $Duplicates[$TopOU]) {
+                                                    New-DiagramEdge -From $TopOU -To $Sub -Color Blue -ArrowsToEnabled -Dashes
+                                                    $Duplicates[$TopOU] = $true
+                                                }
+                                                $TopOU = $Sub
                                             }
-
-                                            New-DiagramEdge @newDiagramEdgeSplat
-                                            $Duplicates[$TopOU] = $true
                                         }
-                                        $TopOU = $Sub
                                     }
                                 }
-                            }
+                            } -EnableFiltering -EnableFilteringButton
                         }
-                    } -EnableFiltering -EnableFilteringButton
+                    }
                 }
+                if ($AddDiagramHierarchical) {
+                    New-HTMLTab -TabName 'Hierarchical' {
+                        New-HTMLSection -HeaderText 'Organization Diagram' {
+                            $Duplicates = [ordered] @{}
+                            New-HTMLDiagram -Height 'calc(50vh)' {
+                                #New-DiagramOptionsLayout -HierarchicalEnabled $true
+                                New-DiagramEvent -ID 'DT-OrganizationalUnits' -ColumnID 2
+                                #New-DiagramOptionsPhysics -RepulsionNodeDistance 200 -Solver repulsion
+                                #New-DiagramOptionsPhysics -Enabled $true -HierarchicalRepulsionAvoidOverlap 1.00
+                                New-DiagramOptionsLayout -ImprovedLayout $true -HierarchicalEnabled $true -HierarchicalDirection FromUpToDown -HierarchicalNodeSpacing 280 #-HierarchicalSortMethod directed -HierarchicalShakeTowards leaves
+                                New-DiagramOptionsPhysics -Enabled $false
 
+                                New-DiagramNode -Label 'Active Directory Forest' -Id 'Forest' -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -Leve 0 -ImageType squareImage
+                                foreach ($Domain in $Organization.Domains) {
+                                    New-DiagramNode -Label $Domain.Name -Id $Domain.DistinguishedName -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -Level 1 -ImageType squareImage
+                                    New-DiagramEdge -From 'Forest' -To $Domain.DistinguishedName -Color Blue -ArrowsToEnabled -Dashes
+                                }
+                                foreach ($Domain in $Organization.OrganizationalUnits.Keys) {
+                                    foreach ($OU in $Organization.OrganizationalUnits[$Domain]) {
+                                        New-DiagramNode -Id $OU.DistinguishedName -Label $OU.Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png' -Level ($OU.OrganizationalUnitsCount + 2) -ImageType squareImage
+                                        if ($OU.OrganizationalUnits.Count -gt 0) {
+                                            $TopOU = $OU.DistinguishedName
+                                            foreach ($Sub in $OU.OrganizationalUnits) {
+                                                #$Name = ConvertFrom-DistinguishedName -DistinguishedName $Sub -ToLastName
+                                                #New-DiagramNode -Id $Sub -Label $Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png'
+                                                if (-not $Duplicates[$TopOU]) {
+                                                    $newDiagramEdgeSplat = @{
+                                                        From            = $TopOU
+                                                        To              = $Sub
+                                                        Color           = 'Blue'
+                                                        ArrowsToEnabled = $true
+                                                        Dashes          = $true
+                                                        ColorOpacity    = 0.7
+                                                    }
+
+                                                    New-DiagramEdge @newDiagramEdgeSplat
+                                                    $Duplicates[$TopOU] = $true
+                                                }
+                                                $TopOU = $Sub
+                                            }
+                                        }
+                                    }
+                                }
+                            } -EnableFiltering -EnableFilteringButton
+                        }
+
+                    }
+                }
             }
         }
-
         New-HTMLTabPanel {
             New-HTMLTab -Name "💡Organizational Units" {
                 New-HTMLSection -Title "Organizational Units" {
-
                     $OrganizationalUnits = @(
                         foreach ($Domain in $Organization.Domains) {
                             $Domain
@@ -147,16 +170,16 @@
                     New-HTMLTable -DataTable $OrganizationalUnits -DataTableID 'DT-OrganizationalUnits' -Filtering -ScrollX -ExcludeProperty 'Objects', 'OrganizationalUnits', 'OrganizationalUnitsCount'
                 }
             }
-            New-HTMLTab -Name "Subnets" {
+            New-HTMLTab -Name "🛜Subnets" {
                 New-HTMLSection -Title "Subnets" {
                     New-HTMLTable -DataTable $Subnets -Filtering -ScrollX
                 }
             }
-            New-HTMLTab -Name "Sites" {
+            New-HTMLTab -Name "🪑Sites" {
                 New-HTMLSection -Title "Sites" {
                     New-HTMLTable -DataTable $Sites -DataTableID 'DT-Sites' -Filtering -ScrollX
                 }
             }
         }
-    } -ShowHTML:(-not $HideHTML) -FilePath $FilePath -Online
+    } -ShowHTML:(-not $HideHTML) -FilePath $FilePath -Online:$Online.IsPresent
 }
