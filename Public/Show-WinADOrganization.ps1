c3e889a4a36f555d8748ac7369533fa2ceed8843
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-04-14 16:57:39 +0200

    Update

--- Public/Show-WinADOrganization.ps1
+++ Public/Show-WinADOrganization.ps1
@@ -1,10 +1,10 @@
 ﻿function Show-WinADOrganization {
     <#
     .SYNOPSIS
-    Generates a detailed HTML report on the organizational units and their relationships within a specified Active Directory forest.
+    Generates a detailed HTML report on the domains, containers, organizational units and their relationships within a specified Active Directory forest.
 
     .DESCRIPTION
-    This cmdlet creates a comprehensive HTML report that includes a diagram of the organizational units and their relationships, as well as a table with detailed information about the organizational units.
+    This cmdlet creates a comprehensive HTML report that includes a diagram of the domains, containers, organizational units and their relationships, as well as tables with detailed information about each component of the organizational structure.
 
     .PARAMETER Conditions
     Specifies the conditions to filter the organizational units and their relationships. This can be a script block that returns a boolean value.
@@ -79,12 +79,29 @@
                         New-HTMLSection -HeaderText 'Organization Diagram' {
                             $Duplicates = [ordered] @{}
                             New-HTMLDiagram -Height 'calc(50vh)' {
-                                New-DiagramEvent -ID 'DT-OrganizationalUnits' -ColumnID 2
-                                New-DiagramNode -Label 'Active Directory Forest' -Id 'Forest' -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -ImageType squareImage
+                                New-DiagramEvent -ID 'DT-OrganizationalUnits' -ColumnID 2 New-DiagramNode -Label 'Active Directory Forest' -ID 'Forest' -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -ImageType squareImage
                                 foreach ($Domain in $Organization.Domains) {
                                     New-DiagramNode -Label $Domain.Name -Id $Domain.DistinguishedName -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -ImageType squareImage
                                     New-DiagramEdge -From 'Forest' -To $Domain.DistinguishedName -Color Blue -ArrowsToEnabled -Dashes
                                 }
+                                # # Add Container nodes
+                                # foreach ($Domain in $Organization.Containers.Keys) {
+                                #     foreach ($Container in $Organization.Containers[$Domain]) {
+                                #         New-DiagramNode -Id $Container.DistinguishedName -Label $Container.Name -Image 'https://cdn-icons-png.flaticon.com/512/4725/4725970.png' -ImageType squareImage
+                                #         # Connect container to its domain
+                                #         if ($Container.ParentContainers.Count -gt 0) {
+                                #             $TopContainer = $Container.DistinguishedName
+                                #             foreach ($Parent in $Container.ParentContainers) {
+                                #                 if (-not $Duplicates[$TopContainer]) {
+                                #                     New-DiagramEdge -From $TopContainer -To $Parent -Color Green -ArrowsToEnabled -Dashes
+                                #                     $Duplicates[$TopContainer] = $true
+                                #                 }
+                                #                 $TopContainer = $Parent
+                                #             }
+                                #         }
+                                #     }
+                                # }
+                                # Add OU nodes
                                 foreach ($Domain in $Organization.OrganizationalUnits.Keys) {
                                     foreach ($OU in $Organization.OrganizationalUnits[$Domain]) {
                                         New-DiagramNode -Id $OU.DistinguishedName -Label $OU.Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png' -ImageType squareImage
@@ -116,13 +133,36 @@
                                 #New-DiagramOptionsPhysics -RepulsionNodeDistance 200 -Solver repulsion
                                 #New-DiagramOptionsPhysics -Enabled $true -HierarchicalRepulsionAvoidOverlap 1.00
                                 New-DiagramOptionsLayout -ImprovedLayout $true -HierarchicalEnabled $true -HierarchicalDirection FromUpToDown -HierarchicalNodeSpacing 280 #-HierarchicalSortMethod directed -HierarchicalShakeTowards leaves
-                                New-DiagramOptionsPhysics -Enabled $false
-
-                                New-DiagramNode -Label 'Active Directory Forest' -Id 'Forest' -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -Leve 0 -ImageType squareImage
+                                New-DiagramOptionsPhysics -Enabled $false New-DiagramNode -Label 'Active Directory Forest' -Id 'Forest' -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -Leve 0 -ImageType squareImage
                                 foreach ($Domain in $Organization.Domains) {
                                     New-DiagramNode -Label $Domain.Name -Id $Domain.DistinguishedName -Image 'https://cdn-icons-png.flaticon.com/512/6329/6329785.png' -Level 1 -ImageType squareImage
                                     New-DiagramEdge -From 'Forest' -To $Domain.DistinguishedName -Color Blue -ArrowsToEnabled -Dashes
                                 }
+                                # # Add Container nodes with appropriate level
+                                # foreach ($Domain in $Organization.Containers.Keys) {
+                                #     foreach ($Container in $Organization.Containers[$Domain]) {
+                                #         New-DiagramNode -Id $Container.DistinguishedName -Label $Container.Name -Image 'https://cdn-icons-png.flaticon.com/512/4725/4725970.png' -Level ($Container.ParentContainersCount + 2) -ImageType squareImage
+                                #         if ($Container.ParentContainers.Count -gt 0) {
+                                #             $TopContainer = $Container.DistinguishedName
+                                #             foreach ($Parent in $Container.ParentContainers) {
+                                #                 if (-not $Duplicates[$TopContainer]) {
+                                #                     $newDiagramEdgeSplat = @{
+                                #                         From            = $TopContainer
+                                #                         To              = $Parent
+                                #                         Color           = 'Green'
+                                #                         ArrowsToEnabled = $true
+                                #                         Dashes          = $true
+                                #                         ColorOpacity    = 0.7
+                                #                     }
+                                #                     New-DiagramEdge @newDiagramEdgeSplat
+                                #                     $Duplicates[$TopContainer] = $true
+                                #                 }
+                                #                 $TopContainer = $Parent
+                                #             }
+                                #         }
+                                #     }
+                                # }
+                                # Add OU nodes
                                 foreach ($Domain in $Organization.OrganizationalUnits.Keys) {
                                     foreach ($OU in $Organization.OrganizationalUnits[$Domain]) {
                                         New-DiagramNode -Id $OU.DistinguishedName -Label $OU.Name -Image 'https://cdn-icons-png.flaticon.com/512/3767/3767084.png' -Level ($OU.OrganizationalUnitsCount + 2) -ImageType squareImage
@@ -157,17 +197,23 @@
             }
         }
         New-HTMLTabPanel {
-            New-HTMLTab -Name "💡Organizational Units" {
-                New-HTMLSection -Title "Organizational Units" {
-                    $OrganizationalUnits = @(
+            New-HTMLTab -Name "🏗️AD Structure" {
+                New-HTMLSection -Title "Active Directory Organizational Structure" {
+                    $ADStructure = @(
+                        # Include domains first
                         foreach ($Domain in $Organization.Domains) {
                             $Domain
                         }
+                        # Include all containers
+                        foreach ($Domain in $Organization.Containers.Keys) {
+                            $Organization.Containers[$Domain]
+                        }
+                        # Include all organizational units
                         foreach ($Domain in $Organization.OrganizationalUnits.Keys) {
                             $Organization.OrganizationalUnits[$Domain]
                         }
                     )
-                    New-HTMLTable -DataTable $OrganizationalUnits -DataTableID 'DT-OrganizationalUnits' -Filtering -ScrollX -ExcludeProperty 'Objects', 'OrganizationalUnits', 'OrganizationalUnitsCount'
+                    New-HTMLTable -DataTable $ADStructure -DataTableID 'DT-OrganizationalUnits' -Filtering -ScrollX -ExcludeProperty 'Objects', 'OrganizationalUnits', 'OrganizationalUnitsCount', 'ParentContainers', 'ParentContainersCount'
                 }
             }
             New-HTMLTab -Name "🛜Subnets" {
