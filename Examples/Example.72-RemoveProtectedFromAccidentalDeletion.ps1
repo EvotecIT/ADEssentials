e5e436e585b6c2394f42cdc9f1cde59cd3bc92b7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 11:47:23 +0200

    feat(Example-RemoveProtectedFromAccidentalDeletion): ✨ add script to remove ProtectedFromAccidentalDeletion attribute
    * Introduced a new PowerShell script to set the ProtectedFromAccidentalDeletion attribute to false for all AD computers.
    * This change allows for easier management of AD objects that were previously protected from accidental deletion.

---
+++ Examples/Example.72-RemoveProtectedFromAccidentalDeletion.ps1
@@ -0,0 +1,8 @@
+﻿
+$Domain = 'ad.evotec.xyz'
+$Computers = Get-ADComputer -Filter "*" -Properties ProtectedFromAccidentalDeletion -Server $Domain
+foreach ($Computer in $Computers) {
+    if ($Computer.ProtectedFromAccidentalDeletion) {
+        Set-ADObject -ProtectedFromAccidentalDeletion $false -Identity $Computer.DistinguishedName -Server $Domain -WhatIf
+    }
+}
