6b708676545f6bdb01a9e70057e553926f4eb969
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 11:47:13 +0200

    refactor(Example-SiteCoverageSettings): ✨ enhance output clarity and verbosity
    * Added `-Verbose` flag to `Get-WinADSiteCoverage` for detailed output.
    * Improved comments for better understanding of each command's purpose.
    * Ensured consistent formatting and readability throughout the script.

--- Examples/Example.71.SiteCoverageSettings.ps1
+++ Examples/Example.71.SiteCoverageSettings.ps1
@@ -1,10 +1,19 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-Get-WinADSiteCoverage | Format-Table *
+# Show all sites coverage for all domains, in console as object
+Get-WinADSiteCoverage -Verbose | Format-Table *
 
+# Show all sites coverage for a specific domain in HTML
+Show-WinADSitesCoverage -IncludeDomains 'ad.evotec.xyz' -Verbose
+
+# Get current SiteCoverage and GCSiteCoverage
 Get-WinADDomainControllerNetLogonSettings -DomainController 'AD1'
 
+# overwrite SiteCoverage and GCSiteCoverage
 Set-WinADDomainControllerNetLogonSettings -DomainController 'AD1' -SiteCoverage 'Default-First-Site-Name' -GCSiteCoverage 'Default-First-Site-Name', 'AD1' -WhatIf
+
+# Get current SiteCoverage and GCSiteCoverage
 Get-WinADDomainControllerNetLogonSettings -DomainController 'AD1'
 
+# Remove currently set SiteCoverage and GCSiteCoverage comepletly
 Set-WinADDomainControllerNetLogonSettings -DomainController 'AD1' -SiteCoverage $null -GCSiteCoverage $null -WhatIf
