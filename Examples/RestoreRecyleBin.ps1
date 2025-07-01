0e9f7348a8b910191ebbceb14f00097272dbd10e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 11:48:33 +0200

    feat(RestoreRecycleBin): ✨ add script to manage deleted objects and pending deletions
    * Introduced a new script to set permissions for the RestoreRecycleBin.
    * Configured access for "CN=Deleted Objects" and "OU=Pending Deletion".

---
+++ Examples/RestoreRecyleBin.ps1
@@ -0,0 +1,4 @@
+﻿dsacls "CN=Deleted Objects,DC=ad,DC=evotec,DC=xyz" /g EVOTEC\RestoreRecycleBin:LCRPWP
+
+
+dsacls "OU=Pending Deletion,DC=ad,DC=evotec,DC=xyz" /I:T /g "EVOTEC\RestoreRecycleBin:WPCC"
