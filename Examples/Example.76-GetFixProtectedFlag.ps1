467ab49cef0feb1489409a5bf4be07ff9932600b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 11:47:51 +0200

    refactor(Example-GetFixProtectedFlag): ✨ update object retrieval for clarity
    * Refactored the object retrieval logic to specify `-Type Computer` for better context.
    * Enhanced output formatting for improved readability.

--- Examples/Example.76-GetFixProtectedFlag.ps1
+++ Examples/Example.76-GetFixProtectedFlag.ps1
@@ -1,7 +1,14 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
 
-#$Objects = Get-WinADBrokenProtectedFromDeletion -Verbose -Type All -LimitProcessing 3 -ReturnBrokenOnly
-#$Objects | Format-Table -AutoSize *
+$Objects = Get-WinADBrokenProtectedFromDeletion -Verbose -Type Computer -LimitProcessing 3 -ReturnBrokenOnly
+$Objects | Format-Table -AutoSize *
+
+Get-WinADBrokenProtectedFromDeletion -ReturnBrokenOnly -DistinguishedName @(
+    'CN=Test1,OU=Protected,OU=Computers,OU=Devices,OU=Production,DC=ad,DC=evotec,DC=xyz'
+
+    'CN=EX2016X1,OU=Disabled,OU=Computers,OU=Devices,OU=Production,DC=ad,DC=evotec,DC=xyz'
+) | Format-Table
+return
 
 #Repair-WinADBrokenProtectedFromDeletion -Type All -LimitProcessing 5 -Verbose -WhatIf
 
