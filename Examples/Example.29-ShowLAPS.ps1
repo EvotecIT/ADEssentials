284769ca8dd487a47c6f6378d84c2e797b9fadc3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-09-04 23:07:00 +0200

    fix(LAPS): 🐛 enhance LAPS reporting and tracking
    * Improved tracking for Windows LAPS and migration scenarios.
    * Updated HTML reports to include detailed statistics for Windows LAPS and DSRM coverage.
    * Refined data collection logic for better accuracy in LAPS and BitLocker integration.

--- Examples/Example.29-ShowLAPS.ps1
+++ Examples/Example.29-ShowLAPS.ps1
@@ -1,10 +1,10 @@
 ﻿Import-Module .\ADEssentials.psd1 -Force
-Import-Module C:\Support\GitHub\PSWriteHTML\PSWriteHTML.psd1 -force
+Import-Module C:\Support\GitHub\PSWriteHTML\PSWriteHTML.psd1 -Force
 
 #$LAPS = Get-WinADBitlockerLapsSummary -LapsOnly
 #$LAPS | Format-Table *
 
-Invoke-ADEssentials -Type Laps -Online #, LapsACL, LapsAndBitLocker -Online
+Invoke-ADEssentials -Type Laps, LapsACL, LapsAndBitLocker -Online
 
 #$LAPSComputers = Get-WinADComputerACLLAPS
 #$LAPSComputers | Format-Table *
